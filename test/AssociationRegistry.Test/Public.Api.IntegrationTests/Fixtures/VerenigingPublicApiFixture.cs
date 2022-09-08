71a047a85b68892f917785e6773acae482ddcb13
Koen Metsu <koen@metsu.be>
2022-09-08 14:03:35 +0200

    fix: OR-1182 seed minio with json ld context blob

--- test/AssociationRegistry.Test/Public.Api.IntegrationTests/Fixtures/VerenigingPublicApiFixture.cs
+++ test/AssociationRegistry.Test/Public.Api.IntegrationTests/Fixtures/VerenigingPublicApiFixture.cs
@@ -6,6 +6,11 @@ using Microsoft.Extensions.Configuration;
 
 namespace AssociationRegistry.Test.Public.Api.IntegrationTests.Fixtures;
 
+using AssociationRegistry.Public.Api.S3;
+using Be.Vlaanderen.Basisregisters.BlobStore;
+using Microsoft.Extensions.DependencyInjection;
+using Tests;
+
 public class VerenigingPublicApiFixture : IDisposable
 {
     public HttpClient HttpClient { get; private set; }
@@ -35,7 +40,26 @@ public class VerenigingPublicApiFixture : IDisposable
 
         _testServer = new TestServer(hostBuilder);
 
+        var blobClient = _testServer.Services.GetRequiredService<VerenigingenBlobClient>();
+        var associatedResourceJson = typeof(Scenario).GetAssociatedResourceJson("list-verenigingen-context");
+        if (!blobClient.BlobExistsAsync(WellknownBuckets.Verenigingen.Blobs.ListVerenigingenContext).GetAwaiter().GetResult())
+        {
+            blobClient.CreateBlobAsync(WellknownBuckets.Verenigingen.Blobs.ListVerenigingenContext, Metadata.None,
+                ContentType.Parse("application/json"),
+                GenerateStreamFromString(associatedResourceJson)).GetAwaiter().GetResult();
+        }
+
         HttpClient = _testServer.CreateClient();
+    }
+
+    private static Stream GenerateStreamFromString(string s)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(s);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
     }
 
     public void Dispose()
