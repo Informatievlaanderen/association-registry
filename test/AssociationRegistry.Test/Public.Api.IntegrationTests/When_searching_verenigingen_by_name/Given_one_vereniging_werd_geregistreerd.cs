f61955f255248ba611f9c0c1003066e452412a9f
Erik Talboom <erik.talboom@vlaanderen.be>
2022-10-11 15:20:39 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:test/AssociationRegistry.Test/Public.Api.IntegrationTests/When_searching_verenigingen_by_name/Given_one_vereniging_werd_geregistreerd.cs

--- test/AssociationRegistry.Test/Public.Api.IntegrationTests/When_searching_verenigingen_by_name/Given_one_vereniging_werd_geregistreerd.cs
+++ test/AssociationRegistry.Test/Public.Api.IntegrationTests/When_searching_verenigingen_by_name/Given_one_vereniging_werd_geregistreerd.cs
@@ -1,11 +1,13 @@
 namespace AssociationRegistry.Test.Public.Api.IntegrationTests.When_searching_verenigingen_by_name;
 
+using System.ComponentModel;
 using System.Diagnostics;
 using Fixtures;
 using FluentAssertions;
 using Marten;
 using Marten.Events;
 using Marten.Events.Projections;
+using Nest;
 using Xunit;
 
 public class Given_one_vereniging_werd_geregistreerd : IClassFixture<VerenigingPublicApiFixture>
@@ -13,6 +15,8 @@ public class Given_one_vereniging_werd_geregistreerd : IClassFixture<VerenigingP
     private readonly HttpClient _httpClient;
 
     const string VerenigingenZoekenOpNaam = "/v1/verenigingen/zoeken2?q=naam:bd";
+    private const string VCode = "v000001";
+    private const string Naam = "Feestcommittee Oudenaarde";
 
     public Given_one_vereniging_werd_geregistreerd(VerenigingPublicApiFixture verenigingPublicApiFixture)
     {
@@ -27,11 +31,13 @@ public class Given_one_vereniging_werd_geregistreerd : IClassFixture<VerenigingP
     [Fact]
     public async Task? Then_we_retrieve_one_vereniging_matching_the_name_searched()
     {
-        var esProjection = new ElasticSearchProjection();
-        esProjection.Apply(null, new List<StreamAction>(new StreamAction[]
-        {
-            StreamAction.Start("v000001", new Event[])
-        }));
+        var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+            .BasicAuthentication("elastic", "local_development")
+            .DefaultIndex("verenigingsregister-verenigingen");
+        var client = new ElasticClient(settings);
+        var esEventHandler = new ElasticEventHandler(client);
+        //esEventHandler.HandleEvent(new VerenigingWerdGeregistreerd(VCode, Naam));
+
         var responseMessage = await _httpClient.GetAsync(VerenigingenZoekenOpNaam);
         var content = await responseMessage.Content.ReadAsStringAsync();
         var goldenMaster = GetType().GetAssociatedResourceJson(
@@ -54,20 +60,28 @@ public class VerenigingWerdGeregistreerd
     }
 }
 
+public class Vereniging
+{
+    public string VCode { get; set; }
+    public string Naam { get; set; }
+}
+
 public class ElasticEventHandler
 {
-    public ElasticEventHandler()
-    {
+    private readonly IElasticClient _elasticClient;
 
+    public ElasticEventHandler(IElasticClient elasticClient)
+    {
+        _elasticClient = elasticClient;
     }
 
     public void HandleEvent(VerenigingWerdGeregistreerd message)
     {
-        // elasticClient.Index(new Vereniging(){ VCode = message.VCode, Name = message.Name });
+        _elasticClient.IndexDocument(new Vereniging(){ VCode = message.VCode, Naam = message.Naam });
     }
 }
 
-public class ElasticSearchProjection: IProjection
+/*public class ElasticSearchProjection: IProjection
 {
     public void Apply(IDocumentOperations operations, IReadOnlyList<StreamAction> streams)
     {
@@ -85,4 +99,4 @@ public class ElasticSearchProjection: IProjection
         Apply(operations, streams);
         return Task.CompletedTask;
     }
-}
+}*/
