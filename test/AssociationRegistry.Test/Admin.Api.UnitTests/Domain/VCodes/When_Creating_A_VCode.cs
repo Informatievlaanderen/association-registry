9698a1ba7f18667cfd6d2b5e9ff804bd6f68404a
Quinten Van Assche <quinten.vanassche@greenstack.be>
2022-10-19 09:14:26 +0200

    feat: or-1244 refactor VCode tests

--- test/AssociationRegistry.Test/Admin.Api.UnitTests/Domain/VCodes/When_Creating_A_VCode.cs
+++ test/AssociationRegistry.Test/Admin.Api.UnitTests/Domain/VCodes/When_Creating_A_VCode.cs
@@ -15,7 +15,7 @@ public class When_Creating_A_VCode
         [InlineData(1234, "V001234")]
         [InlineData(987654, "V987654")]
         [InlineData(999999, "V999999")]
-        public void Then_it_returns_a_new_vcode(int intCode, string expectedVCode)
+        public void Then_it_returns_a_new_vcode_formatted_as_V000000(int intCode, string expectedVCode)
         {
             var vCode = new VCode(intCode);
 
@@ -23,12 +23,22 @@ public class When_Creating_A_VCode
         }
     }
 
-    public class Given_An_Invalid_Integer
+    public class Given_An_Integer_Smaller_Than_Or_Equal_To_Zero
     {
         [Theory]
         [InlineData(0)]
         [InlineData(-1)]
         [InlineData(-1000)]
+        public void Then_it_throws_an_InvalidVCodeException(int intCode)
+        {
+            var ctor = () => new VCode(intCode);
+            ctor.Should().Throw<OutOfRangeVCode>();
+        }
+    }
+
+    public class Given_An_Integer_Bigger_Than_Or_Equals_To_One_Million
+    {
+        [Theory]
         [InlineData(1_000_000)]
         [InlineData(123_456_789)]
         public void Then_it_throws_an_InvalidVCodeException(int intCode)
@@ -38,26 +48,29 @@ public class When_Creating_A_VCode
         }
     }
 
-    public class Given_A_Valid_String
+    public class Given_A_Valid_String_With_Cappital_V
     {
         [Theory]
         [InlineData("V000001")]
         [InlineData("V000002")]
         [InlineData("V845685")]
         [InlineData("V999999")]
-        public void Given_Capital_V_Then_it_returns_a_new_vcode(string strCode)
+        public void Then_it_returns_a_new_vcode_formatted_as_V000000(string strCode)
         {
             var vCode = new VCode(strCode);
 
             vCode.Value.Should().Be(strCode);
         }
+    }
 
+    public class Given_A_Valid_String_With_Lower_Case_V
+    {
         [Theory]
-        [InlineData("v000001","V000001")]
-        [InlineData("v000002","V000002")]
-        [InlineData("v458565","V458565")]
-        [InlineData("v999999","V999999")]
-        public void Given_LowerCase_v_Then_it_returns_a_new_vcode(string strCode, string expectedVCode)
+        [InlineData("v000001", "V000001")]
+        [InlineData("v000002", "V000002")]
+        [InlineData("v458565", "V458565")]
+        [InlineData("v999999", "V999999")]
+        public void Then_it_returns_a_new_vcode_formatted_as_V000000(string strCode, string expectedVCode)
         {
             var vCode = new VCode(strCode);
 
@@ -65,25 +78,38 @@ public class When_Creating_A_VCode
         }
     }
 
-    public class Given_An_Invalid_String
+    public class Given_A_String_Of_Invalid_Length
     {
         [Theory]
         [InlineData("V00000000001")]
         [InlineData("V0000001")]
         [InlineData("V00001")]
-        public void Given_Invalid_Length_Then_It_Throws_an_InvalidVCodeLengthException(string strCode)
+        public void Then_It_Throws_an_InvalidVCodeLengthException(string strCode)
         {
             var ctor = () => new VCode(strCode);
             ctor.Should().Throw<InvalidVCodeLength>();
         }
+    }
 
+    public class Given_A_String_Not_Starting_With_V
+    {
         [Theory]
         [InlineData("1234567")]
         [InlineData("A123456")]
         [InlineData("ABCDEFG")]
+        public void Then_It_Throws_an_InvalidVCodeFormatException(string strCode)
+        {
+            var ctor = () => new VCode(strCode);
+            ctor.Should().Throw<InvalidVCodeFormat>();
+        }
+    }
+
+    public class Given_A_String_Not_Ending_With_Six_Numbers
+    {
+        [Theory]
         [InlineData("VBCDEFG")]
         [InlineData("V12345A")]
-        public void Given_It_Does_Not_Start_With_V_Then_It_Throws_an_InvalidVCodeFormatException(string strCode)
+        public void Then_It_Throws_an_InvalidVCodeFormatException(string strCode)
         {
             var ctor = () => new VCode(strCode);
             ctor.Should().Throw<InvalidVCodeFormat>();
