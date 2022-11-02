59ac4439676796aaffb7e36a1146cff9375c5b20
Quinten Van Assche <quinten.vanassche@greenstack.be>
2022-11-02 15:36:01 +0100

    feat: or-1265 add dotcover to build.sh

--- build.fsx
+++ build.fsx
@@ -9,6 +9,7 @@ nuget Be.Vlaanderen.Basisregisters.Build.Pipeline 6.0.3 //"
 open Fake.Core
 open Fake.Core.TargetOperators
 open Fake.IO.FileSystemOperators
+open Fake.DotNet
 open ``Build-generic``
 
 let product = "Basisregisters Vlaanderen"
@@ -31,6 +32,17 @@ let push = push dockerRepository
 
 supportedRuntimeIdentifiers <- [ "msil"; "linux-x64" ]
 
+let testWithDotNet path =
+  let fxVersion = getDotNetClrVersionFromGlobalJson()
+
+  let cmd = sprintf "test --no-build --no-restore --logger trx --configuration Release --no-build --no-restore /p:RuntimeFrameworkVersion=%s --dcReportType=HTML" fxVersion
+
+  let result = DotNet.exec (id) "dotcover" cmd
+  if result.ExitCode <> 0 then failwith "Test Failure"
+
+let testSolution sln =
+  testWithDotNet (sprintf "%s.sln" sln)
+
 // Solution -----------------------------------------------------------------------
 
 Target.create "Restore_Solution" (fun _ -> restore "AssociationRegistry")
@@ -39,7 +51,7 @@ Target.create "Build_Solution" (fun _ ->
   setVersions "SolutionInfo.cs"
   buildSolution "AssociationRegistry")
 
-Target.create "Test_Solution" (fun _ -> test "AssociationRegistry")
+Target.create "Test_Solution" (fun _ -> testSolution "AssociationRegistry")
 
 Target.create "Publish_Solution" (fun _ ->
   [
