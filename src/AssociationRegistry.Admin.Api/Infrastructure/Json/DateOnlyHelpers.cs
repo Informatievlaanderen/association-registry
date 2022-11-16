b1437cd0c01727b10098633ccf4ffbecc877d511
Wodan <ianluyten@gmail.com>
2022-11-16 12:14:30 +0100

    feat: or-1267 use tryparseexact instead of try-catch for dateonly convertors

---
+++ src/AssociationRegistry.Admin.Api/Infrastructure/Json/DateOnlyHelpers.cs
@@ -0,0 +1,12 @@
+namespace AssociationRegistry.Admin.Api.Infrastructure.Json;
+
+using System;
+using System.Globalization;
+
+public static class DateOnlyHelpers
+{
+    public static DateOnly TryParse(string dateOnly, string format)
+        => DateOnly.TryParseExact(dateOnly, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out var result)
+            ? result
+            : throw new InvalidDateFormat();
+}
