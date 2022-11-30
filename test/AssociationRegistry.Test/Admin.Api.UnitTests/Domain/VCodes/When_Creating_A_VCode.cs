949f212d91e60914c27a56e9c924fd4c1a8a111d
Wodan <ianluyten@gmail.com>
2022-11-30 13:08:59 +0100

    feat: OR-1338 make tests fail

--- test/AssociationRegistry.Test/Admin.Api.UnitTests/Domain/VCodes/When_Creating_A_VCode.cs
+++ test/AssociationRegistry.Test/Admin.Api.UnitTests/Domain/VCodes/When_Creating_A_VCode.cs
@@ -10,12 +10,11 @@ public class When_Creating_A_VCode
     public class Given_A_Valid_Integer
     {
         [Theory]
-        [InlineData(1001, "V1001")]
-        [InlineData(1002, "V1002")]
-        [InlineData(1234, "V1234")]
+        [InlineData(1001, "V0001001")]
+        [InlineData(1002, "V0001002")]
+        [InlineData(1234, "V0001234")]
         [InlineData(9876543, "V9876543")]
         [InlineData(9999999, "V9999999")]
-        [InlineData(1_234_567_890, "V1234567890")]
         public void Then_it_returns_a_new_vcode_formatted_as_V0000(int intCode, string expectedVCode)
         {
             var vCode = VCode.Create(intCode);
@@ -41,10 +40,10 @@ public class When_Creating_A_VCode
     public class Given_A_Valid_String_With_Capital_V
     {
         [Theory]
-        [InlineData("V1001")]
-        [InlineData("V1002")]
-        [InlineData("V845685")]
-        [InlineData("V999999")]
+        [InlineData("V0001001")]
+        [InlineData("V0001002")]
+        [InlineData("V0845685")]
+        [InlineData("V9999999")]
         public void Then_it_returns_a_new_vcode_formatted_as_V0000(string strCode)
         {
             var vCode = VCode.Create(strCode);
@@ -56,10 +55,10 @@ public class When_Creating_A_VCode
     public class Given_A_Valid_String_With_Lower_Case_V
     {
         [Theory]
-        [InlineData("v1001", "V1001")]
-        [InlineData("v1002", "V1002")]
-        [InlineData("v458565", "V458565")]
-        [InlineData("v999999", "V999999")]
+        [InlineData("v0001001", "V0001001")]
+        [InlineData("v0001002", "V0001002")]
+        [InlineData("v0458565", "V0458565")]
+        [InlineData("v9999999", "V9999999")]
         public void Then_it_returns_a_new_vcode_formatted_as_V0000(string strCode, string expectedVCode)
         {
             var vCode = VCode.Create(strCode);
@@ -71,9 +70,9 @@ public class When_Creating_A_VCode
     public class Given_A_String_Not_Starting_With_V
     {
         [Theory]
-        [InlineData("1234567")]
-        [InlineData("A123456")]
-        [InlineData("ABCDEFG")]
+        [InlineData("12345678")]
+        [InlineData("A1234567")]
+        [InlineData("ABCDEFGH")]
         public void Then_It_Throws_an_InvalidVCodeFormatException(string strCode)
         {
             var ctor = () => VCode.Create(strCode);
@@ -84,8 +83,8 @@ public class When_Creating_A_VCode
     public class Given_A_String_Not_Ending_With_Numbers
     {
         [Theory]
-        [InlineData("VBCDEFG")]
-        [InlineData("V12345A")]
+        [InlineData("VBCDEFGH")]
+        [InlineData("V12345AB")]
         public void Then_It_Throws_an_InvalidVCodeFormatException(string strCode)
         {
             var ctor = () => VCode.Create(strCode);
