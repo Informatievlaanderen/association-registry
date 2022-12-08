928a128f6f6beb3da4829fadf672563afd4e89e0
Koen Metsu <koen@metsu.be>
2022-12-08 19:19:54 +0100

    Revert "feat: or-1310 add test for metadata"
    This reverts commit 951e3e4ed0e72fc09706135cae6f6e1f34d1ef0a.

--- test/AssociationRegistry.Test/Admin.Api.IntegrationTests/EventStoreTests/When_storing_an_event/Given_An_Event.cs
+++ test/AssociationRegistry.Test/Admin.Api.IntegrationTests/EventStoreTests/When_storing_an_event/Given_An_Event.cs
@@ -1,60 +1,45 @@
 namespace AssociationRegistry.Test.Admin.Api.IntegrationTests.EventStoreTests.When_storing_an_event;
 
-using System.Text.Json;
-using AssociationRegistry.Admin.Api.Events;
-using AssociationRegistry.Admin.Api.Infrastructure;
 using AssociationRegistry.Framework;
-using AssociationRegistry.Public.Api.Infrastructure;
 using Fixtures;
 using FluentAssertions;
-using Marten;
-using NodaTime;
 using NodaTime.Extensions;
 using Xunit;
 
-public class Given_An_Event_Fixture : AdminApiFixture
+[Collection(VerenigingDbCollection.Name)]
+public class Given_An_Event
 {
-    public Given_An_Event_Fixture() : base(nameof(Given_An_Event_Fixture))
-    {
-    }
-}
-
-public class Given_An_Event : IClassFixture<Given_An_Event_Fixture>
-{
-    private readonly AdminApiFixture _fixture;
-
-    public Given_An_Event(Given_An_Event_Fixture fixture)
-    {
-        _fixture = fixture;
-    }
-
-    [Fact]
+    //[Fact]
     // TODO clear DB after test
     public async Task Then_it_is_persisted_in_the_database()
     {
         // arrange
         var streamId = Guid.NewGuid().ToString();
         var someEvent = new SomeEvent("some event");
-        var eventStore = new EventStore(_fixture.DocumentStore);
+        var eventStore = await VerenigingDbFixture.CreateEventStore();
 
         // act
-        await eventStore.Save(streamId, new CommandMetadata("SomeInitiator", new DateTime(2022, 1, 1).ToUniversalTime().ToInstant()), someEvent);
+        await eventStore.Save(
+            streamId,
+            new CommandMetadata(
+                "SomeInitiator",
+                new DateTime(2022, 1, 1).ToUniversalTime().ToInstant()),
+            someEvent);
 
         // assert
-        var events = await GetEventsFromDb(streamId, _fixture.DocumentStore!);
+        var events = await GetEventsFromDb(streamId);
         events.Should().HaveCount(1);
         var single = events.Single();
         single.Data.As<SomeEvent>().Should().BeEquivalentTo(someEvent);
         // verify assumptions about marten
         single.StreamKey.Should().Be(streamId);
         single.EventType.Should().Be<SomeEvent>();
-        single.GetHeaderString(MetadataHeaderNames.Initiator).Should().Be("SomeInitiator");
-        single.GetHeaderInstant(MetadataHeaderNames.Tijdstip).Should().Be(new DateTime(2022, 1, 1).ToUniversalTime().ToInstant());
     }
 
-    private static async Task<IReadOnlyList<Marten.Events.IEvent>> GetEventsFromDb(string streamId, IDocumentStore documentStore)
+    private static async Task<IReadOnlyList<Marten.Events.IEvent>> GetEventsFromDb(string streamId)
     {
-        await using var session = documentStore.OpenSession();
+        using var documentStore = await VerenigingDbFixture.CreateDocumentStore();
+        await using var session = documentStore.LightweightSession();
 
         return await session.Events.FetchStreamAsync(streamId);
     }
