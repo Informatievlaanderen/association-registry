45794fc53a3421e164a96eb4c91e40056097ad1c
Koen Metsu <koen@metsu.be>
2022-12-08 18:44:25 +0100

    feat: or-1310 add root conn

--- src/AssociationRegistry/Framework/CommandEnvelope.cs
+++ src/AssociationRegistry/Framework/CommandEnvelope.cs
@@ -3,5 +3,3 @@
 using MediatR;
 
 public record CommandEnvelope<TCommand>(TCommand Command, CommandMetadata Metadata) : IRequest<Unit> where TCommand : IRequest<Unit>;
-
-public record CommandMetadata(string Initiator, NodaTime.Instant Tijdstip);
