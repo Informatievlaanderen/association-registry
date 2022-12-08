b44243e42c3d8ceab31478cf7c7eb2860dd418e5
Koen Metsu <koen@metsu.be>
2022-12-08 17:29:40 +0100

    feat: or-1310 add well-known media types

---
+++ src/AssociationRegistry.Admin.Api/Constants/WellknownMediaTypes.cs
@@ -0,0 +1,7 @@
+namespace AssociationRegistry.Admin.Api.Constants;
+
+public static class WellknownMediaTypes
+{
+    public const string JsonLd = "application/ld+json";
+    public const string Json = "application/json";
+}
