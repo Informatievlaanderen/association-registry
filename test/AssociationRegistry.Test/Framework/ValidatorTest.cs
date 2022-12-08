facc8af2e4b3d0f0103c73b9d2b685434bfc2d32
Koen Metsu <koen@metsu.be>
2022-12-08 20:35:43 +0100

    feat: or-1190 add opentelemetry

--- test/AssociationRegistry.Test/Framework/ValidatorTest.cs
+++ test/AssociationRegistry.Test/Framework/ValidatorTest.cs
@@ -1,14 +1,13 @@
 namespace AssociationRegistry.Test.Framework;
 
-using Baseline;
 using FluentValidation;
 
 public abstract class ValidatorTest
 {
     public ValidatorTest()
     {
-        // This seems to fix a nullpointerexception caused by running a TestServer and
-        // these tests together. Probably through concurrent use of this Global variable.
-        ValidatorOptions.Global.DisplayNameResolver = (_, member, _) => member?.Name.SplitPascalCase();
+        // // This seems to fix a nullpointerexception caused by running a TestServer and
+        // // these tests together. Probably through concurrent use of this Global variable.
+        // ValidatorOptions.Global.DisplayNameResolver = (_, member, _) => member?.Name.SplitPascalCase();
     }
 }
