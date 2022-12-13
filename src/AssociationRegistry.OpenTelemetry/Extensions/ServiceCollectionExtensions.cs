c071fac7d29d99f610740f558d71ba2a695cb1af
Quinten Van Assche <quinten.vanassche@greenstack.be>
2022-12-13 16:16:17 +0100

    refactor: or-1190 extract openTelemetry to own project

--- src/AssociationRegistry.OpenTelemetry/Extensions/ServiceCollectionExtensions.cs
+++ src/AssociationRegistry.OpenTelemetry/Extensions/ServiceCollectionExtensions.cs
@@ -1,126 +1,23 @@
 ﻿namespace AssociationRegistry.OpenTelemetry.Extensions;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
+using global::OpenTelemetry.Exporter;
+using global::OpenTelemetry.Logs;
+using global::OpenTelemetry.Metrics;
+using global::OpenTelemetry.Resources;
+using global::OpenTelemetry.Trace;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddBlobClients(this IServiceCollection services, S3BlobClientOptions s3Options)
-        => services
-            .AddSingleton(
-                sp => new VerenigingenBlobClient(
-                    s3Options.Buckets[WellknownBuckets.Verenigingen.Name],
-                    bucketName => new S3BlobClient(sp.GetRequiredService<AmazonS3Client>(), bucketName),
-                    sp.GetRequiredService<ILogger<VerenigingenBlobClient>>()
-                ));
-
-    public static void AddS3(this IServiceCollection services, IConfiguration configuration)
-    {
-        // Use MINIO
-        if (configuration.GetValue<string>("MINIO_SERVER") != null)
-        {
-            if (configuration.GetValue<string>("MINIO_ROOT_USER") == null)
-            {
-                throw new InvalidOperationException("The MINIO_ROOT_USER configuration variable was not set.");
-            }
-
-            if (configuration.GetValue<string>("MINIO_ROOT_PASSWORD") == null)
-            {
-                throw new InvalidOperationException("The MINIO_ROOT_PASSWORD configuration variable was not set.");
-            }
-
-            services.AddSingleton(
-                new AmazonS3Client(
-                    new BasicAWSCredentials(
-                        configuration.GetValue<string>("MINIO_ROOT_USER"),
-                        configuration.GetValue<string>("MINIO_ROOT_PASSWORD")),
-                    new AmazonS3Config
-                    {
-                        RegionEndpoint = RegionEndpoint.USEast1, // minio's default region
-                        ServiceURL = configuration.GetValue<string>("MINIO_SERVER"),
-                        ForcePathStyle = true,
-                    }
-                )
-            );
-        }
-        else // Use AWS
-        {
-            services.AddSingleton(new AmazonS3Client());
-        }
-    }
-
-    public static void AddDataCache(this IServiceCollection services)
-        => services.AddSingleton<IVerenigingenRepository>(
-            sp =>
-                VerenigingenRepository.Load(sp.GetRequiredService<VerenigingenBlobClient>()).GetAwaiter().GetResult());
-
-    public static void AddJsonLdContexts(this IServiceCollection services)
-    {
-        services.AddSingleton<IVerenigingenRepository>(
-            sp =>
-                VerenigingenRepository.Load(sp.GetRequiredService<VerenigingenBlobClient>()).GetAwaiter().GetResult());
-    }
-
-    public static IServiceCollection AddElasticSearch(
-        this IServiceCollection services,
-        ElasticSearchOptionsSection elasticSearchOptions)
-    {
-        var elasticClient = CreateElasticClient(elasticSearchOptions);
-        EnsureIndexExists(elasticClient, elasticSearchOptions.Indices!.Verenigingen!);
-
-        services.AddSingleton(_ => elasticClient);
-        services.AddSingleton<IElasticClient>(_ => elasticClient);
-
-        return services;
-    }
-
-    private static void EnsureIndexExists(IElasticClient elasticClient, string verenigingenIndexName)
-    {
-        if (!elasticClient.Indices.Exists(verenigingenIndexName).Exists)
-            elasticClient.Indices.CreateVerenigingIndex(verenigingenIndexName);
-    }
-
-    private static ElasticClient CreateElasticClient(ElasticSearchOptionsSection elasticSearchOptions)
-    {
-        var settings = new ConnectionSettings(new Uri(elasticSearchOptions.Uri!))
-            .BasicAuthentication(
-                elasticSearchOptions.Username,
-                elasticSearchOptions.Password)
-            .DefaultMappingFor(
-                typeof(VerenigingDocument),
-                descriptor => descriptor.IndexName(elasticSearchOptions.Indices!.Verenigingen));
-
-        var elasticClient = new ElasticClient(settings);
-        return elasticClient;
-    }
-
-    public static IServiceCollection RegisterDomainEventHandlers(this IServiceCollection services, Assembly assembly)
-    {
-        assembly.GetTypes()
-            .Where(
-                item => item.GetInterfaces()
-                            .Where(i => i.IsGenericType)
-                            .Any(i => i.GetGenericTypeDefinition() == typeof(IDomainEventHandler<>))
-                        && !item.IsAbstract && !item.IsInterface)
-            .ToList()
-            .ForEach(
-                serviceType =>
-                {
-                    // allow only 1 eventhandler per class
-                    var interfaceType = serviceType.GetInterfaces().Single(i => i.GetGenericTypeDefinition() == typeof(IDomainEventHandler<>));
-                    services.AddScoped(interfaceType, serviceType);
-                });
-        return services;
-    }
-
     public static void AddOpenTelemetry(this IServiceCollection services)
     {
-        var assembly = typeof(Startup).Assembly;
-        var serviceName = assembly.FullName;
+        var executingAssembly = Assembly.GetExecutingAssembly();
+        var serviceName = executingAssembly.FullName;
         var serviceVersion = "1.0.0";
-        var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "unknown";
+        var assemblyVersion = executingAssembly.GetName().Version?.ToString() ?? "unknown";
         Action<ResourceBuilder> configureResource = r => r.AddService(
                 serviceName, serviceVersion: assemblyVersion, serviceInstanceId: Environment.MachineName)
             .AddAttributes(
