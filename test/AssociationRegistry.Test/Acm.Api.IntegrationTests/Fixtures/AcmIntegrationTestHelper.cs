8a07442ac16de70b47a742a9be47c1ce8655ae73
Koen Metsu <koen@metsu.be>
2022-12-20 16:54:52 +0100

    feat: OR-1278 output acm response

--- test/AssociationRegistry.Test/Acm.Api.IntegrationTests/Fixtures/AcmIntegrationTestHelper.cs
+++ test/AssociationRegistry.Test/Acm.Api.IntegrationTests/Fixtures/AcmIntegrationTestHelper.cs
@@ -5,6 +5,7 @@ using IdentityModel;
 using IdentityModel.AspNetCore.OAuth2Introspection;
 using IdentityModel.Client;
 using Microsoft.Extensions.Configuration;
+using Xunit.Abstractions;
 
 public class AcmIntegrationTestHelper
 {
@@ -21,10 +22,14 @@ public class AcmIntegrationTestHelper
     public async Task<HttpClient> CreateAcmClient()
         => await CreateAcmClient("dv_verenigingsregister_hoofdvertegenwoordigers");
 
-    public async Task<HttpClient> CreateAcmClient(string scope)
-        => await CreateMachine2MachineClientFor("acmClient", scope, "secret");
+    public async Task<HttpClient> CreateAcmClient(string scope, ITestOutputHelper testOutputHelper = null)
+        => await CreateMachine2MachineClientFor("acmClient", scope, "secret", testOutputHelper);
 
-    private async Task<HttpClient> CreateMachine2MachineClientFor(string clientId, string scope, string clientSecret)
+    private async Task<HttpClient> CreateMachine2MachineClientFor(
+        string clientId,
+        string scope,
+        string clientSecret,
+        ITestOutputHelper testOutputHelper = null)
     {
         var editApiConfiguration = _fixture.ConfigurationRoot!.GetSection(nameof(OAuth2IntrospectionOptions))
             .Get<OAuth2IntrospectionOptions>();
@@ -44,6 +49,13 @@ public class AcmIntegrationTestHelper
             });
 
         var acmResponse = await tokenClient.RequestTokenAsync(OidcConstants.GrantTypes.ClientCredentials);
+        if (testOutputHelper != null)
+        {
+            testOutputHelper.WriteLine($"Error: {acmResponse.ErrorDescription}");
+            testOutputHelper.WriteLine($"AccessToken: {acmResponse.AccessToken}");
+            testOutputHelper.WriteLine($"IdentityToken: {acmResponse.IdentityToken}");
+        }
+
         var token = acmResponse.AccessToken;
         var httpClientFor = _fixture.Server.CreateClient();
         httpClientFor.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
