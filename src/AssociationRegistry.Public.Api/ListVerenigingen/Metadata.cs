6f411e39b3db54bf0a0d1b831875546a61cfb94f
Quinten Van Assche <quinten.vanassche@greenstack.be>
2022-12-20 09:06:07 +0100

    feat: OR-1278 remove static endpoints

--- src/AssociationRegistry.Public.Api/ListVerenigingen/Metadata.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AssociationRegistry.Public.Api.ListVerenigingen;
-
-using Constants;
-
-public record Metadata(Pagination Pagination);
-
-public record Pagination(int TotalCount, int Offset, int Limit);
-
-public record PaginationQueryParams(int Offset = PagingConstants.DefaultOffset, int Limit = PagingConstants.DefaultLimit);
