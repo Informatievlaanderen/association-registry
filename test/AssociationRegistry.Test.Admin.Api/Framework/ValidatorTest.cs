b0baa1a563733e234f33cb5655458db304b0a60d
Koen Metsu <koen@metsu.be>
2023-01-09 11:16:18 +0100

    test: or-1193 add fallback

--- test/AssociationRegistry.Test.Admin.Api/Framework/ValidatorTest.cs
+++ test/AssociationRegistry.Test.Admin.Api/Framework/ValidatorTest.cs
@@ -9,6 +9,6 @@ public abstract class ValidatorTest
     {
         // This seems to fix a nullpointerexception caused by running a TestServer and
         // these tests together. Probably through concurrent use of this Global variable.
-        ValidatorOptions.Global.DisplayNameResolver = (_, member, _) => member?.Name.SplitPascalCase();
+        ValidatorOptions.Global.DisplayNameResolver = (_, member, _) => member?.Name.SplitPascalCase() ?? "";
     }
 }
