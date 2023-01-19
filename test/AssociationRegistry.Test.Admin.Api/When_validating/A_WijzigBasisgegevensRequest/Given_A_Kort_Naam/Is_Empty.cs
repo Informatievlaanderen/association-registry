1424a02e36757d0f906744ad2c1412b64e709a4a
Quinten Van Assche <quinten.vanassche@greenstack.be>
2023-01-19 11:17:53 +0100

    feat: or-1563 add validation for patch kortenaam

---
+++ test/AssociationRegistry.Test.Admin.Api/When_validating/A_WijzigBasisgegevensRequest/Given_A_Kort_Naam/Is_Empty.cs
@@ -0,0 +1,18 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.When_validating.A_WijzigBasisgegevensRequest.Given_A_Kort_Naam;
+
+using AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens;
+using Framework;
+using FluentValidation.TestHelper;
+using Xunit;
+
+public class Is_Empty : ValidatorTest
+{
+    [Fact]
+    public void Then_it_has_no_validation_errors()
+    {
+        var validator = new WijzigBasisgegevensRequestValidator();
+        var result = validator.TestValidate(new WijzigBasisgegevensRequest { KorteNaam = "" });
+
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
