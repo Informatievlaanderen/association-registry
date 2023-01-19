4ae0ef0b66a0cceae3078f748ff2da38756857c0
Erik Talboom <erik.talboom@vlaanderen.be>
2023-01-19 13:07:56 +0100

    fix: or-1563 check that initiator is required

---
+++ test/AssociationRegistry.Test.Admin.Api/When_validating/A_WijzigBasisgegevensRequest/Given_Initiator/Is_Empty.cs
@@ -0,0 +1,21 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.When_validating.A_WijzigBasisgegevensRequest.Given_Initiator;
+
+using AssociationRegistry.Admin.Api.Verenigingen.Registreer;
+using AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens;
+using AssociationRegistry.Test.Admin.Api.Framework;
+using FluentValidation.TestHelper;
+using Xunit;
+
+public class Is_Empty : ValidatorTest
+{
+    [Fact]
+    public void Then_it_has_validation_error__initiator_mag_niet_leeg_zijn()
+    {
+        var validator = new WijzigBasisgegevensRequestValidator();
+        var result = validator.TestValidate(new WijzigBasisgegevensRequest() { Initiator = "" });
+
+        result.ShouldHaveValidationErrorFor(vereniging => vereniging.Initiator)
+            .WithErrorMessage("'Initiator' mag niet leeg zijn.")
+            .Only();
+    }
+}
---
+++ test/AssociationRegistry.Test.Admin.Api/When_validating/A_WijzigBasisgegevensRequest/Given_Initiator/Is_Valid.cs
@@ -0,0 +1,19 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.When_validating.A_WijzigBasisgegevensRequest.Given_Initiator;
+
+using AssociationRegistry.Admin.Api.Verenigingen.Registreer;
+using AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens;
+using AssociationRegistry.Test.Admin.Api.Framework;
+using FluentValidation.TestHelper;
+using Xunit;
+
+public class Is_Valid : ValidatorTest
+    {
+        [Fact]
+        public void Then_it_has_no_validation_errors()
+        {
+            var validator = new WijzigBasisgegevensRequestValidator();
+            var result = validator.TestValidate(new WijzigBasisgegevensRequest() { Initiator = "abcd" });
+
+            result.ShouldNotHaveValidationErrorFor(vereniging => vereniging.Initiator);
+        }
+    }
