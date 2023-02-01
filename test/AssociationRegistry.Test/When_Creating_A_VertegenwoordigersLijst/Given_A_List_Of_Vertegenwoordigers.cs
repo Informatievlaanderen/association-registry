3407c82e7764b6a17b4fea93560e1a337af51a91
Koen Metsu <koen@metsu.be>
2023-02-01 11:48:54 +0100

    feat: or-1575 ensure unique insz within vereniging

---
+++ test/AssociationRegistry.Test/When_Creating_A_VertegenwoordigersLijst/Given_A_List_Of_Vertegenwoordigers.cs
@@ -0,0 +1,30 @@
+namespace AssociationRegistry.Test.When_Creating_A_VertegenwoordigersLijst;
+
+using AutoFixture;
+using FluentAssertions;
+using Framework;
+using INSZ;
+using Vertegenwoordigers;
+using Xunit;
+
+public class Given_A_List_Of_Vertegenwoordigers
+{
+    [Fact]
+    public void Then_It_Returns_A_Filled_VertegenwoordigersLijst()
+    {
+        var fixture = new Fixture();
+        var vertegenwoordiger1 = Vertegenwoordiger.Create(Insz.Create(InszTestSet.Insz1), false, fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>());
+        var vertegenwoordiger2 = Vertegenwoordiger.Create(Insz.Create(InszTestSet.Insz2), false, fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>());
+        var listOfVertegenwoordigers = new List<Vertegenwoordiger>()
+        {
+            vertegenwoordiger1,
+            vertegenwoordiger2,
+        };
+
+        var vertegenwoordigersLijst = VertegenwoordigersLijst.Create(listOfVertegenwoordigers);
+
+        vertegenwoordigersLijst.Should().HaveCount(2);
+        vertegenwoordigersLijst[0].Should().BeEquivalentTo(vertegenwoordiger1);
+        vertegenwoordigersLijst[1].Should().BeEquivalentTo(vertegenwoordiger2);
+    }
+}
---
+++ test/AssociationRegistry.Test/When_Creating_A_VertegenwoordigersLijst/Given_Null.cs
@@ -0,0 +1,14 @@
+﻿namespace AssociationRegistry.Test.When_Creating_A_VertegenwoordigersLijst;
+
+using FluentAssertions;
+using Vertegenwoordigers;
+using Xunit;
+
+public class Given_Null
+{
+    [Fact]
+    public void Then_It_Returns_A_VertegenwoordigersLijst()
+    {
+        VertegenwoordigersLijst.Create(null).Should().BeEquivalentTo(VertegenwoordigersLijst.Empty);
+    }
+}
