cffcf5daffcbc4186dee2c008c1810a989284711
Erik Talboom <erik.talboom@vlaanderen.be>
2023-02-01 13:45:08 +0100

    test: or-1575 add tests for Bis nummer

--- test/AssociationRegistry.Test/Framework/InszTestSet.cs
+++ test/AssociationRegistry.Test/Framework/InszTestSet.cs
@@ -6,4 +6,8 @@ public static class InszTestSet
     public const string Insz1_WithCharacters = "01.13.15-001.49";
     public const string Insz2 = "03209820396";
     public const string Insz2_WithCharacters = "03.20.98-203.96";
+    public const string Bis1 = "71432512346";
+    public const string Bis2 = "71432512375";
+    public const string Bis3 = "71232512303";
+    public const string Bis4 = "71232512332";
 }
--- test/AssociationRegistry.Test/When_Creating_An_Insz/Given_A_Valid_String.cs
+++ test/AssociationRegistry.Test/When_Creating_An_Insz/Given_A_Valid_String.cs
@@ -11,6 +11,10 @@ public class Given_A_Valid_String
     [Theory]
     [InlineData(InszTestSet.Insz1, InszTestSet.Insz1)]
     [InlineData(InszTestSet.Insz2_WithCharacters, InszTestSet.Insz2)]
+    [InlineData(InszTestSet.Bis1, InszTestSet.Bis1)]
+    [InlineData(InszTestSet.Bis2, InszTestSet.Bis2)]
+    [InlineData(InszTestSet.Bis3, InszTestSet.Bis3)]
+    [InlineData(InszTestSet.Bis4, InszTestSet.Bis4)]
     public void Then_it_Returns_A_Insz(string inszString, string expectedInsz)
     {
         string insz = Insz.Create(inszString);
