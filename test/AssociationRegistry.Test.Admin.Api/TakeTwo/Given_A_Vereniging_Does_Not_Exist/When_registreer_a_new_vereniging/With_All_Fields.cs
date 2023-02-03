95e875ea0e91a21a9bd0a0235bc4e024653635c8
Erik Talboom <erik.talboom@vlaanderen.be>
2023-02-03 16:38:17 +0100

    test: or-1569 add singleton to ensure registreer vereniging request is only executed once for multiple tests in test class

--- test/AssociationRegistry.Test.Admin.Api/TakeTwo/Given_A_Vereniging_Does_Not_Exist/When_registreer_a_new_vereniging/With_All_Fields.cs
+++ test/AssociationRegistry.Test.Admin.Api/TakeTwo/Given_A_Vereniging_Does_Not_Exist/When_registreer_a_new_vereniging/With_All_Fields.cs
@@ -1,29 +1,27 @@
 ﻿namespace AssociationRegistry.Test.Admin.Api.TakeTwo.Given_A_Vereniging_Does_Not_Exist.When_registreer_a_new_vereniging;
 
+using System.Net;
 using AssociationRegistry.Admin.Api.Constants;
+using AssociationRegistry.Admin.Api.Infrastructure;
+using AssociationRegistry.Admin.Api.Infrastructure.ConfigurationBindings;
 using AssociationRegistry.Admin.Api.Verenigingen.Registreer;
 using Events;
 using Framework;
 using AutoFixture;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Xunit;
 using Xunit.Categories;
 
-[Collection(nameof(AdminApiCollection))]
-[Category("AdminApi")]
-[IntegrationTest]
-public class With_All_Fields
-{
-    private readonly EventsInDbScenariosFixture _fixture;
-    private readonly RegistreerVerenigingRequest _request;
-    private readonly RegistreerVerenigingRequest.Vertegenwoordiger[] _vertegenwoordigers;
+public sealed class When_RegistreerVereniging_WithAllFields {
+    public readonly RegistreerVerenigingRequest Request;
+    private HttpResponseMessage? _response;
 
-    public With_All_Fields(EventsInDbScenariosFixture fixture)
+    private When_RegistreerVereniging_WithAllFields()
     {
-        _fixture = fixture;
         var autoFixture = new Fixture();
-        _request = new RegistreerVerenigingRequest
+        Request = new RegistreerVerenigingRequest
         {
             Naam = autoFixture.Create<string>(),
             KorteNaam = autoFixture.Create<string>(),
@@ -96,6 +94,44 @@ public class With_All_Fields
                 },
             },
         };
+    }
+
+    private static When_RegistreerVereniging_WithAllFields? called;
+    public static When_RegistreerVereniging_WithAllFields Called
+        => called ??= new When_RegistreerVereniging_WithAllFields();
+
+    public HttpResponseMessage Response(AdminApiFixture2 fixture)
+        => _response ??= fixture.DefaultClient.RegistreerVereniging(GetJsonBody(Request)).GetAwaiter().GetResult();
+
+    private string GetJsonBody(RegistreerVerenigingRequest request)
+        => GetType()
+            .GetAssociatedResourceJson($"files.request.with_all_fields")
+            .Replace("{{vereniging.naam}}", request.Naam)
+            .Replace("{{vereniging.korteNaam}}", request.KorteNaam)
+            .Replace("{{vereniging.korteBeschrijving}}", request.KorteBeschrijving)
+            .Replace("{{vereniging.startdatum}}", request.StartDatum!.Value.ToString(WellknownFormats.DateOnly))
+            .Replace("{{vereniging.kboNummer}}", request.KboNummer)
+            .Replace("{{vereniging.initiator}}", request.Initiator)
+            .Replace("{{vereniging.contactInfoLijst}}", JsonConvert.SerializeObject(request.ContactInfoLijst))
+            .Replace("{{vereniging.locaties}}", JsonConvert.SerializeObject(request.Locaties));
+}
+
+[Collection(nameof(AdminApiCollection))]
+[Category("AdminApi")]
+[IntegrationTest]
+public class With_All_Fields
+{
+    private readonly EventsInDbScenariosFixture _fixture;
+    private readonly RegistreerVerenigingRequest.Vertegenwoordiger[] _vertegenwoordigers;
+
+    private RegistreerVerenigingRequest Request
+        => When_RegistreerVereniging_WithAllFields.Called.Request;
+    private HttpResponseMessage Response
+        => When_RegistreerVereniging_WithAllFields.Called.Response(_fixture);
+
+    public With_All_Fields(EventsInDbScenariosFixture fixture)
+    {
+        _fixture = fixture;
 
         _vertegenwoordigers = new[]
         {
@@ -135,22 +171,7 @@ public class With_All_Fields
                 },
             },
         };
-
-        _fixture.DefaultClient.RegistreerVereniging(GetJsonBody(_request)).GetAwaiter().GetResult();
     }
-
-    private string GetJsonBody(RegistreerVerenigingRequest request)
-        => GetType()
-            .GetAssociatedResourceJson($"files.request.with_all_fields")
-            .Replace("{{vereniging.naam}}", request.Naam)
-            .Replace("{{vereniging.korteNaam}}", request.KorteNaam)
-            .Replace("{{vereniging.korteBeschrijving}}", request.KorteBeschrijving)
-            .Replace("{{vereniging.startdatum}}", request.StartDatum!.Value.ToString(WellknownFormats.DateOnly))
-            .Replace("{{vereniging.kboNummer}}", request.KboNummer)
-            .Replace("{{vereniging.initiator}}", request.Initiator)
-            .Replace("{{vereniging.contactInfoLijst}}", JsonConvert.SerializeObject(request.ContactInfoLijst))
-            .Replace("{{vereniging.locaties}}", JsonConvert.SerializeObject(request.Locaties))
-            .Replace("{{vereniging.vertegenwoordigers}}", JsonConvert.SerializeObject(request.Vertegenwoordigers));
 
     [Fact]
     public void Then_it_saves_the_events()
@@ -160,16 +181,40 @@ public class With_All_Fields
 
         var savedEvent = session.Events
             .QueryRawEventDataOnly<VerenigingWerdGeregistreerd>()
-            .Single(e => e.Naam == _request.Naam);
+            .Single(e => e.Naam == Request.Naam);
 
-        savedEvent.KorteNaam.Should().Be(_request.KorteNaam);
-        savedEvent.KorteBeschrijving.Should().Be(_request.KorteBeschrijving);
-        savedEvent.Startdatum.Should().Be(_request.StartDatum);
-        savedEvent.KboNummer.Should().Be(_request.KboNummer);
+        savedEvent.KorteNaam.Should().Be(Request.KorteNaam);
+        savedEvent.KorteBeschrijving.Should().Be(Request.KorteBeschrijving);
+        savedEvent.Startdatum.Should().Be(Request.StartDatum);
+        savedEvent.KboNummer.Should().Be(Request.KboNummer);
         savedEvent.ContactInfoLijst.Should().HaveCount(1);
-        savedEvent.ContactInfoLijst![0].Should().BeEquivalentTo(_request.ContactInfoLijst[0]);
+        savedEvent.ContactInfoLijst![0].Should().BeEquivalentTo(Request.ContactInfoLijst[0]);
         savedEvent.Locaties.Should().HaveCount(1);
-        savedEvent.Locaties![0].Should().BeEquivalentTo(_request.Locaties[0]);
+        savedEvent.Locaties![0].Should().BeEquivalentTo(Request.Locaties[0]);
         savedEvent.Vertegenwoordigers!.Should().BeEquivalentTo(_vertegenwoordigers);
+    }
+
+    [Fact]
+    public void Then_it_returns_an_accepted_response_with_correct_headers()
+    {
+        Response.StatusCode.Should().Be(HttpStatusCode.Accepted);
+    }
+
+    [Fact]
+    public void Then_it_returns_a_location_header()
+    {
+        Response.Headers.Should().ContainKey(Microsoft.Net.Http.Headers.HeaderNames.Location);
+        Response.Headers.Location!.OriginalString.Should()
+            .StartWith($"{_fixture.ServiceProvider.GetRequiredService<AppSettings>().BaseUrl}/v1/verenigingen/V");
+    }
+
+    [Fact]
+    public void Then_it_returns_a_sequence_header()
+    {
+        Response.Headers.Should().ContainKey(WellknownHeaderNames.Sequence);
+        var sequenceValues = Response.Headers.GetValues(WellknownHeaderNames.Sequence).ToList();
+        sequenceValues.Should().HaveCount(1);
+        var sequence = Convert.ToInt64(sequenceValues.Single());
+        sequence.Should().BeGreaterThan(0);
     }
 }
