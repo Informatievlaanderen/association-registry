3b9effca147d29154c8a9dcf9572e183590f8a59
Erik Talboom <erik.talboom@vlaanderen.be>
2023-02-03 16:38:17 +0100

    test: or-1569 add singleton to ensure registreer vereniging request is only executed once for multiple tests in test class

--- test/AssociationRegistry.Test.Admin.Api/CommandHandler/Scenarios/IScenario.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AssociationRegistry.Test.Admin.Api.CommandHandler.Scenarios;
-
-using AssociationRegistry.Framework;
-
-public interface IScenario
-{
-    IEnumerable<IEvent> Events();
-}
