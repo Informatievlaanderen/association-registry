1944d6c0488cc18ac10f40f57ecf1e625154c72b
Erik Talboom <erik.talboom@vlaanderen.be>
2023-02-09 15:28:54 +0100

    test: or-1569 fix dependencies between tests on VCode and fix multiple if-match headers to not be possible

--- test/AssociationRegistry.Test.Admin.Api/Given_VerenigingWerdGeregistreerd/When_WijzigBasisGegevens/RequestHandling/With_Naam_Null.cs
+++ test/AssociationRegistry.Test.Admin.Api/Given_VerenigingWerdGeregistreerd/When_WijzigBasisGegevens/RequestHandling/With_Naam_Null.cs
@@ -12,7 +12,7 @@ public class With_Naam_Null
 {
     private readonly WijzigBasisgegevensController _controller;
     private readonly WijzigBasisgegevensRequest _requestWithNaamNull = new() {Initiator = "OVO000001", Naam = null};
-    private const string VCode = "V0001001";
+    private const string VCode = "V0009001";
 
     public With_Naam_Null()
     {
