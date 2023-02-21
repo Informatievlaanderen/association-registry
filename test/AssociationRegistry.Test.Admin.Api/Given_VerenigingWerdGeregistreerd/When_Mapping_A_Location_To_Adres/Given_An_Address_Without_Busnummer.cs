760475f7481a0867ba0c294a67182fb119b00085
Erik Talboom <erik.talboom@vlaanderen.be>
2023-02-21 16:26:51 +0100

    fix: or-1567 remove bus from adres when no busnummer is present

---
+++ test/AssociationRegistry.Test.Admin.Api/Given_VerenigingWerdGeregistreerd/When_Mapping_A_Location_To_Adres/Given_An_Address_Without_Busnummer.cs
@@ -0,0 +1,26 @@
+namespace AssociationRegistry.Test.Admin.Api.Given_VerenigingWerdGeregistreerd.When_Mapping_A_Location_To_Adres;
+
+using AssociationRegistry.Admin.Api.Infrastructure.Extensions;
+using Events;
+using FluentAssertions;
+using Xunit;
+using Xunit.Categories;
+
+[UnitTest]
+public class Given_An_Address_Without_Busnummer
+{
+    [Fact]
+    public void Then_Adres_does_not_contain_Busnummer()
+    {
+        const string straatnaam = "Den berg";
+        const string huisnummer = "7";
+        const string postcode = "9000";
+        const string gemeente = "Gent";
+        const string land = "België";
+
+        var locatie = new VerenigingWerdGeregistreerd.Locatie(
+            null, straatnaam, huisnummer, null, postcode, gemeente, land, true, "Activiteiten");
+
+        locatie.ToAdresString().Should().Be($"{straatnaam} {huisnummer}, {postcode} {gemeente}, {land}");
+    }
+}
