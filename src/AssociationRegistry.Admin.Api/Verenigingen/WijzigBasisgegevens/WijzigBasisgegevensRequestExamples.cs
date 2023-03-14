5f929c6f11245574e6ceafe34e21000e7958262e
Koen Metsu <koen@metsu.be>
2023-03-14 14:27:50 +0100

    feat: or-1633 add contact info to request examples

--- src/AssociationRegistry.Admin.Api/Verenigingen/WijzigBasisgegevens/WijzigBasisgegevensRequestExamples.cs
+++ src/AssociationRegistry.Admin.Api/Verenigingen/WijzigBasisgegevens/WijzigBasisgegevensRequestExamples.cs
@@ -1,6 +1,7 @@
 namespace AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens;
 
 using System;
+using CommonRequestDataTypes;
 using Primitives;
 using Swashbuckle.AspNetCore.Filters;
 
@@ -12,7 +13,19 @@ public class WijzigBasisgegevensRequestExamples : IExamplesProvider<WijzigBasisg
             Naam = "Naam van de vereniging",
             KorteNaam = "Korte naam van de vereniging",
             KorteBeschrijving = "Korte beschrijving van de vereniging",
-            Startdatum = NullOrEmpty<DateOnly>.Create(new DateOnly(2023,1,1)),
+            Startdatum = NullOrEmpty<DateOnly>.Create(new DateOnly(2023, 1, 1)),
+            ContactInfoLijst = new[]
+            {
+                new ContactInfo
+                {
+                    Contactnaam = "Naam van het contact",
+                    SocialMedia = "https://example.org",
+                    Website = "https://example.org",
+                    PrimairContactInfo = false,
+                    Telefoon = "123456789",
+                    Email = "info@example.org"
+                }
+            },
             Initiator = "OVO000001",
         };
 }
