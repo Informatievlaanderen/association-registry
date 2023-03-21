581c77edbd95f972e4ddf577da169c9747ce6299
Erik Talboom <erik.talboom@vlaanderen.be>
2023-03-21 15:36:54 +0100

    test: or-1645 rework given_StartdatumWerdGewijzigd to composition

--- test/AssociationRegistry.Test.Admin.Api/When_Retrieving_Historiek/Projecting/Given_StartdatumWerdGewijzigd.cs
+++ test/AssociationRegistry.Test.Admin.Api/When_Retrieving_Historiek/Projecting/Given_StartdatumWerdGewijzigd.cs
@@ -2,13 +2,43 @@
 
 using AssociationRegistry.Admin.Api.Constants;
 using AssociationRegistry.Admin.Api.Infrastructure.Extensions;
+using AutoFixture;
 using Events;
+using Framework;
 using Xunit;
+using Xunit.Categories;
 
-public class Given_StartdatumWerdGewijzigd : GivenAnEventTestBase<StartdatumWerdGewijzigd>
+[UnitTest]
+public class Given_StartdatumWerdGewijzigd
 {
     [Fact]
-    public void Test()
-        => AppendsTheCorrectGebeurtenissen(
-            $"Startdatum vereniging werd gewijzigd naar '{Event.Data.Startdatum!.Value.ToString(WellknownFormats.DateOnly)}' door {Event.Initiator} op datum {Event.Tijdstip.ToBelgianDateAndTime()}.");
+    public void Then_it_adds_a_new_gebeurtenis()
+    {
+        var projectEventOnHistoriekDocument =
+            WhenApplying<StartdatumWerdGewijzigd>
+                .ToHistoriekProjectie(
+                    new Fixture().CustomizeAll().Create<StartdatumWerdGewijzigd>());
+
+        var startdatumString = projectEventOnHistoriekDocument.Event.Data.Startdatum!.Value.ToString(WellknownFormats.DateOnly);
+        
+        projectEventOnHistoriekDocument.AppendsTheCorrectGebeurtenissen(
+            $"Startdatum vereniging werd gewijzigd naar '{startdatumString}'.");
+    }
+}
+
+[UnitTest]
+public class Given_StartdatumWerdGewijzigd_With_Null
+{
+    [Fact]
+    public void Then_it_adds_a_new_gebeurtenis_with_empty_string()
+    {
+        var projectEventOnHistoriekDocument =
+            WhenApplying<StartdatumWerdGewijzigd>
+                .ToHistoriekProjectie(
+                    new Fixture().CustomizeAll().Create<StartdatumWerdGewijzigd>()
+                        with { Startdatum = null });
+
+        projectEventOnHistoriekDocument.AppendsTheCorrectGebeurtenissen(
+            $"Startdatum vereniging werd gewijzigd naar ''.");
+    }
 }
