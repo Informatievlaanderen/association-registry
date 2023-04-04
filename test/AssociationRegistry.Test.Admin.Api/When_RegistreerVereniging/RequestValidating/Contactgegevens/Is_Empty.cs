0e97726fa0a792ce8bf2eaaa89a13ab44dbf9fb0
Quinten Van Assche <quinten.vanassche@greenstack.be>
2023-04-04 10:22:50 +0200

    fix: add validation to contacgegevens

---
+++ test/AssociationRegistry.Test.Admin.Api/When_RegistreerVereniging/RequestValidating/Contactgegevens/Is_Empty.cs
@@ -0,0 +1,22 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.When_RegistreerVereniging.RequestValidating.Contactgegevens;
+
+using AssociationRegistry.Admin.Api.Verenigingen.Registreer;
+using Framework;
+using FluentValidation.TestHelper;
+using Xunit;
+
+public class Is_Empty : ValidatorTest
+{
+    [Fact]
+    public void Has_validation_error__contactgegeven_is_verplicht()
+    {
+        var validator = new RegistreerVerenigingRequestValidator();
+        var request = new RegistreerVerenigingRequest()
+        {
+            Initiator = "OVO000001",
+        };
+        var result = validator.TestValidate(request);
+
+        result.ShouldNotHaveValidationErrorFor(toeRequest => toeRequest.Contactgegevens);
+    }
+}
---
+++ test/AssociationRegistry.Test.Admin.Api/When_RegistreerVereniging/RequestValidating/Contactgegevens/Waarde/Is_Valid.cs
@@ -0,0 +1,30 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.When_RegistreerVereniging.RequestValidating.Contactgegevens.Waarde;
+
+using AssociationRegistry.Admin.Api.Verenigingen.Registreer;
+using AssociationRegistry.Admin.Api.Verenigingen.VoegContactGegevenToe;
+using Framework;
+using FluentValidation.TestHelper;
+using Xunit;
+
+public class Is_Valid : ValidatorTest
+{
+    [Fact]
+    public void Has_no_validation_error()
+    {
+        var validator = new RegistreerVerenigingRequestValidator();
+        var result = validator.TestValidate(
+            new RegistreerVerenigingRequest()
+            {
+                Contactgegevens =
+                    new[]
+                    {
+                        new RegistreerVerenigingRequest.Contactgegeven
+                        {
+                            Waarde = "test waarde",
+                        },
+                    },
+            });
+
+        result.ShouldNotHaveValidationErrorFor($"{nameof(RegistreerVerenigingRequest.Contactgegevens)}[0].{nameof(RegistreerVerenigingRequest.Contactgegeven.Waarde)}");
+    }
+}
