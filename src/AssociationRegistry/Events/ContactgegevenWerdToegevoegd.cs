d7991dec519674c5c39f0d3d9d0eb23cf68ce230
Koen Metsu <koen@metsu.be>
2023-04-12 10:30:35 +0200

    refactor: or-1682 simplify VoegContactgegevenToe aggregate method

--- src/AssociationRegistry/Events/ContactgegevenWerdToegevoegd.cs
+++ src/AssociationRegistry/Events/ContactgegevenWerdToegevoegd.cs
@@ -1,5 +1,15 @@
 ﻿namespace AssociationRegistry.Events;
 
+using ContactGegevens;
 using Framework;
 
-public record ContactgegevenWerdToegevoegd(int ContactgegevenId, string Type, string Waarde, string Omschrijving, bool IsPrimair) : IEvent;
+public record ContactgegevenWerdToegevoegd(int ContactgegevenId, string Type, string Waarde, string Omschrijving, bool IsPrimair) : IEvent
+{
+    public static ContactgegevenWerdToegevoegd With(Contactgegeven contactgegeven)
+        => new(
+            contactgegeven.ContactgegevenId,
+            contactgegeven.Type,
+            contactgegeven.Waarde,
+            contactgegeven.Omschrijving,
+            contactgegeven.IsPrimair);
+}
