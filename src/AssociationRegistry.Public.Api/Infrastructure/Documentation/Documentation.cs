4c65e106b117e02b4dc073c8ebb22595409a8f1e
Koen Metsu <koen@metsu.be>
2023-04-24 09:50:55 +0200

    feat: OR-1676 remove redundant sentence

--- src/AssociationRegistry.Public.Api/Infrastructure/Documentation/Documentation.cs
+++ src/AssociationRegistry.Public.Api/Infrastructure/Documentation/Documentation.cs
@@ -9,8 +9,6 @@ public static class Documentation
     private static string IntroductieText
         => @"
 ---
-Momenteel leest u de documentatie voor versie v1 van de Basisregisters Vlaanderen Verenigingsregister Publieke API.
-
 Voor meer algemene informatie over het gebruik van deze API, raadpleeg onze <a href=""https://vlaamseoverheid.atlassian.net/wiki/spaces/AGB/pages/6285361348/API+documentatie"">publieke confluence pagina</a>.
 
 # Introductie
