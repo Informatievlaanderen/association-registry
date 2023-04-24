e685c300d893a5ebaa291674524e4c7a653e0771
Erik Talboom <erik.talboom@vlaanderen.be>
2023-04-24 16:20:49 +0200

    fix: or-1683 readded problemdetails usings

--- src/AssociationRegistry.Admin.Api/Verenigingen/Contactgegevens/WijzigContactgegeven/WijzigContactgegevenController.cs
+++ src/AssociationRegistry.Admin.Api/Verenigingen/Contactgegevens/WijzigContactgegeven/WijzigContactgegevenController.cs
@@ -14,6 +14,8 @@ using Microsoft.AspNetCore.Mvc;
 using NodaTime;
 using Swashbuckle.AspNetCore.Filters;
 using Wolverine;
+using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetails;
+using ValidationProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ValidationProblemDetails;
 
 [ApiVersion("1.0")]
 [AdvertiseApiVersions("1.0")]
