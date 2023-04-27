2c77a8b65e8219843097d7961929a777b3f8a8bc
Koen Metsu <koen@metsu.be>
2023-04-27 15:54:24 +0200

    feat: or-1706 use oakton so we can generate Marten dynamic types

--- src/AssociationRegistry.Admin.Api/Program.cs
+++ src/AssociationRegistry.Admin.Api/Program.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Net.Mime;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 using Be.Vlaanderen.Basisregisters.Api;
 using Be.Vlaanderen.Basisregisters.Api.Exceptions;
 using Be.Vlaanderen.Basisregisters.Api.Localization;
@@ -56,6 +57,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
+using Oakton;
 using OpenTelemetry.Extensions;
 using Serilog;
 using Serilog.Debugging;
@@ -67,7 +69,7 @@ public class Program
 {
     private const string AdminGlobalPolicyName = "Admin Global";
 
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(
             new WebApplicationOptions
@@ -90,6 +92,7 @@ public class Program
         ConfigureWebHost(builder);
         ConfigureServices(builder);
 
+        builder.Host.ApplyOaktonExtensions();
         builder.Host.UseWolverine(
             options => options.Discovery.IncludeAssembly(typeof(Vereniging).Assembly));
 
@@ -120,7 +123,7 @@ public class Program
 
         ConfigureLifetimeHooks(app);
 
-        app.Run();
+        await app.RunOaktonCommands(args);
     }
 
 
