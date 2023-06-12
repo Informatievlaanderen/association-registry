e45789f21c88f1b2ed7ad766e9cb99f44fb80727
Erik Talboom <erik.talboom@vlaanderen.be>
2023-06-12 18:10:36 +0200

    test: or-1701 fix flaky test on vertegenwoordigers

--- test/AssociationRegistry.Test.Admin.Api/FeitelijkeVereniging/When_Removing_Vertegenwoordiger/CommandHandling/With_An_Unknown_VertegenwoordigerId.cs
+++ test/AssociationRegistry.Test.Admin.Api/FeitelijkeVereniging/When_Removing_Vertegenwoordiger/CommandHandling/With_An_Unknown_VertegenwoordigerId.cs
@@ -14,13 +14,13 @@ using Xunit.Categories;
 [UnitTest]
 public class With_An_Unknown_VertegenwoordigerId
 {
-    private readonly FeitelijkeVerenigingWerdGeregistreerdWithAPrimairVertegenwoordigerScenario _scenario;
+    private readonly FeitelijkeVerenigingWerdGeregistreerdScenario _scenario;
     private readonly VerwijderVertegenwoordigerCommandHandler _commandHandler;
     private readonly Fixture _fixture;
 
     public With_An_Unknown_VertegenwoordigerId()
     {
-        _scenario = new FeitelijkeVerenigingWerdGeregistreerdWithAPrimairVertegenwoordigerScenario();
+        _scenario = new FeitelijkeVerenigingWerdGeregistreerdScenario();
 
         var verenigingRepositoryMock = new VerenigingRepositoryMock(_scenario.GetVerenigingState());
 
