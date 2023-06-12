88a6ceb1faee429b1e5618433f6b2a73e1c54594
Erik Talboom <erik.talboom@vlaanderen.be>
2023-06-12 15:35:22 +0200

    fix: rename json ld context for zoeken in beheer API

--- test/AssociationRegistry.Test.Admin.Api/When_Retrieving_Json_Ld_Context/Given_the_resource_exists.cs
+++ test/AssociationRegistry.Test.Admin.Api/When_Retrieving_Json_Ld_Context/Given_the_resource_exists.cs
@@ -21,7 +21,7 @@ public class Given_The_Resource_Exists
 
     [Theory]
     [InlineData("detail-vereniging-context.json")]
-    [InlineData("list-verenigingen-context.json")]
+    [InlineData("zoek-verenigingen-context.json")]
     public async Task Then_we_get_a_successful_response(string contextName)
     {
         var response = await _adminApiClient.GetJsonLdContext(contextName);
@@ -30,7 +30,7 @@ public class Given_The_Resource_Exists
 
     [Theory]
     [InlineData("detail-vereniging-context.json")]
-    [InlineData("list-verenigingen-context.json")]
+    [InlineData("zoek-verenigingen-context.json")]
     public async Task Then_the_context_json_is_returned(string contextName)
     {
         var response = await _adminApiClient.GetJsonLdContext(contextName);
