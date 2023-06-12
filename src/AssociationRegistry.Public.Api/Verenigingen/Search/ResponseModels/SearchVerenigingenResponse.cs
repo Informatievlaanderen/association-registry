8980d1b0b4fd8b1bef1fdee82f30ca62084e9b8e
Erik Talboom <erik.talboom@vlaanderen.be>
2023-06-12 14:29:15 +0200

    feat: or-1757 add Json-LD context to zoeken in public API

--- src/AssociationRegistry.Public.Api/Verenigingen/Search/ResponseModels/SearchVerenigingenResponse.cs
+++ src/AssociationRegistry.Public.Api/Verenigingen/Search/ResponseModels/SearchVerenigingenResponse.cs
@@ -5,6 +5,10 @@ using System.Runtime.Serialization;
 [DataContract]
 public class SearchVerenigingenResponse
 {
+    /// <summary>De JSON-LD open data context</summary>
+    [DataMember(Name = "@context")]
+    public string Context { get; init; }= null!;
+
     /// <summary>
     /// Dit is de lijst van verenigingen die het resultaat van de zoekopdracht zijn
     /// </summary>
