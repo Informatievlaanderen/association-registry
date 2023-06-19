24d563a226afe96ab9eabd87ab8d957f236851c9
Erik Talboom <erik.talboom@vlaanderen.be>
2023-06-19 19:42:25 +0200

    refactor: or-1780 add the documentation that changing the opt-out is only available for FV and add this field to the examples

--- src/AssociationRegistry.Admin.Api/Verenigingen/WijzigBasisgegevens/FeitelijkeVereniging/WijzigBasisgegevensRequestExamples.cs
+++ src/AssociationRegistry.Admin.Api/Verenigingen/WijzigBasisgegevens/FeitelijkeVereniging/WijzigBasisgegevensRequestExamples.cs
@@ -13,5 +13,6 @@ public class WijzigBasisgegevensRequestExamples : IExamplesProvider<WijzigBasisg
             KorteNaam = "Korte naam van de vereniging",
             KorteBeschrijving = "Korte beschrijving van de vereniging",
             Startdatum = NullOrEmpty<DateOnly>.Create(new DateOnly(year: 2023, month: 1, day: 1)),
+            IsUitgeschrevenUitPubliekeDatastroom = true,
         };
 }
