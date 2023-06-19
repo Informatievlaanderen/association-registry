1431d7b358e3b8ef37d9dd98cd08a6f9bc012e57
Koen Metsu <koen@metsu.be>
2023-06-19 13:35:39 +0200

    feat: or-1717 add validator for pagination query params (admin)

---
+++ src/AssociationRegistry.Admin.Api/Verenigingen/Search/RequestModels/PaginationQueryParamValidator.cs
@@ -0,0 +1,14 @@
+namespace AssociationRegistry.Admin.Api.Verenigingen.Search.RequestModels;
+
+using FluentValidation;
+using Infrastructure.ConfigurationBindings;
+
+public class PaginationQueryParamsValidator: AbstractValidator<PaginationQueryParams>
+{
+    public PaginationQueryParamsValidator(AppSettings appSettings)
+    {
+        RuleFor(x => x)
+            .Must(x => x.Limit + x.Offset <= appSettings.Search.MaxNumberOfSearchResults)
+            .WithMessage($"'Limit' en 'Offset' mogen samen niet groter dan {appSettings.Search.MaxNumberOfSearchResults} zijn.");
+    }
+}
