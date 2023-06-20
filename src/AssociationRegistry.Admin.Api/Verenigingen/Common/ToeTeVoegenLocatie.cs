5cb4dc7c454405d4589a67b8653e5af709cac53f
Erik Talboom <erik.talboom@vlaanderen.be>
2023-06-20 17:13:12 +0200

    feat: or-1776 add AdresId to registreer FV request [WIP

--- src/AssociationRegistry.Admin.Api/Verenigingen/Common/ToeTeVoegenLocatie.cs
+++ src/AssociationRegistry.Admin.Api/Verenigingen/Common/ToeTeVoegenLocatie.cs
@@ -69,4 +69,21 @@ public class ToeTeVoegenAdres
     /// <summary>Het land van het adres</summary>
     [DataMember]
     public string Land { get; set; } = null!;
+
+    /// <summary>De unieke identificator van het adres in een andere bron</summary>
+    [DataMember]
+    public AdresId? AdresId { get; set; }
+}
+
+/// <summary>De unieke identificator van het adres in een andere bron</summary>
+[DataContract]
+public class AdresId
+{
+    /// <summary>De code van de bron van het adres</summary>
+    [DataMember]
+    public string AdresbronCode { get; set; } = null!;
+
+    /// <summary>De unieke identificator volgens de bron</summary>
+    [DataMember]
+    public string AdresbronWaarde { get; set; } = null!;
 }
