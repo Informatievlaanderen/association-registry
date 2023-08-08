1d1196e63283984c9d0d0e3eedeabfe7e16cd106
Quinten Van Assche <quinten.vanassche@greenstack.be>
2023-08-08 16:16:46 +0200

    refactor: or-1860 split parsing date and validation into serperate extention methods

---
+++ src/AssociationRegistry.Admin.Api/Infrastructure/Extensions/DateOnlyExtentions.cs
@@ -0,0 +1,13 @@
+﻿namespace AssociationRegistry.Admin.Api.Infrastructure.Extensions;
+
+using System;
+using NodaTime;
+
+public static class StringExtensions
+{
+    public static bool IsNullOrBeforeToday(this DateOnly? dateOnly)
+        => dateOnly is null || dateOnly <= DateOnly.FromDateTime(SystemClock.Instance.GetCurrentInstant().ToDateTimeUtc());
+
+    public static bool IsNullOrAfterToday(this DateOnly? dateOnly)
+        => dateOnly is null || dateOnly >= DateOnly.FromDateTime(SystemClock.Instance.GetCurrentInstant().ToDateTimeUtc());
+}
---
+++ src/AssociationRegistry.Admin.Api/Infrastructure/Extensions/DateOnlyHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace AssociationRegistry.Admin.Api.Infrastructure.Extensions;
+
+using System;
+
+public static class DateOnlyHelper
+{
+    public static DateOnly? ParseOrNull(string? datumString, string format)
+    {
+        if (string.IsNullOrWhiteSpace(datumString))
+            return null;
+        return DateOnly.TryParseExact(datumString, format, out var date)
+            ? date
+            : null;
+    }
+}
