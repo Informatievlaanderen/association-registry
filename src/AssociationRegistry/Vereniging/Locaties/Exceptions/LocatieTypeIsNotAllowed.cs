453ddd32713c87eb95c515a22154d1c00dd7046f
Quinten Van Assche <quinten.vanassche@greenstack.be>
2023-08-22 11:51:38 +0200

    feat: or-1825 prevent MaatschappelijkeZetel from beÃing added

---
+++ src/AssociationRegistry/Vereniging/Locaties/Exceptions/LocatieTypeIsNotAllowed.cs
@@ -0,0 +1,16 @@
+﻿namespace AssociationRegistry.Vereniging.Exceptions;
+
+using System.Runtime.Serialization;
+using Be.Vlaanderen.Basisregisters.AggregateSource;
+
+[Serializable]
+public class LocatieTypeIsNotAllowed : DomainException
+{
+    public LocatieTypeIsNotAllowed() : base(ExceptionMessages.LocatieTypeIsNotAllowed)
+    {
+    }
+
+    protected LocatieTypeIsNotAllowed(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
