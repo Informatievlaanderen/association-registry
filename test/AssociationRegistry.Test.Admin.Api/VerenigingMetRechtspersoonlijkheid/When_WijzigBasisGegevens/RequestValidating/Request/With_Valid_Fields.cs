546f2f8d33bfa99f52989ba7dbbe5777b31f0784
Koen Metsu <koen@metsu.be>
2023-08-28 10:49:32 +0200

    feat: OR-1823 Add roepnaam to WijzigBasisgegevensValidator

--- test/AssociationRegistry.Test.Admin.Api/VerenigingMetRechtspersoonlijkheid/When_WijzigBasisGegevens/RequestValidating/Request/With_Valid_Fields.cs
+++ test/AssociationRegistry.Test.Admin.Api/VerenigingMetRechtspersoonlijkheid/When_WijzigBasisGegevens/RequestValidating/Request/With_Valid_Fields.cs
@@ -1,7 +1,7 @@
 ﻿namespace AssociationRegistry.Test.Admin.Api.VerenigingMetRechtspersoonlijkheid.When_WijzigBasisGegevens.RequestValidating.Request;
 
-using AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens.FeitelijkeVereniging;
-using AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens.FeitelijkeVereniging.RequestModels;
+using AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens.MetRechtspersoonlijkheid;
+using AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens.MetRechtspersoonlijkheid.RequestModels;
 using FluentValidation.TestHelper;
 using Xunit;
 using Xunit.Categories;
@@ -10,17 +10,20 @@ using Xunit.Categories;
 public class With_Valid_Fields
 {
     [Theory]
-    [InlineData("beschrijving", null)]
-    [InlineData(null, new[] {"abcd"})]
-    [InlineData("beschrijving", new[] {"abcd"})]
-    public void Then_it_should_not_have_errors(string? korteBeschrijving, string[] hoofdactiviteiten)
+    [InlineData("beschrijving", null, null)]
+    [InlineData(null, new[] { "abcd" }, null)]
+    [InlineData(null, null, "roepnaam")]
+    [InlineData("beschrijving", new[] { "abcd" }, "roepnaam")]
+    public void Then_it_should_not_have_errors(string? korteBeschrijving, string[] hoofdactiviteiten, string roepnaam)
     {
         var validator = new WijzigBasisgegevensRequestValidator();
+
         var result = validator.TestValidate(
             new WijzigBasisgegevensRequest
             {
                 KorteBeschrijving = korteBeschrijving,
                 HoofdactiviteitenVerenigingsloket = hoofdactiviteiten,
+                Roepnaam = roepnaam,
             });
 
         result.ShouldNotHaveAnyValidationErrors();
