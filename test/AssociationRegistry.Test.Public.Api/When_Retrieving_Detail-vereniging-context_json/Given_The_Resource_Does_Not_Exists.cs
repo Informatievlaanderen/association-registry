8aeee1f3ee8302f981e620fb0d28f77b4448b578
Quinten Van Assche <quinten.vanassche@greenstack.be>
2023-08-31 12:57:40 +0200

    feat: or-1889 add tests

---
+++ test/AssociationRegistry.Test.Public.Api/When_Retrieving_Detail-vereniging-context_json/Given_The_Resource_Does_Not_Exists.cs
@@ -0,0 +1,27 @@
+﻿namespace AssociationRegistry.Test.Public.Api.When_Retrieving_Detail_vereniging_context_json;
+
+using Fixtures;
+using FluentAssertions;
+using System.Net;
+using Xunit;
+using Xunit.Categories;
+
+[Collection(VerenigingPublicApiCollection.Name)]
+[Category("PublicApi")]
+[IntegrationTest]
+public class Given_The_Resource_Does_Not_Exists : IClassFixture<StaticPublicApiFixture>
+{
+    private readonly HttpClient _httpClient;
+
+    public Given_The_Resource_Does_Not_Exists(StaticPublicApiFixture fixture)
+    {
+        _httpClient = fixture.HttpClient;
+    }
+
+    [Fact]
+    public async Task Then_we_get_a_notFound_response()
+    {
+        var response = await _httpClient.GetAsync("DoesNotExist.json");
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
