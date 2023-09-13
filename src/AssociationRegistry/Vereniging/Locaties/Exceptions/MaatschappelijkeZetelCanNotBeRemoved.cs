8d944e3a111c8b2d04eabf3e4a5939cd17fde497
Quinten Van Assche <quinten.vanassche@greenstack.be>
2023-09-13 11:59:38 +0200

    fix: or-1905 fix maatschappelijkezetelCannotBeRemoved

---
+++ src/AssociationRegistry/Vereniging/Locaties/Exceptions/MaatschappelijkeZetelCanNotBeRemoved.cs
@@ -0,0 +1,16 @@
+﻿namespace AssociationRegistry.Vereniging.Exceptions;
+
+using Be.Vlaanderen.Basisregisters.AggregateSource;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class MaatschappelijkeZetelCanNotBeRemoved : DomainException
+{
+    public MaatschappelijkeZetelCanNotBeRemoved() : base(ExceptionMessages.MaatschappelijkeZetelCanNotBeRemoved)
+    {
+    }
+
+    protected MaatschappelijkeZetelCanNotBeRemoved(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
