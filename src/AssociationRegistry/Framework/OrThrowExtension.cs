4f684b2845a9ede0c5ccb0b7d4cb9fa9b1154db5
Koen Metsu <koen@metsu.be>
2023-09-14 11:23:07 +0200

    refactor: or-1915 Add convenience method to catch unsupported operation exception

---
+++ src/AssociationRegistry/Framework/OrThrowExtension.cs
@@ -0,0 +1,40 @@
+namespace AssociationRegistry.Framework;
+
+using Be.Vlaanderen.Basisregisters.AggregateSource;
+using Vereniging.Exceptions;
+
+public static class OrThrowExtension
+{
+    public static OrThrowExtensionHelper<TVereniging> OrWhen<TVereniging>(this Task<TVereniging> source, Type type)
+        => new(source, type);
+
+    public static OrThrowExtensionHelper<TVereniging> OrWhenUnsupportedOperationForType<TVereniging>(this Task<TVereniging> source)
+        => OrWhen(source, typeof(UnsupportedOperationForVerenigingstype));
+
+    public class OrThrowExtensionHelper<TVereniging>
+    {
+        private readonly Task<TVereniging> _vereniging;
+        private readonly Type _type;
+
+        public OrThrowExtensionHelper(Task<TVereniging> vereniging, Type type)
+        {
+            _vereniging = vereniging;
+            _type = type;
+        }
+
+        public async Task<TVereniging> Throw<TException>() where TException : DomainException, new()
+        {
+            try
+            {
+                return await _vereniging;
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == _type)
+                    throw new TException();
+
+                throw;
+            }
+        }
+    }
+}
