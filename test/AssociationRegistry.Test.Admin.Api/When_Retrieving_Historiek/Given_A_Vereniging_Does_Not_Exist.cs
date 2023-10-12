4eda11c14ccf5dd35caeb6bb0ca5fc1153172392
Koen Metsu <koen@metsu.be>
2023-10-12 14:54:14 +0200

    test: or-1949 write failing test for 404

--- test/AssociationRegistry.Test.Admin.Api/When_Retrieving_Historiek/Given_A_Vereniging_Does_Not_Exist.cs
+++ test/AssociationRegistry.Test.Admin.Api/When_Retrieving_Historiek/Given_A_Vereniging_Does_Not_Exist.cs
@@ -1,8 +1,10 @@
 namespace AssociationRegistry.Test.Admin.Api.When_Retrieving_Historiek;
 
+using Be.Vlaanderen.Basisregisters.BasicApiProblem;
 using System.Net;
 using Fixtures;
 using FluentAssertions;
+using Newtonsoft.Json;
 using Xunit;
 using Xunit.Categories;
 
@@ -22,4 +24,14 @@ public class Given_A_Vereniging_Does_Not_Exist
     [Fact]
     public void Then_we_get_a_404()
         => _response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+
+    [Fact]
+    public async Task Then_we_get_a_detail()
+    {
+        var content = await _response.Content.ReadAsStringAsync();
+        var problemDetails = JsonConvert.DeserializeObject<ProblemDetails>(content);
+
+        problemDetails.Detail.Should().NotBeEmpty();
+        problemDetails.Detail.Should().Be("De gevraagde vereniging werd niet gevonden");
+    }
 }
