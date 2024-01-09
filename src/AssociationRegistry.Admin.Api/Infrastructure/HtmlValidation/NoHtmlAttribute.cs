c21ad1a0863465f91f878de6ea6b7ff688aaf9da
Koen Metsu <koen@metsu.be>
2024-01-09 10:22:57 +0100

    feat: or-2035 wip: block html through attributes

---
+++ src/AssociationRegistry.Admin.Api/Infrastructure/HtmlValidation/NoHtmlAttribute.cs
@@ -0,0 +1,12 @@
+
+
+namespace AssociationRegistry.Admin.Api.Infrastructure.HtmlValidation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.RegularExpressions;
+
+    public class NoHtmlAttribute : Attribute
+    {
+    }
+}
