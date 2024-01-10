39ac006414d81d1a1c5412a37301dc8d0795622b
Koen Metsu <koen@metsu.be>
2024-01-10 14:38:43 +0100

    feat: or-2035 make recursive

--- src/AssociationRegistry.Admin.Api/Infrastructure/Extensions/FluentValidatorExtensions.cs
+++ src/AssociationRegistry.Admin.Api/Infrastructure/Extensions/FluentValidatorExtensions.cs
@@ -27,38 +27,58 @@ public class NoHtmlValidator<T> : AbstractValidator<T>
 {
     public NoHtmlValidator()
     {
-        // TODO: nested
-        // TODO: es kijken naar 'I <3 <Programming>' als waarde?
-        var propertiesWithNoHtml = typeof(T).GetProperties()
-                                            .Where(p => p.GetCustomAttributes(typeof(NoHtmlAttribute), false).Any());
+        ProcessNestedProperties(typeof(T), "");
+    }
+
+    private void ProcessNestedProperties(System.Type type, string parentPropertyName)
+    {
+        var propertiesWithNoHtml = type.GetProperties()
+                                        .Where(p => p.GetCustomAttributes(typeof(NoHtmlAttribute), false).Any());
 
         foreach (var property in propertiesWithNoHtml)
         {
+            string fullPropertyName = string.IsNullOrEmpty(parentPropertyName)
+                                      ? property.Name
+                                      : $"{parentPropertyName}.{property.Name}";
+
             if (property.PropertyType == typeof(string))
             {
-                RuleFor(model => GetPropertyValue(model, property.Name) as string)
-                   .Must(BeNoHtml!)
+                RuleFor(model => GetPropertyValue(model, fullPropertyName) as string)
+                   .Must(BeNoHtml)
                    .WithMessage(ExceptionMessages.UnsupportedContent)
-                   .When(model => GetPropertyValue(model, property.Name) != null);
+                   .When(model => GetPropertyValue(model, fullPropertyName) != null);
             }
             else if (property.PropertyType == typeof(string[]))
             {
-                RuleForEach(model => GetPropertyValue(model, property.Name) as string[])
-                   .Must(BeNoHtml!)
+                RuleForEach(model => GetPropertyValue(model, fullPropertyName) as string[])
+                   .Must(BeNoHtml)
                    .WithName(property.Name)
                    .WithMessage(ExceptionMessages.UnsupportedContent)
-                   .When(model => GetPropertyValue(model, property.Name) != null);
+                   .When(model => GetPropertyValue(model, fullPropertyName) != null);
+            }
+            else if (property.PropertyType.IsClass && property.PropertyType != typeof(string))
+            {
+                ProcessNestedProperties(property.PropertyType, fullPropertyName);
             }
         }
     }
 
     private static object? GetPropertyValue(T model, string propertyName)
     {
-        var property = typeof(T).GetProperty(propertyName);
+        var properties = propertyName.Split('.');
+        var value = (object)model;
 
-        return property?.GetValue(model);
+        foreach (var prop in properties)
+        {
+            var property = value.GetType().GetProperty(prop);
+            value = property?.GetValue(value);
+
+            if (value == null)
+                return null;
+        }
+
+        return value;
     }
-
     private static bool BeNoHtml(string propertyValue)
         => !Regex.IsMatch(propertyValue, "<.*?>");
 }
