a8c0f3135376f59145d1fe0d0a77a14029ba160f
Quinten Van Assche <quinten.vanassche@greenstack.be>
2024-02-27 15:59:33 +0100

    feat: or-2099 remove contactgegeven on sync

--- test/AssociationRegistry.Test.Admin.Api/VerenigingMetRechtspersoonlijkheid/When_SyncKbo/CommandHandling/With_A_Different_And_Invalid_Contactgegeven.cs
+++ test/AssociationRegistry.Test.Admin.Api/VerenigingMetRechtspersoonlijkheid/When_SyncKbo/CommandHandling/With_A_Different_And_Invalid_Contactgegeven.cs
@@ -78,3 +78,64 @@ public class With_A_Different_And_Invalid_Contactgegeven
            .ContainSingle(e => e.GetType() == typeof(KboSyncSuccessful));
     }
 }
+
+[UnitTest]
+public class With_A_Removed_Contactgegeven
+{
+    private readonly VerenigingRepositoryMock _verenigingRepositoryMock;
+    private readonly VerenigingMetRechtspersoonlijkheidWerdGeregistreerd_With_Contactgegeven_Scenario _scenario;
+    private readonly string _newContactgegevenWaarde;
+
+    public With_A_Removed_Contactgegeven()
+    {
+        _scenario = new VerenigingMetRechtspersoonlijkheidWerdGeregistreerd_With_Contactgegeven_Scenario();
+        _verenigingRepositoryMock = new VerenigingRepositoryMock(_scenario.GetVerenigingState());
+
+        var fixture = new Fixture().CustomizeAdminApi();
+
+        _newContactgegevenWaarde = fixture.Create<string>();
+
+        var verenigingVolgensKbo = _scenario.VerenigingVolgensKbo;
+
+        verenigingVolgensKbo.Contactgegevens = new ContactgegevensVolgensKbo()
+        {
+            Email = null,
+            Website = null,
+            Telefoonnummer = null,
+            GSM = null,
+        };
+
+        var command = new SyncKboCommand(_scenario.KboNummer);
+        var commandMetadata = fixture.Create<CommandMetadata>();
+        var commandHandler = new SyncKboCommandHandler(new MagdaGeefVerenigingNumberFoundMagdaGeefVerenigingService(verenigingVolgensKbo));
+
+        commandHandler.Handle(
+            new CommandEnvelope<SyncKboCommand>(command, commandMetadata),
+            _verenigingRepositoryMock).GetAwaiter().GetResult();
+    }
+
+    [Fact]
+    public void Then_The_Correct_Vereniging_Is_Loaded_Once()
+    {
+        _verenigingRepositoryMock.ShouldHaveLoaded<VerenigingMetRechtspersoonlijkheid>(_scenario.KboNummer);
+    }
+
+    [Fact]
+    public void Then_A_ContactgegevenWerdVerwijderd_And_ContactgegevenKonNietOvergenomenWorden_Event_Is_Saved()
+    {
+        _verenigingRepositoryMock
+           .SaveInvocations[0]
+           .Vereniging
+           .UncommittedEvents
+           .Should()
+           .HaveCount(2)
+           .And
+           .ContainSingle(e => e.Equals(new ContactgegevenWerdVerwijderdUitKBO(
+                                            _scenario.ContactgegevenWerdOvergenomenUitKBO.ContactgegevenId,
+                                            _scenario.ContactgegevenWerdOvergenomenUitKBO.Contactgegeventype,
+                                            _scenario.ContactgegevenWerdOvergenomenUitKBO.TypeVolgensKbo,
+                                            _scenario.ContactgegevenWerdOvergenomenUitKBO.Waarde)))
+           .And
+           .ContainSingle(e => e.GetType() == typeof(KboSyncSuccessful));
+    }
+}
