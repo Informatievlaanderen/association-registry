b780bc80fcda0035075e6fb37f582be409166073
Jan Lesage <jan.lesage@vlaanderen.be>
2024-04-02 09:55:31 +0200

    fix: add security policy name and application settings

--- src/AssociationRegistry.Acm.Api/Constants/Security.cs
+++ src/AssociationRegistry.Acm.Api/Constants/Security.cs
@@ -5,11 +5,13 @@ public static class Security
     public static class ClaimTypes
     {
         public const string Scope = "scope";
+        public const string ClientId = "client_id";
     }
 
     public static class Scopes
     {
         public const string ACM = "dv_verenigingsregister_hoofdvertegenwoordigers";
         public const string Info = "vo_info";
+        public const string Admin = "dv_verenigingsregister_beheer";
     }
 }
