a99d552131b779c372ab80f07f3c9e94e882f3e5
Jan Lesage <jan.lesage@vlaanderen.be>
2024-04-11 16:23:04 +0200

    fix: or-1821 fix sqs transport service url

--- src/AssociationRegistry.Admin.Api/Infrastructure/ConfigurationBindings/AddressMatchOptionsSection.cs
+++ src/AssociationRegistry.Admin.Api/Infrastructure/ConfigurationBindings/AddressMatchOptionsSection.cs
@@ -5,4 +5,5 @@ public class AddressMatchOptionsSection
     public const string SectionName = "AddressMatchOptions";
     public string AddressMatchSqsQueueName { get; set; }
     public bool UseLocalStack { get; set; }
+    public string SqsTransportServiceUrl { get; set; }
 }
