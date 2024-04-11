8a89fb0e94effebb45a595c9aabb71236cef878f
Jan Lesage <jan.lesage@vlaanderen.be>
2024-04-11 18:32:43 +0200

    fix: or-1821 wolverine transport configuration

--- src/AssociationRegistry.Admin.Api/Infrastructure/ConfigurationBindings/AddressMatchOptionsSection.cs
+++ src/AssociationRegistry.Admin.Api/Infrastructure/ConfigurationBindings/AddressMatchOptionsSection.cs
@@ -4,6 +4,9 @@ public class AddressMatchOptionsSection
 {
     public const string SectionName = "AddressMatchOptions";
     public string AddressMatchSqsQueueName { get; set; }
-    public bool UseLocalStack { get; set; }
+    public string AddressMatchSqsDeadLetterQueueName { get; set; }
     public string SqsTransportServiceUrl { get; set; }
+    public bool OptimizeArtifactWorkflow { get; set; }
+    public bool AutoProvision { get; set; }
+    public bool UseLocalStack { get; set; }
 }
