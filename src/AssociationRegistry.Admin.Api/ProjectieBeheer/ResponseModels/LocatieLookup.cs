7ab8ef8208f856f8c7f859471bea3bb59c640ef2
emalfroy <egon.malfroy@gmail.com>
2024-05-15 15:56:07 +0200

    feat: or-1437 add locatie lookup projection endpoins

---
+++ src/AssociationRegistry.Admin.Api/ProjectieBeheer/ResponseModels/LocatieLookup.cs
@@ -0,0 +1,28 @@
+﻿namespace AssociationRegistry.Admin.Api.ProjectieBeheer.ResponseModels;
+
+using System.Collections.Generic;
+
+
+public record LocatiesMetAdresIdVolgensVCode
+{
+    public record LocatieLookup(int LocatieId, string AdresId);
+
+    public string VCode { get; set; }
+    public IEnumerable<LocatieLookup> Data { get; set; }
+}
+
+public record LocatiesMetAdresIdVolgensAdresId
+{
+    public record LocatieLookup(string VCode, int LocatieId);
+
+    public string AdresId { get; set; }
+    public IEnumerable<LocatieLookup> Data { get; set; }
+}
+
+public record LocatiesMetAdresIdVolgensLocatieId
+{
+    public record LocatieLookup(string VCode, string AdresId);
+
+    public int LocatieId { get; set; }
+    public IEnumerable<LocatieLookup> Data { get; set; }
+}
