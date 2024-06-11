7e9d7e84be28f4d7e75cb2b42d0f25103045ea85
emalfroy <egon.malfroy@gmail.com>
2024-06-11 10:38:53 +0200

    feat: or-1437 add first tests and implementation TeHeradresserenLocatieMapper

--- test/AssociationRegistry.Test.Admin.Api/Grar/Kafka/When_Receiving_StreetNameWasReaddressedEvent/Given_No_Records_In_LocatieLookupTable.cs
+++ test/AssociationRegistry.Test.Admin.Api/Grar/Kafka/When_Receiving_StreetNameWasReaddressedEvent/Given_No_Records_In_LocatieLookupTable.cs
@@ -1,5 +1,7 @@
 ﻿namespace AssociationRegistry.Test.Admin.Api.Grar.Kafka.When_Receiving_StreetNameWasReaddressedEvent;
 
+using AssociationRegistry.Admin.Api.GrarSync;
+using AssociationRegistry.Admin.Schema.Detail;
 using Xunit;
 using FluentAssertions;
 
@@ -13,17 +15,11 @@ public class Given_No_Records_In_LocatieLookupTable
     [Fact]
     public async Task Then_No_Messages_Are_Queued()
     {
-        var sut = new LocatieFinder();
+        var locatieFinder = new LocatieFinder(new List<LocatieLookupDocument>());
+
+        var sut = new TeHeradresserenLocatiesMapper(locatieFinder);
         var result = await sut.ForAddress("1");
 
         result.Should().BeEmpty();
-    }
-}
-
-public class LocatieFinder
-{
-    public async Task<IEnumerable<object>> ForAddress(string AdresId)
-    {
-        return  ArraySegment<object>.Empty;
     }
 }
