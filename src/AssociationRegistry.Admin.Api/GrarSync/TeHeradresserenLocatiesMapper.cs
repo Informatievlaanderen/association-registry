6e88a9e82cc6106e5f862283b33f018467741d50
emalfroy <egon.malfroy@gmail.com>
2024-06-11 10:38:53 +0200

    feat: or-1437 add first tests and implementation TeHeradresserenLocatieMapper

---
+++ src/AssociationRegistry.Admin.Api/GrarSync/TeHeradresserenLocatiesMapper.cs
@@ -0,0 +1,35 @@
+﻿namespace AssociationRegistry.Admin.Api.GrarSync;
+
+using Schema.Detail;
+
+public class TeHeradresserenLocatiesMapper
+{
+    private readonly ILocatieFinder _locatieFinder;
+
+    public TeHeradresserenLocatiesMapper(ILocatieFinder locatieFinder)
+    {
+        _locatieFinder = locatieFinder;
+    }
+
+    public async Task<IEnumerable<TeHeradresserenLocatiesMessage>> ForAddress(string adresId)
+    {
+        var locaties = await _locatieFinder.FindLocaties(adresId);
+
+        if (!locaties.Any())
+            return Array.Empty<TeHeradresserenLocatiesMessage>();
+
+        var teHeradresserenLocaties = locaties.Select(l => new TeHeradresserenLocatiesMessage(l.VCode, new List<(int, string)>
+        {
+            (l.LocatieId, l.AdresId)
+        }));
+
+        return teHeradresserenLocaties;
+    }
+}
+
+public interface ILocatieFinder
+{
+    public Task<IEnumerable<LocatieLookupDocument>> FindLocaties(string adresId);
+}
+
+public record TeHeradresserenLocatiesMessage(string VCode, List<(int, string)> LocatiesMetAdres);
---
+++ test/AssociationRegistry.Test.Admin.Api/Grar/Kafka/When_Receiving_StreetNameWasReaddressedEvent/Given_Records_In_LocatieLookupTable.cs
@@ -0,0 +1,102 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.Grar.Kafka.When_Receiving_StreetNameWasReaddressedEvent;
+
+using AssociationRegistry.Admin.Api.GrarSync;
+using AssociationRegistry.Admin.Schema.Detail;
+using Xunit;
+using FluentAssertions;
+
+public class Given_LocatieLookup_Records
+{
+    public Given_LocatieLookup_Records()
+    {
+
+    }
+
+    [Fact]
+    public async Task Then_Messages_Are_Queued()
+    {
+        var locatieFinder = new LocatieFinder(new List<LocatieLookupDocument>()
+        {
+            new LocatieLookupDocument()
+            {
+                VCode = "VCode1",
+                AdresId = "123",
+                LocatieId = 1
+            },
+            new LocatieLookupDocument()
+            {
+                VCode = "VCode1",
+                AdresId = "456",
+                LocatieId = 1
+            }
+        });
+
+        var sut = new TeHeradresserenLocatiesMapper(locatieFinder);
+
+        var result = await sut.ForAddress("123");
+
+        result.Should().BeEquivalentTo(new List<TeHeradresserenLocatiesMessage>()
+        {
+            new TeHeradresserenLocatiesMessage("VCode1", new List<(int, string)>(){(1,"123")})
+        });
+    }
+}
+
+public class Given_Multiple_LocatieLookup_Records_For_The_Same_VCode
+{
+    public Given_Multiple_LocatieLookup_Records_For_The_Same_VCode()
+    {
+
+    }
+
+    [Fact]
+    public async Task Then_Messages_Are_Queued()
+    {
+        var locatieFinder = new LocatieFinder(new List<LocatieLookupDocument>()
+        {
+            new LocatieLookupDocument()
+            {
+                VCode = "VCode1",
+                AdresId = "123",
+                LocatieId = 1
+            },
+            new LocatieLookupDocument()
+            {
+                VCode = "VCode1",
+                AdresId = "456",
+                LocatieId = 1
+            },
+            new LocatieLookupDocument()
+            {
+                VCode = "VCode2",
+                AdresId = "123",
+                LocatieId = 2
+            }
+        });
+
+        var sut = new TeHeradresserenLocatiesMapper(locatieFinder);
+
+        var result = await sut.ForAddress("123");
+
+        result.Should().BeEquivalentTo(new List<TeHeradresserenLocatiesMessage>()
+        {
+            new TeHeradresserenLocatiesMessage("VCode1", new List<(int, string)>(){(1,"123")}),
+            new TeHeradresserenLocatiesMessage("VCode2", new List<(int, string)>(){(2,"123")})
+        });
+    }
+}
+
+public class LocatieFinder : ILocatieFinder
+{
+    private readonly List<LocatieLookupDocument> _locatieLookupDocuments;
+
+    public LocatieFinder(List<LocatieLookupDocument> locatieLookupDocuments)
+    {
+        _locatieLookupDocuments = locatieLookupDocuments;
+    }
+
+    public async Task<IEnumerable<LocatieLookupDocument>> FindLocaties(string adresId)
+    {
+        return _locatieLookupDocuments.Where(x => x.AdresId == adresId);
+    }
+}
