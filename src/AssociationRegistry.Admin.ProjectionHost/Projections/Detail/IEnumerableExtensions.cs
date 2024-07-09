7c7cd50f7de758f69c65fcf46dbd35b8e40ec345
Koen Metsu <koen@metsu.be>
2024-07-09 10:36:19 +0200

    fix: or-2286 don't try to clear adresId when locatieWerdVerwijderd

--- src/AssociationRegistry.Admin.ProjectionHost/Projections/Detail/IEnumerableExtensions.cs
+++ src/AssociationRegistry.Admin.ProjectionHost/Projections/Detail/IEnumerableExtensions.cs
@@ -16,6 +16,22 @@ public static class IEnumerableExtensions
               .Append(updatedObject);
     }
 
+    public static IEnumerable<T> UpdateSingleOrNothing<T>(this IEnumerable<T> collection, Func<T, bool> identityFunc, Func<T, T> update)
+        where T : IHasBron
+    {
+        var array = collection as T[] ?? collection.ToArray();
+        var objectToUpdate = array.SingleOrDefault(identityFunc);
+
+        if (objectToUpdate is null)
+            return array;
+
+        var updatedObject = update(objectToUpdate);
+
+        return array
+              .Where(t1 => !identityFunc(t1))
+              .Append(updatedObject);
+    }
+
     public static IEnumerable<T> UpdateSingleWith<T>(this IEnumerable<T> collection, Func<T, bool> identityFunc, Func<T, T> update)
     {
         var array = collection as T[] ?? collection.ToArray();
