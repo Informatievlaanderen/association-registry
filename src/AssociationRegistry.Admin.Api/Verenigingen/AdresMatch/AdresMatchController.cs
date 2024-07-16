03dee71c42e1b13f9b1996a6d34dedb3cc5d8744
emalfroy <egon.malfroy@gmail.com>
2024-07-16 16:36:02 +0200

    feat: or-2159 one time adresmatch adresses without adresid

---
+++ src/AssociationRegistry.Admin.Api/Verenigingen/AdresMatch/AdresMatchController.cs
@@ -0,0 +1,39 @@
+﻿namespace AssociationRegistry.Admin.Api.Verenigingen.AdresMatch;
+
+using Asp.Versioning;
+using Be.Vlaanderen.Basisregisters.Api;
+using Grar.AddressMatch;
+using Marten;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Schema.Detail;
+
+[ApiVersion("1.0")]
+[AdvertiseApiVersions("1.0")]
+[ApiRoute("admin")]
+[ApiExplorerSettings(IgnoreApi = true)]
+[Authorize(Policy = Program.SuperAdminPolicyName)]
+public class AdresMatchController : ApiController
+{
+    [HttpPost("adresmatch")]
+    public async Task<IActionResult> QueueAdressenForAdresMatch(
+        [FromServices] IDocumentStore documentStore,
+        [FromServices] TeAdresMatchenLocatieMessageHandler handler,
+        CancellationToken cancellationToken)
+    {
+        await using var session = documentStore.LightweightSession();
+
+        var messages = session.Query<BeheerVerenigingDetailDocument>()
+                              .Where(w => w.Locaties.Any(a => a.AdresId == null && a.Adres != null))
+                              .ToList()
+                              .SelectMany(s => s.Locaties.Where(a => a.AdresId == null && a.Adres != null)
+                                                .Select(y => (s.VCode, y.LocatieId)));
+
+        foreach (var message in messages)
+        {
+            await handler.Handle(new TeAdresMatchenLocatieMessage(message.VCode, message.LocatieId), cancellationToken);
+        }
+
+        return Ok();
+    }
+}
