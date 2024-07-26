3f879a059863d37cc74dc432908b9ed762b8f907
emalfroy <egon.malfroy@gmail.com>
2024-07-26 10:25:22 +0200

    feat: or-2159 fix null values in locatieszonderadresmatch projections

--- src/AssociationRegistry/Events/AdresKonNietOvergenomenWordenUitAdressenregister.cs
+++ src/AssociationRegistry/Events/AdresKonNietOvergenomenWordenUitAdressenregister.cs
@@ -2,4 +2,7 @@
 
 using Framework;
 
-public record AdresKonNietOvergenomenWordenUitAdressenregister(string VCode, int LocatieId, string Adres, string Reden = "") : IEvent;
+public record AdresKonNietOvergenomenWordenUitAdressenregister(string VCode, int LocatieId, string Adres, string Reden = "") : IEvent
+{
+    public const string RedenLocatieWerdVerwijderd = "Locatie kon niet gevonden worden. Mogelijks is deze verwijderd.";
+}
---
+++ test/AssociationRegistry.Test.Admin.Api/When_Retrieving_LocatieZonderAdresMatch/Projecting/Given_AdresKonNietOvergenomenWordenUitAdressenregister_MetRedenVerwijderdeLocatie.cs
@@ -0,0 +1,45 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.When_Retrieving_LocatieZonderAdresMatch.Projecting;
+
+using AssociationRegistry.Admin.Schema.Detail;
+using AssociationRegistry.Framework;
+using AutoFixture;
+using Events;
+using Fixtures;
+using FluentAssertions;
+using Marten;
+using Xunit;
+
+[Collection(nameof(MultiStreamTestCollection))]
+public class Given_AdresKonNietOvergenomenWordenUitAdressenregister_MetRedenVerwijderdeLocatie : IClassFixture<GivenLocatieWerdVerwijderdFixture>
+{
+    private readonly GivenLocatieWerdVerwijderdFixture _fixture;
+
+    public Given_AdresKonNietOvergenomenWordenUitAdressenregister_MetRedenVerwijderdeLocatie(GivenLocatieWerdVerwijderdFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Then_A_Document_Should_Be_Deleted()
+    {
+        var session = _fixture.DocumentStore.LightweightSession();
+        var docs = await session.Query<LocatieZonderAdresMatchDocument>().ToListAsync();
+        docs.Should().BeEmpty();
+    }
+}
+
+public class GivenLocatieWerdVerwijderdBugFixture : MultiStreamTestFixture
+{
+    public GivenLocatieWerdVerwijderdBugFixture()
+    {
+        var vCode = "V9900003";
+        var locatie = Fixture.Create<Registratiedata.Locatie>() with { LocatieId = 1 };
+
+        Stream(vCode, new IEvent[]
+        {
+            Fixture.Create<FeitelijkeVerenigingWerdGeregistreerd>() with { VCode = vCode, Locaties = new[] { locatie } },
+            Fixture.Create<LocatieWerdVerwijderd>() with { VCode = vCode, Locatie = locatie },
+            Fixture.Create<AdresKonNietOvergenomenWordenUitAdressenregister>() with { VCode = vCode, LocatieId = locatie.LocatieId, Reden = AdresKonNietOvergenomenWordenUitAdressenregister.RedenLocatieWerdVerwijderd},
+        });
+    }
+}
