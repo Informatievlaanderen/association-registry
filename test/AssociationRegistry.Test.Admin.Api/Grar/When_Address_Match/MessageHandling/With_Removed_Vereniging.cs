99402b9126998a2120d4c6e5b9a1a86183c4f6e9
emalfroy <egon.malfroy@gmail.com>
2024-08-06 14:28:16 +0200

    feat: or-2339 dont throw exception for adresmatch when vereniging is verwijderd

---
+++ test/AssociationRegistry.Test.Admin.Api/Grar/When_Address_Match/MessageHandling/With_Removed_Vereniging.cs
@@ -0,0 +1,37 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.Grar.When_Address_Match.MessageHandling;
+
+using AssociationRegistry.Grar;
+using AssociationRegistry.Grar.AddressMatch;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Vereniging;
+using Vereniging.Exceptions;
+using Xunit;
+using Xunit.Categories;
+
+[UnitTest]
+public class With_Removed_Vereniging
+{
+    [Fact]
+    public async Task Then_Nothing()
+    {
+
+        var verenigingRepositoryMock = new Mock<IVerenigingsRepository>();
+
+        var vCode = VCode.Create(1001001);
+
+        verenigingRepositoryMock.Setup(s => s.Load<VerenigingOfAnyKind>(It.IsAny<VCode>(),
+                                                                        It.IsAny<long?>()))
+                                .ThrowsAsync(new VerenigingWerdVerwijderd(vCode));
+
+        var grarClient = new Mock<IGrarClient>();
+
+        var adresMatchenLocatieMessageHandler = new TeAdresMatchenLocatieMessageHandler(verenigingRepositoryMock.Object,
+                                                                                        grarClient.Object,
+                                                                                        new NullLogger<
+                                                                                            TeAdresMatchenLocatieMessageHandler>()
+        );
+
+        await adresMatchenLocatieMessageHandler.Handle(new TeAdresMatchenLocatieMessage(vCode, 1));
+    }
+}
