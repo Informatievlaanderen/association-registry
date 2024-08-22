b67bb5da030aea612fd2df13f7e84e5cf78c1060
emalfroy <egon.malfroy@gmail.com>
2024-08-22 11:07:28 +0200

    fix: or-2378 listen to locatiewerdgewijzigd event for locatiezonderadresmatch projection

---
+++ test/AssociationRegistry.Test.Admin.Api/Projections/When_Retrieving_LocatieZonderAdresMatch/Projector/Given_LocatieWerdGewijzigd.cs
@@ -0,0 +1,46 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.Projections.When_Retrieving_LocatieZonderAdresMatch.Projector;
+
+using AssociationRegistry.Admin.Schema.Detail;
+using AssociationRegistry.Events;
+using AssociationRegistry.Framework;
+using AssociationRegistry.Vereniging;
+using AutoFixture;
+using FluentAssertions;
+using Framework.Fixtures;
+using Marten;
+using Xunit;
+
+[Collection(nameof(MultiStreamTestCollection))]
+public class Given_LocatieWerdGewijzigd : IClassFixture<GivenLocatieWerdGewijzigdFixture>
+{
+    private readonly GivenLocatieWerdGewijzigdFixture _fixture;
+
+    public Given_LocatieWerdGewijzigd(GivenLocatieWerdGewijzigdFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Then_A_Document_Should_Be_Created()
+    {
+        var session = _fixture.DocumentStore.LightweightSession();
+        var docs = await session.Query<LocatieZonderAdresMatchDocument>().ToListAsync();
+        docs.Should().NotBeEmpty();
+        docs.Should().HaveCount(1);
+    }
+}
+
+public class GivenLocatieWerdGewijzigdFixture : MultiStreamTestFixture
+{
+    public GivenLocatieWerdGewijzigdFixture()
+    {
+        var vCode = "V9900002";
+        var locatie = Fixture.Create<Registratiedata.Locatie>() with { LocatieId = 1, Locatietype = Locatietype.Activiteiten.Waarde };
+
+        Stream(vCode, new IEvent[]
+        {
+            Fixture.Create<FeitelijkeVerenigingWerdGeregistreerd>() with { VCode = vCode, Locaties = Array.Empty<Registratiedata.Locatie>() },
+            Fixture.Create<LocatieWerdGewijzigd>() with { Locatie = locatie },
+        });
+    }
+}
