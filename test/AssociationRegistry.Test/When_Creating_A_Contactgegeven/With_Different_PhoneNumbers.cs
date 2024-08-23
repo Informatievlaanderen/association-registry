43a7ab89decd88d702a42ef74f5f7d63462b80fc
Jan Lesage <jan.lesage@vlaanderen.be>
2024-08-23 10:36:38 +0200

    feat: or-2273 add phone number validation for contactgegeven

---
+++ test/AssociationRegistry.Test/When_Creating_A_Contactgegeven/With_Different_PhoneNumbers.cs
@@ -0,0 +1,32 @@
+﻿namespace AssociationRegistry.Test.When_Creating_A_Contactgegeven;
+
+using Vereniging;
+using Xunit;
+using Xunit.Categories;
+
+[UnitTest]
+public class With_Different_PhoneNumbers
+{
+    [Theory]
+    [InlineData("+32 412 34 56 78", "0032412345678")]
+    [InlineData("+32 051 12 34 56", "003251123456")]
+    [InlineData("+32 51 12 34 56", "003251123456")]
+    [InlineData("+32 (412)/34.56.78", "0032412345678")]
+    [InlineData("(+32) 0412 34 56 78", "0032412345678")]
+    [InlineData("+32 412/34-56-78", "0032412345678")]
+    [InlineData("+32412345678", "0032412345678")]
+    [InlineData("+32 412 34 56 78 (home)", "0032412345678")]
+    [InlineData("0412 34 56 78", "0032412345678")]
+    [InlineData("0412345678", "0032412345678")]
+    [InlineData("051 12 34 56", "003251123456")]
+    [InlineData("051123456", "003251123456")]
+    [InlineData("0032 412 34 56 78", "0032412345678")]
+    [InlineData("+31 (0)20 369 0664", "0031203690664")]
+    [InlineData("+4989 9982804-50", "004989998280450")]
+    public void NormalizePhoneNumber_ShouldReturnCorrectFormat(string input, string expected)
+    {
+        var actual = Contactgegeven.NormalizePhoneNumber(input);
+
+        Assert.Equal(expected, actual);
+    }
+}
