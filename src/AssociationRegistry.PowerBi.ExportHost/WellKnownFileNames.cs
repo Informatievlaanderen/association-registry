e7b3041adc9e0fb5f2e206c7f94858424131d95b
emalfroy <egon.malfroy@gmail.com>
2024-09-03 18:24:26 +0200

    feat: or-2390 add contactgegeven for power bi export

--- src/AssociationRegistry.PowerBi.ExportHost/WellKnownFileNames.cs
+++ src/AssociationRegistry.PowerBi.ExportHost/WellKnownFileNames.cs
@@ -5,4 +5,5 @@ public class WellKnownFileNames
     public const string Hoofdactiviteiten = "Hoofdactiviteiten.csv";
     public const string Basisgegevens = "Basisgegevens.csv";
     public const string Locaties = "Locaties.csv";
+    public const string Contactgegevens = "Contactgegevens.csv";
 }
