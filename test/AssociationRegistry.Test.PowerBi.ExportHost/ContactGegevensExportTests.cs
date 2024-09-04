06659a5d0f302bc6e9066b4241bce5cc9fe91044
emalfroy <egon.malfroy@gmail.com>
2024-09-04 16:30:52 +0200

    test: or-2354 refactor tests power bi export

--- test/AssociationRegistry.Test.PowerBi.ExportHost/ContactGegevensExportTests.cs
+++ test/AssociationRegistry.Test.PowerBi.ExportHost/ContactGegevensExportTests.cs
@@ -1,51 +1,81 @@
-﻿// namespace AssociationRegistry.Test.PowerBi.ExportHost;
-//
-// using Admin.Schema.PowerBiExport;
-// using AssociationRegistry.PowerBi.ExportHost;
-// using AssociationRegistry.PowerBi.ExportHost.Writers;
-// using AutoFixture;
-// using Common.AutoFixture;
-// using FluentAssertions;
-// using System.Text;
-// using Xunit;
-//
-// public class ContactgegevensExportTests
-// {
-//     [Fact]
-//     public async Task WithMultipleDocuments_ThenCsvExportShouldExport()
-//     {
-//
-//         var fixture = new Fixture().CustomizeDomain();
-//
-//         var docs = fixture.CreateMany<PowerBiExportDocument>();
-//
-//         var content = await GenerateCsv(docs);
-//         var stringBuilder = new StringBuilder();
-//         stringBuilder.Append("beschrijving,bron,contactgegevenId,contactgegevenType,isPrimair,vCode,waarde\r\n");
-//
-//         foreach (var doc in docs)
-//         {
-//             foreach (var contactgegeven in doc.Contactgegevens)
-//             {
-//                 stringBuilder.Append($"{contactgegeven.Beschrijving},{contactgegeven.Bron},{contactgegeven.ContactgegevenId},{contactgegeven.Contactgegeventype},{contactgegeven.IsPrimair},{doc.VCode},{contactgegeven.Waarde}\r\n");
-//             }
-//         }
-//
-//         content.Should().BeEquivalentTo(stringBuilder.ToString());
-//     }
-//
-//     private static async Task<string> GenerateCsv(IEnumerable<PowerBiExportDocument> docs)
-//     {
-//         var exporter = new PowerBiDocumentExporter();
-//
-//         var exportStream = await exporter.Export(docs, new ContactgegevensRecordWriter());
-//
-//         using var reader = new StreamReader(exportStream, Encoding.UTF8);
-//
-//         var content = await reader.ReadToEndAsync();
-//
-//         return content;
-//     }
-// }
-//
-//
+﻿namespace AssociationRegistry.Test.PowerBi.ExportHost;
+
+using Admin.Schema.PowerBiExport;
+using Amazon.S3;
+using Amazon.S3.Model;
+using AssociationRegistry.PowerBi.ExportHost;
+using AssociationRegistry.PowerBi.ExportHost.Writers;
+using AutoFixture;
+using Common.AutoFixture;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System.Text;
+using Xunit;
+
+public class ContactgegevensExportTests
+{
+    private Stream _resultStream = null;
+
+    [Fact]
+    public async Task WithMultipleDocuments_ThenCsvExportShouldExport()
+    {
+        var fixture = new Fixture().CustomizeDomain();
+        var s3ClientMock = SetupS3Client();
+        var docs = fixture.CreateMany<PowerBiExportDocument>();
+
+        await Export(docs, s3ClientMock.Object);
+
+        var actualResult = await GetActualResult();
+        var expectedResult = GetExpectedResult(docs);
+
+        actualResult.Should().BeEquivalentTo(expectedResult);
+    }
+
+    private async Task<string> GetActualResult()
+    {
+        using var reader = new StreamReader(_resultStream, Encoding.UTF8);
+
+        var content = await reader.ReadToEndAsync();
+
+        return content;
+    }
+
+    private static string GetExpectedResult(IEnumerable<PowerBiExportDocument> docs)
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append("beschrijving,bron,contactgegevenId,contactgegevenType,isPrimair,vCode,waarde\r\n");
+
+        foreach (var doc in docs)
+        {
+            foreach (var contactgegeven in doc.Contactgegevens)
+            {
+                stringBuilder.Append($"{contactgegeven.Beschrijving},{contactgegeven.Bron},{contactgegeven.ContactgegevenId},{contactgegeven.Contactgegeventype},{contactgegeven.IsPrimair},{doc.VCode},{contactgegeven.Waarde}\r\n");
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private Mock<IAmazonS3> SetupS3Client()
+    {
+        var s3ClientMock = new Mock<IAmazonS3>();
+
+        s3ClientMock.Setup(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>(), default))
+                    .Callback<PutObjectRequest, CancellationToken>((request, _) => _resultStream = request.InputStream)
+                    .ReturnsAsync(new PutObjectResponse { HttpStatusCode = System.Net.HttpStatusCode.OK });
+
+        return s3ClientMock;
+    }
+
+    private static async Task Export(IEnumerable<PowerBiExportDocument> docs, IAmazonS3 s3Client)
+    {
+        var exporter = new Exporter(WellKnownFileNames.Contactgegevens,
+                                    "something",
+                                    new ContactgegevensRecordWriter(),
+                                    s3Client,
+                                    new NullLogger<Exporter>());
+
+        await exporter.Export(docs);
+    }
+}
--- test/AssociationRegistry.Test.PowerBi.ExportHost/HoofdactiviteitenExportTests.cs
+++ test/AssociationRegistry.Test.PowerBi.ExportHost/HoofdactiviteitenExportTests.cs
@@ -1,4 +1,105 @@
-﻿// namespace AssociationRegistry.Test.PowerBi.ExportHost;
+﻿namespace AssociationRegistry.Test.PowerBi.ExportHost;
+
+using Admin.Schema.PowerBiExport;
+using Amazon.S3;
+using Amazon.S3.Model;
+using AssociationRegistry.PowerBi.ExportHost;
+using AssociationRegistry.PowerBi.ExportHost.Writers;
+using AutoFixture;
+using Common.AutoFixture;
+using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using System.Text;
+using Xunit;
+
+public class HoofdactiviteitenRecordWriterTests
+{
+    private Stream _resultStream = null;
+
+    private readonly Fixture _fixture;
+    private readonly Mock<IAmazonS3> _s3ClientMock;
+
+    public HoofdactiviteitenRecordWriterTests()
+    {
+       _fixture = new Fixture().CustomizeDomain();
+       _s3ClientMock = SetupS3Client();
+    }
+
+    [Fact]
+    public async Task WithNoDocuments_ThenCsvExportsOnlyHeaders()
+    {
+        var docs = Array.Empty<PowerBiExportDocument>();
+
+        await Export(docs);
+
+        var actualResult = await GetActualResult();
+        actualResult.Should().BeEquivalentTo($"code,naam,vcode\r\n");
+    }
+
+    [Fact]
+    public async Task WithMultipleDocuments_ThenCsvExportShouldExport()
+    {
+        var docs = _fixture.CreateMany<PowerBiExportDocument>();
+
+        await Export(docs);
+
+        var actualResult = await GetActualResult();
+        var expectedResult = GetExpectedResult(docs);
+
+        actualResult.Should().BeEquivalentTo(expectedResult);
+    }
+
+    private async Task<string> GetActualResult()
+    {
+        using var reader = new StreamReader(_resultStream, Encoding.UTF8);
+
+        var content = await reader.ReadToEndAsync();
+
+        return content;
+    }
+
+    private static string GetExpectedResult(IEnumerable<PowerBiExportDocument> docs)
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append("code,naam,vcode\r\n");
+
+        foreach (var doc in docs)
+        {
+            foreach (var hoofdactiviteitVerenigingsloket in doc.HoofdactiviteitenVerenigingsloket)
+            {
+                stringBuilder.Append(
+                    $"{hoofdactiviteitVerenigingsloket.Code},{hoofdactiviteitVerenigingsloket.Naam},{doc.VCode}\r\n");
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private Mock<IAmazonS3> SetupS3Client()
+    {
+        var s3ClientMock = new Mock<IAmazonS3>();
+
+        s3ClientMock.Setup(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>(), default))
+                    .Callback<PutObjectRequest, CancellationToken>((request, _) => _resultStream = request.InputStream)
+                    .ReturnsAsync(new PutObjectResponse { HttpStatusCode = System.Net.HttpStatusCode.OK });
+
+        return s3ClientMock;
+    }
+
+    private async Task Export(IEnumerable<PowerBiExportDocument> docs)
+    {
+        var exporter = new Exporter(WellKnownFileNames.Hoofdactiviteiten,
+                                    "something",
+                                    new HoofdactiviteitenRecordWriter(),
+                                    _s3ClientMock.Object,
+                                    new NullLogger<Exporter>());
+
+        await exporter.Export(docs);
+    }
+}
+
+// namespace AssociationRegistry.Test.PowerBi.ExportHost;
 //
 // using Admin.Schema.PowerBiExport;
 // using AssociationRegistry.PowerBi.ExportHost;
