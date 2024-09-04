de6147161df012b62e637829c67e66fd9e2b60ac
emalfroy <egon.malfroy@gmail.com>
2024-09-04 13:23:25 +0200

    refactor: or-2354 extract exporters to classes power bi export

---
+++ src/AssociationRegistry.PowerBi.ExportHost/Exporters/HoofdactiviteitenExporter.cs
@@ -0,0 +1,30 @@
+﻿namespace AssociationRegistry.PowerBi.ExportHost.Exporters;
+
+using Admin.Schema.PowerBiExport;
+using Records;
+using CsvHelper;
+
+public class HoofdactiviteitenExporter
+{
+    private readonly CsvWriter _csvWriter;
+
+    public HoofdactiviteitenExporter(CsvWriter csvWriter)
+    {
+        _csvWriter = csvWriter;
+    }
+
+    public async Task Export(IEnumerable<PowerBiExportDocument> docs)
+    {
+        foreach (var vereniging in docs)
+        {
+            foreach (var hoofdactiviteitVerenigingsloket in vereniging.HoofdactiviteitenVerenigingsloket)
+            {
+                _csvWriter.WriteRecord(new HoofdactiviteitenRecord(
+                                           hoofdactiviteitVerenigingsloket.Code, hoofdactiviteitVerenigingsloket.Naam,
+                                           vereniging.VCode));
+
+                await _csvWriter.NextRecordAsync();
+            }
+        }
+    }
+}
