d0f86bde1a5584c3c771963789b11d76a08529fc
emalfroy <egon.malfroy@gmail.com>
2024-09-19 11:29:41 +0200

    test: or-2248 cleanup get all public api e2e test

--- test/AssociationRegistry.Test.E2E/When_Stop_Vereniging/Publiek_Detail_All/Returns_ArrayOfDetailResponses.cs
+++ test/AssociationRegistry.Test.E2E/When_Stop_Vereniging/Publiek_Detail_All/Returns_ArrayOfDetailResponses.cs
@@ -6,11 +6,7 @@ using FluentAssertions;
 using Framework.AlbaHost;
 using Framework.ApiSetup;
 using Framework.TestClasses;
-using KellermanSoftware.CompareNetObjects;
-using NodaTime;
 using Public.Api.Verenigingen.Detail.ResponseModels;
-using Public.ProjectionHost.Infrastructure.Extensions;
-using When_Registreer_Vereniging;
 using Xunit;
 
 [Collection(nameof(PubliekStopVerenigingCollection))]
@@ -20,20 +16,6 @@ public class Returns_ArrayOfDetailResponses(StopVerenigingContext<PublicApiSetup
 {
     protected override Func<IAlbaHost, PubliekVerenigingDetailResponse[]> GetResponse =>
         host => host.GetPubliekDetailAll(VCode);
-
-    [Fact]
-    public void With_Context()
-    {
-        Response.Single().Context.ShouldCompare("http://127.0.0.1:11004/v1/contexten/publiek/detail-vereniging-context.json");
-    }
-
-    [Fact]
-    public void With_Metadata_DatumLaatsteAanpassing()
-    {
-        Response.Single().Metadata.DatumLaatsteAanpassing.ShouldCompare(Instant.FromDateTimeOffset(DateTimeOffset.Now).ToBelgianDate(),
-                                                                        compareConfig: new ComparisonConfig
-                                                                            { MaxMillisecondsDateDifference = 5000 });
-    }
 
     [Fact]
     public void WithVereniging()
