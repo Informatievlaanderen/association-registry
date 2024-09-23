1889e4bf48618af0d39d8d8c1308760c29df58bf
emalfroy <egon.malfroy@gmail.com>
2024-09-23 16:57:33 +0200

    test: or-2438 verify writer is called in publiek get all

--- test/AssociationRegistry.Test.Public.Api/Controller/PubliekGetAllTests.cs
+++ test/AssociationRegistry.Test.Public.Api/Controller/PubliekGetAllTests.cs
@@ -4,21 +4,39 @@ using AssociationRegistry.Public.Api.Infrastructure.ConfigurationBindings;
 using AssociationRegistry.Public.Api.Queries;
 using AssociationRegistry.Public.Api.Verenigingen.Detail;
 using AssociationRegistry.Public.Schema.Detail;
+using Microsoft.AspNetCore.Http;
 using Moq;
 using Xunit;
 
 public class PubliekGetAllTests
 {
     [Fact]
-    public async Task TestController()
+    public async Task VerifyQueryIsCalled()
     {
         var cancellationToken = CancellationToken.None;
         var mock = new Mock<IQuery<IAsyncEnumerable<PubliekVerenigingDetailDocument>>>();
+
         var sut = new DetailVerenigingenController(
             new AppSettings());
 
         await sut.GetAll(mock.Object, Mock.Of<IResponseWriter>(), cancellationToken);
 
         mock.Verify(x => x.ExecuteAsync(cancellationToken), Times.Once);
+    }
+
+    [Fact]
+    public async Task VerifyWriterIsCalled()
+    {
+        var cancellationToken = CancellationToken.None;
+        var mock = new Mock<IResponseWriter>();
+
+        var sut = new DetailVerenigingenController(
+            new AppSettings());
+
+        await sut.GetAll(new Mock<IQuery<IAsyncEnumerable<PubliekVerenigingDetailDocument>>>().Object, mock.Object, cancellationToken);
+
+        mock.Verify(
+            x => x.Write(It.IsAny<HttpResponse>(), It.IsAny<IAsyncEnumerable<PubliekVerenigingDetailDocument>>(),
+                         It.IsAny<CancellationToken>()), Times.Once);
     }
 }
