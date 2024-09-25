6a8735f85a0aa1ff81a62cbe4b315c70a63650a4
emalfroy <egon.malfroy@gmail.com>
2024-09-25 14:03:27 +0200

    fix: or-2421 change givenstraatnaam back to straatnaam

--- src/AssociationRegistry/Grar/Models/AddressDetailOsloResponse.cs
+++ src/AssociationRegistry/Grar/Models/AddressDetailOsloResponse.cs
@@ -174,7 +174,7 @@ public class AdresDetailStraatnaam
     /// </summary>
     [DataMember(Name = "Straatnaam", Order = 3)]
     [JsonProperty(Required = Required.DisallowNull)]
-    public GivenDifferentStraatnaam GivenDifferentStraatnaam { get; set; }
+    public Straatnaam Straatnaam { get; set; }
 }
 
 
