43665c94c017b8e343cc8636726969458a9191e5
emalfroy <egon.malfroy@gmail.com>
2024-09-30 10:06:27 +0200

    fix: or-2440 change datacontract TeVerwijderenVereniging

--- test/AssociationRegistry.Test.E2E/Data/TeVerwijderenVereniging.cs
+++ test/AssociationRegistry.Test.E2E/Data/TeVerwijderenVereniging.cs
@@ -1,12 +1,19 @@
-﻿namespace AssociationRegistry.Public.Api.Verenigingen.Detail.ResponseModels;
+﻿namespace AssociationRegistry.Test.E2E.Data;
 
 using System.Runtime.Serialization;
 
 [DataContract]
 public class TeVerwijderenVereniging
 {
+    [DataMember(Name = "vereniging")]
+    public TeVerwijderenVerenigingData Vereniging { get; set; }
+}
+
+public class TeVerwijderenVerenigingData
+{
     [DataMember(Name = "vcode")]
     public string VCode { get; set; }
     [DataMember(Name = "teVerwijderen")]
     public bool TeVerwijderen { get; set; }
 }
+
