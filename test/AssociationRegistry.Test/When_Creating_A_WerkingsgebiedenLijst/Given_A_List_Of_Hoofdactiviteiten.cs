d4f90f1b419f7594f7f282a63039aefc67e0254f
Koen Metsu <koen@metsu.be>
2024-10-04 13:30:12 +0200

    test: or-2444 validate domain for duplicate werkingsgebieden

---
+++ test/AssociationRegistry.Test/When_Creating_A_WerkingsgebiedenLijst/Given_A_List_Of_Hoofdactiviteiten.cs
@@ -0,0 +1,26 @@
+﻿namespace AssociationRegistry.Test.When_Creating_A_WerkingsgebiedenLijst;
+
+using AssociationRegistry.Vereniging;
+using AutoFixture;
+using FluentAssertions;
+using Xunit;
+using Xunit.Categories;
+
+[UnitTest]
+public class Given_A_List_Of_Werkingsgebieden
+{
+    [Fact]
+    public void Then_It_Returns_A_Filled_WerkingsgebiedenLijst()
+    {
+        var fixture = new Fixture();
+
+        var werkingsgebieden = Werkingsgebied.All
+                                                               .OrderBy(_ => fixture.Create<int>())
+                                                               .Take(2)
+                                                               .ToArray();
+
+        var werkingsgebiedenLijst = Werkingsgebieden.FromArray(werkingsgebieden);
+
+        werkingsgebiedenLijst.Should().BeEquivalentTo(werkingsgebieden);
+    }
+}
