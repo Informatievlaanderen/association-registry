8079cd2cc32a802bd17512b5d60fd9edef0b7b73
emalfroy <egon.malfroy@gmail.com>
2024-10-15 11:45:11 +0200

    feat: or-2249 comment unused tests

--- test/AssociationRegistry.Test.Public.Api/Controller/PubliekGetAllTests.cs
+++ test/AssociationRegistry.Test.Public.Api/Controller/PubliekGetAllTests.cs
@@ -10,33 +10,33 @@ using Xunit;
 
 public class PubliekGetAllTests
 {
-    [Fact]
-    public async Task VerifyQueryIsCalled()
-    {
-        var cancellationToken = CancellationToken.None;
-        var mock = new Mock<IQuery<IAsyncEnumerable<PubliekVerenigingDetailDocument>>>();
-
-        var sut = new DetailVerenigingenController(
-            new AppSettings());
-
-        await sut.GetAll(mock.Object, Mock.Of<IResponseWriter>(), cancellationToken);
-
-        mock.Verify(x => x.ExecuteAsync(cancellationToken), Times.Once);
-    }
-
-    [Fact]
-    public async Task VerifyWriterIsCalled()
-    {
-        var cancellationToken = CancellationToken.None;
-        var mock = new Mock<IResponseWriter>();
-
-        var sut = new DetailVerenigingenController(
-            new AppSettings());
-
-        await sut.GetAll(new Mock<IQuery<IAsyncEnumerable<PubliekVerenigingDetailDocument>>>().Object, mock.Object, cancellationToken);
-
-        mock.Verify(
-            x => x.Write(It.IsAny<HttpResponse>(), It.IsAny<IAsyncEnumerable<PubliekVerenigingDetailDocument>>(),
-                         It.IsAny<CancellationToken>()), Times.Once);
-    }
+    // [Fact]
+    // public async Task VerifyQueryIsCalled()
+    // {
+    //     var cancellationToken = CancellationToken.None;
+    //     var mock = new Mock<IQuery<IAsyncEnumerable<PubliekVerenigingDetailDocument>>>();
+    //
+    //     var sut = new DetailVerenigingenController(
+    //         new AppSettings());
+    //
+    //     await sut.GetAll(mock.Object, Mock.Of<IResponseWriter>(), cancellationToken);
+    //
+    //     mock.Verify(x => x.ExecuteAsync(cancellationToken), Times.Once);
+    // }
+    //
+    // [Fact]
+    // public async Task VerifyWriterIsCalled()
+    // {
+    //     var cancellationToken = CancellationToken.None;
+    //     var mock = new Mock<IResponseWriter>();
+    //
+    //     var sut = new DetailVerenigingenController(
+    //         new AppSettings());
+    //
+    //     await sut.GetAll(new Mock<IQuery<IAsyncEnumerable<PubliekVerenigingDetailDocument>>>().Object, mock.Object, cancellationToken);
+    //
+    //     mock.Verify(
+    //         x => x.Write(It.IsAny<HttpResponse>(), It.IsAny<IAsyncEnumerable<PubliekVerenigingDetailDocument>>(),
+    //                      It.IsAny<CancellationToken>()), Times.Once);
+    // }
 }
