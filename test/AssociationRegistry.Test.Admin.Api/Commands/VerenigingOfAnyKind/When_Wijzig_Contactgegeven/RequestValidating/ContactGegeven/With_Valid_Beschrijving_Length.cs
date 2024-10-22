04220da8b38129b34f73f11c58107d8292bea5b3
emalfroy <egon.malfroy@gmail.com>
2024-10-22 09:00:13 +0200

    feat: or-2468 change limit beschrijving contactgegeven to 128 char

---
+++ test/AssociationRegistry.Test.Admin.Api/Commands/VerenigingOfAnyKind/When_Wijzig_Contactgegeven/RequestValidating/ContactGegeven/With_Valid_Beschrijving_Length.cs
@@ -0,0 +1,28 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.Commands.VerenigingOfAnyKind.When_Wijzig_Contactgegeven.RequestValidating.ContactGegeven;
+
+using AssociationRegistry.Admin.Api.Verenigingen.Contactgegevens.FeitelijkeVereniging.WijzigContactgegeven;
+using AssociationRegistry.Admin.Api.Verenigingen.Contactgegevens.FeitelijkeVereniging.WijzigContactgegeven.RequestModels;
+using FluentValidation.TestHelper;
+using Framework;
+using Xunit;
+
+public class With_Valid_Beschrijving_Length : ValidatorTest
+{
+    [Fact]
+    public void Has_validation_error__contactgegeven_moet_een_waarde_bevatten()
+    {
+        var validator = new WijzigContactgegevenValidator();
+
+        var request = new WijzigContactgegevenRequest
+        {
+            Contactgegeven = new TeWijzigenContactgegeven()
+            {
+                Beschrijving = new string('A', 128),
+            },
+        };
+
+        var result = validator.TestValidate(request);
+
+        result.ShouldNotHaveValidationErrorFor(toeRequest => toeRequest.Contactgegeven.Beschrijving);
+    }
+}
