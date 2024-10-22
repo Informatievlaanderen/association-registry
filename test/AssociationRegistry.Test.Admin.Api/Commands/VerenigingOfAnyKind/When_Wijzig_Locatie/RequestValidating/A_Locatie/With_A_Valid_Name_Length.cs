6fc1213fa0a65130b83c7c83005a4d2bd83b4c2c
emalfroy <egon.malfroy@gmail.com>
2024-10-22 08:38:05 +0200

    feat: or-2468 change limit locatie naam to 128 char

---
+++ test/AssociationRegistry.Test.Admin.Api/Commands/VerenigingOfAnyKind/When_Wijzig_Locatie/RequestValidating/A_Locatie/With_A_Valid_Name_Length.cs
@@ -0,0 +1,24 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.Commands.VerenigingOfAnyKind.When_Wijzig_Locatie.RequestValidating.A_Locatie;
+
+using AssociationRegistry.Admin.Api.Verenigingen.Locaties.FeitelijkeVereniging.WijzigLocatie;
+using AssociationRegistry.Admin.Api.Verenigingen.Locaties.FeitelijkeVereniging.WijzigLocatie.RequestModels;
+using AssociationRegistry.Test.Admin.Api.Framework;
+using FluentValidation.TestHelper;
+using Xunit;
+using Xunit.Categories;
+
+[UnitTest]
+public class With_A_Valid_Name_Length : ValidatorTest
+{
+    [Fact]
+    public void Has_no_validation_errors()
+    {
+        var validator = new WijzigLocatieRequestValidator();
+        var request = new WijzigLocatieRequest { Locatie = new TeWijzigenLocatie() };
+        request.Locatie.Naam = new string('A', 128);
+
+        var result = validator.TestValidate(request);
+
+        result.ShouldNotHaveValidationErrorFor(x => x.Locatie.Naam);
+    }
+}
