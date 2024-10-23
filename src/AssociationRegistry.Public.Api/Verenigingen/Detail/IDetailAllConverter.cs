6c2fea2e9e6a1bc5248b938a6cb669c540ccc640
emalfroy <egon.malfroy@gmail.com>
2024-10-23 10:34:51 +0200

    refactor: or-2249 introduce DetailAllConverter for serializing verenigingen for writing to stream

---
+++ src/AssociationRegistry.Public.Api/Verenigingen/Detail/IDetailAllConverter.cs
@@ -0,0 +1,65 @@
+﻿namespace AssociationRegistry.Public.Api.Verenigingen.Detail;
+
+using Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Formatters.Json;
+using Infrastructure.ConfigurationBindings;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using Newtonsoft.Json;
+using Schema.Constants;
+using Schema.Detail;
+
+public interface IDetailAllConverter
+{
+    string SerializeToJson(PubliekVerenigingDetailDocument publiekVerenigingDetailDocument);
+}
+
+public class DetailAllConverter : IDetailAllConverter
+{
+    private readonly AppSettings _appSettings;
+    private readonly JsonSerializerSettings _serializerSettings;
+
+    public DetailAllConverter(AppSettings appSettings)
+    {
+        _appSettings = appSettings;
+        _serializerSettings = JsonSerializerSettingsProvider.CreateSerializerSettings().ConfigureDefaultForApi();
+    }
+
+    public string SerializeToJson(PubliekVerenigingDetailDocument vereniging)
+    {
+        if (IsTeVerwijderenVereniging(vereniging))
+        {
+            return
+                JsonConvert.SerializeObject(
+                    new TeVerwijderenVereniging
+                    {
+                        Vereniging = new TeVerwijderenVereniging.TeVerwijderenVerenigingData
+                        {
+                            VCode = vereniging.VCode,
+                            TeVerwijderen = true,
+                            DeletedAt = vereniging.DatumLaatsteAanpassing,
+                        },
+                    },
+                    _serializerSettings);
+        }
+
+        var mappedVereniging = PubliekVerenigingDetailMapper.MapDetailAll(vereniging, _appSettings);
+        return JsonConvert.SerializeObject(mappedVereniging, _serializerSettings);
+    }
+
+    private static bool IsTeVerwijderenVereniging(PubliekVerenigingDetailDocument vereniging)
+        => vereniging.Deleted ||
+           vereniging.Status == VerenigingStatus.Gestopt ||
+           vereniging.IsUitgeschrevenUitPubliekeDatastroom is not null &&
+           vereniging.IsUitgeschrevenUitPubliekeDatastroom.Value;
+
+    public class TeVerwijderenVereniging
+    {
+        public TeVerwijderenVerenigingData Vereniging { get; set; }
+
+        public class TeVerwijderenVerenigingData
+        {
+            public string VCode { get; set; }
+            public bool TeVerwijderen { get; set; }
+            public string DeletedAt { get; set; }
+        }
+    }
+}
---
+++ test/AssociationRegistry.Test.Public.Api/DetailAllStreamWriterTests.cs
@@ -0,0 +1,65 @@
+﻿namespace AssociationRegistry.Test.Public.Api;
+
+using AssociationRegistry.Public.Api.Verenigingen.Detail;
+using AssociationRegistry.Public.Schema.Detail;
+using AutoFixture;
+using FluentAssertions;
+using Framework;
+using Moq;
+using Xunit;
+
+public class DetailAllStreamWriterTests
+{
+    private readonly Fixture _fixture;
+
+    public DetailAllStreamWriterTests()
+    {
+        _fixture = new Fixture().CustomizePublicApi();
+    }
+
+    [Fact]
+    public async Task WithEmptyData_ReturnsEmptyStream()
+    {
+        var sut = new DetailAllStreamWriter(Mock.Of<IDetailAllConverter>());
+
+        var stream = await sut.WriteAsync(GetData([]), CancellationToken.None);
+
+        using var reader = new StreamReader(stream);
+
+        var streamData = await reader.ReadToEndAsync();
+        streamData.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public async Task WithData_ReturnsStreamWithData()
+    {
+        var publiekVerenigingDetailDocument = _fixture.Create<PubliekVerenigingDetailDocument>();
+        var mock = new Mock<IDetailAllConverter>();
+
+        var mappedJson = _fixture.Create<string>();
+
+        mock.Setup(s => s.SerializeToJson(publiekVerenigingDetailDocument))
+            .Returns(mappedJson);
+
+        var sut = new DetailAllStreamWriter(mock.Object);
+
+        var stream = await sut.WriteAsync(GetData([publiekVerenigingDetailDocument]), CancellationToken.None);
+
+        using var reader = new StreamReader(stream);
+
+        var streamData = await reader.ReadToEndAsync();
+        streamData.Should().BeEquivalentTo(mappedJson + Environment.NewLine);
+    }
+
+    private async IAsyncEnumerable<PubliekVerenigingDetailDocument> GetData(IEnumerable<PubliekVerenigingDetailDocument> data)
+    {
+        var docs = data;
+
+        foreach (var doc in docs)
+        {
+            yield return doc;
+        }
+    }
+}
+
+
