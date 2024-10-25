22722432f0cb7ca0a4a6bd7f76293d32d0632271
emalfroy <egon.malfroy@gmail.com>
2024-10-25 13:39:10 +0200

    refactor: or-2513 move class to outer scope

--- src/AssociationRegistry/Events/AdresMatchUitAdressenregister.cs
+++ src/AssociationRegistry/Events/AdresMatchUitAdressenregister.cs
@@ -1,7 +1,6 @@
 ﻿namespace AssociationRegistry.Events;
 
 using Grar.Models;
-using System.Text.RegularExpressions;
 
 public record AdresMatchUitAdressenregister
 {
@@ -18,26 +17,11 @@ public record AdresMatchUitAdressenregister
                 "België"),
         };
 
-    public class GemeenteNaamSuffixCleanerRegex
-    {
-        private readonly Regex _regex;
-        private const string RemoveBracketsAndContent = "\\(.*?\\)";
-
-        public GemeenteNaamSuffixCleanerRegex()
-        {
-           _regex = new Regex(RemoveBracketsAndContent);
-        }
-
-        public string Clean(string gemeentenaam)
-            => _regex.Replace(gemeentenaam, "").Trim();
-    }
-
-    // hekelgem (affligem)
     public AdresMatchUitAdressenregister DecorateWithPostalInformation(string origineleGemeentenaam, PostalInformationResponse? postalInformationResponse)
     {
         if (postalInformationResponse is null) return this;
 
-        var origineleGemeenteNaamClean = new Regex("\\(.*?\\)").Replace(origineleGemeentenaam, "").Trim();
+        var origineleGemeenteNaamClean = GemeenteNaamSuffixCleanerRegex.Instance.Clean(origineleGemeentenaam);
 
         if (postalInformationResponse.Postnamen.Length == 1)
         {
