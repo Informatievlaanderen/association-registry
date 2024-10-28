6fc63c7f9dd8113025412b1b33cbcda3931f9056
emalfroy <egon.malfroy@gmail.com>
2024-10-28 08:51:53 +0100

    chore: or-2513 remove unused files

--- src/AssociationRegistry.Public.Api/Verenigingen/Detail/IDetailAllFileWrapper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AssociationRegistry.Public.Api.Verenigingen.Detail;
-
-public interface IDetailAllFileWrapper
-{
-    public Task<string> GetPreSignedUrlAsync(CancellationToken cancellationToken);
-    public Task PutAsync(Stream stream, CancellationToken cancellationToken);
-}
