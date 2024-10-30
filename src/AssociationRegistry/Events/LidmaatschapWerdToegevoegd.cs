06796162388160b69a7388ca469e7317f9547894
Jan Lesage <jan.lesage@vlaanderen.be>
2024-10-30 10:49:47 +0100

    fix: or-2514 make datamember descriptions more uniform

--- src/AssociationRegistry/Events/LidmaatschapWerdToegevoegd.cs
+++ src/AssociationRegistry/Events/LidmaatschapWerdToegevoegd.cs
@@ -7,10 +7,6 @@ using Vereniging.Bronnen;
 
 public record LidmaatschapWerdToegevoegd(Registratiedata.Lidmaatschap Lidmaatschap) : IEvent
 {
-    [IgnoreDataMember]
-    public Bron Bron
-        => Bron.Initiator;
-
     public static LidmaatschapWerdToegevoegd With(Lidmaatschap lidmaatschap)
         => new(
             Registratiedata.Lidmaatschap.With(lidmaatschap)
