e8a35986dfc2d1728b4f7f055b68555c61c6ccc3
Jan Lesage <jan.lesage@vlaanderen.be>
2024-10-30 14:52:21 +0100

    fix: or-2516 use HttpDelete for verwijder lidmaatschap

--- src/AssociationRegistry.Admin.Api/Verenigingen/Lidmaatschap/VerwijderLidmaatschap/VerwijderLidmaatschapController.cs
+++ src/AssociationRegistry.Admin.Api/Verenigingen/Lidmaatschap/VerwijderLidmaatschap/VerwijderLidmaatschapController.cs
@@ -54,7 +54,7 @@ public class VerwijderLidmaatschapController : ApiController
     /// <response code="400">Er was een probleem met de doorgestuurde waarden.</response>
     /// <response code="412">De gevraagde vereniging heeft niet de verwachte sequentiewaarde.</response>
     /// <response code="500">Er is een interne fout opgetreden.</response>
-    [HttpPatch("{vCode}/lidmaatschappen")]
+    [HttpDelete("{vCode}/lidmaatschappen")]
     [ConsumesJson]
     [ProducesJson]
     [SwaggerRequestExample(typeof(VerwijderLidmaatschapRequest), typeof(VerwijderLidmaatschapRequestExamples))]
