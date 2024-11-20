a2e8f5b5c5f1111dc2fa61ccf0dace36b1ba7a7a
Jan Lesage <jan.lesage@vlaanderen.be>
2024-11-20 09:30:41 +0100

    test: or-2558 add tests for registreer feitelijke vereniging command handling

---
+++ test/AssociationRegistry.Test.Admin.Api/Commands/FeitelijkeVereniging/When_RegistreerFeitelijkeVereniging/RequestValidating/A_WerkingsgebiedenLijst/Is_NVT.cs
@@ -0,0 +1,43 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.Commands.FeitelijkeVereniging.When_RegistreerFeitelijkeVereniging.RequestValidating.
+    A_WerkingsgebiedenLijst;
+
+using AssociationRegistry.Admin.Api.Verenigingen.Registreer.FeitelijkeVereniging.RequetsModels;
+using AssociationRegistry.Admin.Api.Verenigingen.WijzigBasisgegevens.FeitelijkeVereniging.RequestModels;
+using FluentValidation.TestHelper;
+using Test.Framework;
+using Xunit;
+using Xunit.Categories;
+using ValidatorTest = Framework.ValidatorTest;
+
+[UnitTest]
+public class Is_NVT : ValidatorTest
+{
+    [Fact]
+    public void Has_no_validation_errors_when_list_has_no_other_entries()
+    {
+        var validator = new RegistreerFeitelijkeVerenigingRequestValidator(new ClockStub(DateOnly.MaxValue));
+
+        var result = validator.TestValidate(new RegistreerFeitelijkeVerenigingRequest
+        {
+            Werkingsgebieden = ["NVT"],
+        });
+
+        result.ShouldNotHaveValidationErrorFor(vereniging => vereniging.Werkingsgebieden);
+    }
+
+    [Fact]
+    public void Has_validation_errors_when_list_has_more_entries()
+    {
+        var validator = new WijzigBasisgegevensRequestValidator();
+
+        var result = validator.TestValidate(new WijzigBasisgegevensRequest
+        {
+            Werkingsgebieden = ["NVT", "BE25"],
+        });
+
+        result.ShouldHaveValidationErrorFor(vereniging => vereniging.Werkingsgebieden)
+              .WithErrorMessage("De waarde NVT in de werkingsgebiedenLijst mag niet met andere waarden gecombineerd worden.");
+
+        ;
+    }
+}
