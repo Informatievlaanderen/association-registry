13eb1b3dbd3a6766d553b7cdfb73929ee595e5c4
Jan Lesage <jan.lesage@vlaanderen.be>
2024-11-20 08:47:08 +0100

    fix: or-2558 use when clause in RegistreerFeitelijkeVerenigingRequestValidator to simplify validator

---
+++ test/AssociationRegistry.Test/Werkingsgebied/WerkingsgebiedenTests.cs
@@ -0,0 +1,21 @@
+﻿namespace AssociationRegistry.Test.Werkingsgebied;
+
+using FluentAssertions;
+using Vereniging;
+using Xunit;
+
+public class WerkingsgebiedenTests
+{
+    [Fact]
+    public void Werkingsgebieden_NietBepaald_Is_Empty()
+    {
+        Werkingsgebieden.NietBepaald.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Werkingsgebieden_NietVanToepassing_Is_Not_Empty()
+    {
+        Werkingsgebieden.NietVanToepassing.Should().NotBeEmpty();
+        Werkingsgebieden.NietVanToepassing.Should().Contain(Werkingsgebied.Create("NVT"));
+    }
+}
