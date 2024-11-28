554d46bf12f9ceff506b8b14f010a2c1d79dc858
Jan Lesage <jan.lesage@vlaanderen.be>
2024-11-28 16:34:36 +0100

    refactor: or-2472 use adres id inside merger handler instead of the complete message

--- test/AssociationRegistry.Test.Admin.Api/GrarSync/When_Consuming_Merger_Events/AdresMergerHandler/Given_No_Locations_Found.cs
+++ test/AssociationRegistry.Test.Admin.Api/GrarSync/When_Consuming_Merger_Events/AdresMergerHandler/Given_No_Locations_Found.cs
@@ -11,22 +11,21 @@ using Xunit;
 
 public class Given_No_Locations_Found
 {
-
     [Fact]
     public async Task Then_It_Does_Not_Queue_Anything()
     {
         var fixture = new Fixture().CustomizeAdminApi();
-        var message = fixture.Create<AddressWasRetiredBecauseOfMunicipalityMerger>();
+        var adresId = fixture.Create<int>();
 
         var sqsClientWrapperMock = new Mock<ISqsClientWrapper>();
         var teHeradresserenLocatiesFinder = new Mock<ITeHeradresserenLocatiesFinder>();
 
-        teHeradresserenLocatiesFinder.Setup(s => s.Find(message.AddressPersistentLocalId))
+        teHeradresserenLocatiesFinder.Setup(s => s.Find(adresId))
                                      .ReturnsAsync([]);
 
         var sut = new AdresMergerHandler(sqsClientWrapperMock.Object, teHeradresserenLocatiesFinder.Object);
 
-        await sut.Handle(message);
+        await sut.Handle(adresId);
 
         sqsClientWrapperMock.Verify(v => v.QueueReaddressMessage(It.IsAny<TeHeradresserenLocatiesMessage>()), Times.Never());
     }
