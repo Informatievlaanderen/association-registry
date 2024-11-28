37ec9643cbeaeb3e705f455360c96042d8de7de7
emalfroy <egon.malfroy@gmail.com>
2024-11-28 12:06:27 +0100

    feat: or-2472 implement AddressMergerHandler

---
+++ src/AssociationRegistry.Admin.Api/GrarSync/AddressMergerHandler.cs
@@ -0,0 +1,33 @@
+﻿namespace AssociationRegistry.Admin.Api.GrarSync;
+
+using Be.Vlaanderen.Basisregisters.GrAr.Contracts.AddressRegistry;
+using Grar.HeradresseerLocaties;
+using Infrastructure.AWS;
+
+public class AdresMergerHandler
+{
+    private readonly ISqsClientWrapper _sqsClientWrapper;
+    private readonly ITeHeradresserenLocatiesFinder _teHeradresserenLocatiesFinder;
+
+    public AdresMergerHandler(ISqsClientWrapper sqsClientWrapper, ITeHeradresserenLocatiesFinder teHeradresserenLocatiesFinder)
+    {
+        _sqsClientWrapper = sqsClientWrapper;
+        _teHeradresserenLocatiesFinder = teHeradresserenLocatiesFinder;
+    }
+
+    public async Task Handle(AddressWasRetiredBecauseOfMunicipalityMerger addressWasRetiredMessage)
+    {
+        var messages = await _teHeradresserenLocatiesFinder.Find(addressWasRetiredMessage.AddressPersistentLocalId);
+
+        foreach (var message in messages)
+        {
+            await _sqsClientWrapper.QueueReaddressMessage(message);
+        }
+    }
+}
+
+public interface ITeHeradresserenLocatiesFinder
+{
+    Task<TeHeradresserenLocatiesMessage[]> Find(int messageAddressPersistentLocalId);
+}
+
