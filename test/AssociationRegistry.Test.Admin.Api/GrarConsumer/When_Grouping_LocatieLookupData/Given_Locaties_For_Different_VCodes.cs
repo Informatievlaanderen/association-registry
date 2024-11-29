b8eeb21344b117ff0327eb9be6ee720a2d3de36c
Koen Metsu <koen@metsu.be>
2024-11-29 14:12:28 +0100

    test: or-2472 improve readability

--- test/AssociationRegistry.Test.Admin.Api/GrarConsumer/When_Grouping_LocatieLookupData/Given_Locaties_For_Different_VCodes.cs
+++ test/AssociationRegistry.Test.Admin.Api/GrarConsumer/When_Grouping_LocatieLookupData/Given_Locaties_For_Different_VCodes.cs
@@ -5,7 +5,10 @@ using AssociationRegistry.Admin.Api.GrarConsumer.Groupers;
 using AssociationRegistry.Grar.GrarConsumer.TeHeradresserenLocaties;
 using AssociationRegistry.Test.Common.AutoFixture;
 using AutoFixture;
+using Be.Vlaanderen.Basisregisters.Utilities;
 using FluentAssertions;
+using Grar.Models;
+using JasperFx.Core;
 using Xunit;
 
 public class Given_Locaties_For_Different_VCodes
@@ -15,52 +18,69 @@ public class Given_Locaties_For_Different_VCodes
     {
         var fixture = new Fixture().CustomizeAdminApi();
         var destinationAdresId = fixture.Create<int>();
-        var stringDestinationAdresId = destinationAdresId.ToString();
-        var sourceAdresId = fixture.Create<int>().ToString();
 
-        var vCode1 = fixture.Create<string>();
-        var vCode2 = fixture.Create<string>();
-
-        var locatieLookupData = new[]
-        {
-            fixture.Create<LocatieLookupData>() with
-            {
-                VCode = vCode1,
-                AdresId = sourceAdresId,
-                LocatieId = 1
-            },
-            fixture.Create<LocatieLookupData>() with
-            {
-                VCode = vCode2,
-                AdresId = sourceAdresId,
-                LocatieId = 1
-            },
-            fixture.Create<LocatieLookupData>() with
-            {
-                VCode = vCode1,
-                AdresId = sourceAdresId,
-                LocatieId = 2
-            },
-        };
+        var locatieLookupData = new LocatieLookupTestData(fixture, fixture.Create<string>());
 
         var actual = LocatiesVolgensVCodeGrouper.Group(locatieLookupData, destinationAdresId);
 
         actual.Should().BeEquivalentTo(
+        [
+            new TeHeradresserenLocatiesMessage(
+                locatieLookupData.VCode1,
+                locatieLookupData.For(locatieLookupData.VCode1)
+                                 .Select(x => new TeHeradresserenLocatie(x.LocatieId, destinationAdresId.ToString()))
+                                 .ToList(),
+                ""),
+            new TeHeradresserenLocatiesMessage(
+                locatieLookupData.VCode2,
+                locatieLookupData.For(locatieLookupData.VCode2)
+                                 .Select(x => new TeHeradresserenLocatie(x.LocatieId, destinationAdresId.ToString()))
+                                 .ToList(),
+                ""),
+        ]);
+    }
+
+    public class LocatieLookupTestData : List<LocatieLookupData>
+    {
+        public string VCode1 => "1";
+        public string VCode2 => "2";
+
+        public LocatieLookupTestData For(string vCode)
+        {
+            return new(this.Where(x => x.VCode == vCode)
+                           .ToArray());
+        }
+
+        public TeHeradresserenLocatie[] MapLocatieData(Func<LocatieLookupData, TeHeradresserenLocatie> map)
+            => this.Select(map).ToArray();
+
+        private LocatieLookupTestData(IEnumerable<LocatieLookupData> data) : base(data)
+        {
+        }
+
+        public LocatieLookupTestData(IFixture fixture, string sourceAdresId)
+        {
+            AddRange(
             [
-                new TeHeradresserenLocatiesMessage(
-                    vCode1,
-                    [
-                        new(1, stringDestinationAdresId),
-                        new(2, stringDestinationAdresId),
-                    ]
-                   ,
-                    ""),
-                new TeHeradresserenLocatiesMessage(
-                    vCode2,
-                    [
-                        new(1, stringDestinationAdresId),
-                    ],
-                    ""),
+                fixture.Create<LocatieLookupData>() with
+                {
+                    VCode = VCode1,
+                    AdresId = sourceAdresId,
+                    LocatieId = 1
+                },
+                fixture.Create<LocatieLookupData>() with
+                {
+                    VCode = VCode2,
+                    AdresId = sourceAdresId,
+                    LocatieId = 1
+                },
+                fixture.Create<LocatieLookupData>() with
+                {
+                    VCode = VCode1,
+                    AdresId = sourceAdresId,
+                    LocatieId = 2
+                }
             ]);
+        }
     }
 }
