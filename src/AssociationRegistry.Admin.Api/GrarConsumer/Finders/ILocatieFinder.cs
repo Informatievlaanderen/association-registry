f0aa08fbfc885ea6f6e345597d14309488768db9
Koen Metsu <koen@metsu.be>
2024-11-29 15:53:08 +0100

    wip: or-2472 group inside LocatieFinder

--- src/AssociationRegistry.Admin.Api/GrarConsumer/Finders/ILocatieFinder.cs
+++ src/AssociationRegistry.Admin.Api/GrarConsumer/Finders/ILocatieFinder.cs
@@ -5,5 +5,5 @@ using Schema.Detail;
 public interface ILocatieFinder
 {
     Task<IQueryable<LocatieLookupDocument>> FindLocaties(params string[] adresIds);
-    Task<LocatieMetVCode[]> FindLocaties(params int[] adresIds);
+    Task<LocatieIdsPerVCode> FindLocaties(params int[] adresIds);
 }
