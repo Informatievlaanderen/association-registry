d11f61991cfffa93c934db7b62fda37eece91384
Jan Lesage <jan.lesage@vlaanderen.be>
2024-12-04 16:38:38 +0100

    wip: or-2596 rewrite localstack shell script for sqs init

--- .localstack/init/ready.d/01_sqs-init.sh
+++ .localstack/init/ready.d/01_sqs-init.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
-awslocal s3api create-bucket --bucket verenigingsregister-uwp-data
-
 # Set variables
 REGION="us-east-1"
 ACCOUNT_ID="000000000000" # Default account ID used by LocalStack
 MAX_RECEIVE_COUNT=3
+QUEUE_PART=""
 
-# DLQ names
-DLQ_NAME="verenigingsregister-addressmatch-dlq"
+with_dlq()
+{
+  QUEUE_PART=$@
+  QUEUE_NAME="verenigingsregister-$@"
+  DLQ_NAME="$QUEUE_NAME-dlq"
+  DLQ_ARN="arn:aws:sqs:$REGION:$ACCOUNT_ID:$DLQ_NAME"
 
-# Construct DLQ ARNs
-DLQ_ARN="arn:aws:sqs:$REGION:$ACCOUNT_ID:$DLQ_NAME"
+  awslocal sqs create-queue --region $REGION --queue-name $QUEUE_NAME
+  awslocal sqs create-queue --region $REGION --queue-name $DLQ_NAME
+  awslocal sqs set-queue-attributes \
+    --queue-url http://sqs.$REGION.localhost.localstack.cloud:4566/000000000000/$QUEUE_NAME \
+    --attributes "{
+      \"RedrivePolicy\": \"{ \\\"maxReceiveCount\\\": \\\"3\\\", \\\"deadLetterTargetArn\\\": \\\"arn:aws:sqs:$REGION:000000000000:$DLQ_NAME\\\" }\"
+    }"
+}
 
-# Create DLQs
-awslocal sqs create-queue --region $REGION --queue-name $DLQ_NAME
-
-awslocal sqs create-queue --region $REGION --queue-name verenigingsregister-addressmatch --attribute file:///etc/localstack/init/ready.d/01_sqs-init.redrive.json
+with_dlq addressmatch
+with_dlq addressmatch-e2e
+with_dlq grarsync
+with_dlq grarsync-e2e
