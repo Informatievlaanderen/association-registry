2c151f9c85b36904bf00f75892d9fe90990b8ab9
Koen Metsu <koen@metsu.be>
2024-12-05 15:39:19 +0100

    chore: or-2595 remove unused code and comments

--- src/AssociationRegistry.Admin.Api/Infrastructure/AWS/SqsClientWrapper.cs
+++ src/AssociationRegistry.Admin.Api/Infrastructure/AWS/SqsClientWrapper.cs
@@ -1,7 +1,6 @@
 namespace AssociationRegistry.Admin.Api.Infrastructure.AWS;
 
 using Acties.GrarConsumer.HeradresseerLocaties;
-using Amazon.SQS;
 using Framework;
 using Grar.GrarUpdates.Fusies.TeHeradresserenLocaties;
 using Hosts.Configuration;
@@ -12,17 +11,11 @@ using Wolverine;
 
 public class SqsClientWrapper : ISqsClientWrapper
 {
-    private readonly IAmazonSQS _sqsClient;
     private readonly IMessageBus _messageBus;
-    private readonly string _kboSyncQueueUrl;
-    private readonly string _readdressQueueUrl;
 
-    public SqsClientWrapper(IAmazonSQS sqsClient, AppSettings appSettings, GrarOptions grarOptions, IMessageBus messageBus)
+    public SqsClientWrapper(IMessageBus messageBus)
     {
-        _sqsClient = sqsClient;
         _messageBus = messageBus;
-        _kboSyncQueueUrl = appSettings.KboSyncQueueUrl;
-        _readdressQueueUrl = grarOptions.Sqs.GrarSyncQueueUrl;
     }
 
     public async Task QueueReaddressMessage(HeradresseerLocatiesMessage message)
@@ -32,9 +25,6 @@ public class SqsClientWrapper : ISqsClientWrapper
 
     public async Task QueueMessage<TMessage>(TMessage message)
     {
-        // await _sqsClient.SendMessageAsync(
-        //     _readdressQueueUrl,
-        //     JsonSerializer.Serialize(message));
         await _messageBus.SendAsync(message);
     }
 
