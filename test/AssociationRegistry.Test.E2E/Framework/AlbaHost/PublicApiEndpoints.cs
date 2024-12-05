ec10eee9f2ac9acd921877c09fc560f77df2330d
Koen Metsu <koen@metsu.be>
2024-12-05 13:53:00 +0100

    Revert "test: or-2595 debug with exception"
    This reverts commit 572c2ff0399528dac5bafd32d91c97cb4db2785c.

--- test/AssociationRegistry.Test.E2E/Framework/AlbaHost/PublicApiEndpoints.cs
+++ test/AssociationRegistry.Test.E2E/Framework/AlbaHost/PublicApiEndpoints.cs
@@ -20,23 +20,12 @@ public static class PublicApiEndpoints
     public static JObject[] GetPubliekDetailAll(this IAlbaHost source)
     {
         var locationHeader = GetDetailAllLocationHeader(source);
-        using var handler = new HttpClientHandler();
-        using var s3HttpClient = new HttpClient(handler);
 
-        handler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
+        var s3Response = GetS3Response(locationHeader);
 
-        try
-        {
-            var s3Response = s3HttpClient.GetAsync(locationHeader).GetAwaiter().GetResult();
+        var responseContent = s3Response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
-            var responseContent = s3Response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-
-            return ParseResponse(responseContent);
-        }
-        catch (Exception e)
-        {
-            throw new Exception($"{e.Message}: (locationheader: {locationHeader}, handler baseaddress {s3HttpClient.BaseAddress}.");
-        }
+        return ParseResponse(responseContent);
     }
 
     private static JObject[] ParseResponse(string responseContent)
@@ -46,7 +35,6 @@ public static class PublicApiEndpoints
 
         var verenigingen = new List<JObject>();
         var reader = new StringReader(responseContent);
-
         while (reader.Peek() != -1)
         {
             var line = reader.ReadLine();
@@ -56,11 +44,19 @@ public static class PublicApiEndpoints
         return verenigingen.ToArray();
     }
 
+    private static HttpResponseMessage GetS3Response(Uri? locationHeader)
+    {
+        using var handler = new HttpClientHandler();
+        handler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
+
+        using var s3HttpClient = new HttpClient(handler);
+        return s3HttpClient.GetAsync(locationHeader).GetAwaiter().GetResult();
+    }
+
     private static Uri? GetDetailAllLocationHeader(IAlbaHost source)
     {
         var client = source.Server.CreateClient();
         var response = client.GetAsync($"/v1/verenigingen/detail/all").GetAwaiter().GetResult();
-
         return response.Headers.Location;
     }
 }
