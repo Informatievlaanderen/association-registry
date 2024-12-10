babc5be30876fbe5b78aa434e82846910f8b60c3
emalfroy <egon.malfroy@gmail.com>
2024-12-10 10:53:57 +0100

    test: or-2334 write e2e test for markeer als dubbel van

---
+++ test/AssociationRegistry.Test.E2E/Scenarios/Requests/FeitelijkeVereniging/MarkeerAlsDubbelVanRequestFactory.cs
@@ -0,0 +1,41 @@
+namespace AssociationRegistry.Test.E2E.Scenarios.Requests.FeitelijkeVereniging;
+
+using Alba;
+using Framework.ApiSetup;
+using Givens.FeitelijkeVereniging;
+using Marten.Events;
+using System.Net;
+using Vereniging;
+using When_Markeer_Als_Dubbel_Van;
+
+public class MarkeerAlsDubbelVanRequestFactory : ITestRequestFactory<MarkeerAlsDubbelVanRequest>
+{
+    private readonly MultipleWerdGeregistreerdScenario _scenario;
+
+    public MarkeerAlsDubbelVanRequestFactory(MultipleWerdGeregistreerdScenario scenario)
+    {
+        _scenario = scenario;
+    }
+
+    public async Task<RequestResult<MarkeerAlsDubbelVanRequest>> ExecuteRequest(IApiSetup apiSetup)
+    {
+        var request = new MarkeerAlsDubbelVanRequest
+        {
+            IsDubbelVan = _scenario.AndereFeitelijkeVerenigingWerdGeregistreerd.VCode,
+        };
+
+        await apiSetup.AdminApiHost.Scenario(s =>
+        {
+            s.Post
+             .Json(request, JsonStyle.Mvc)
+             .ToUrl($"/v1/verenigingen/{_scenario.FeitelijkeVerenigingWerdGeregistreerd.VCode}/dubbelVan");
+
+            s.StatusCodeShouldBe(HttpStatusCode.Accepted);
+        });
+
+        await apiSetup.AdminProjectionHost.WaitForNonStaleProjectionDataAsync(TimeSpan.FromSeconds(60));
+        await apiSetup.PublicProjectionHost.WaitForNonStaleProjectionDataAsync(TimeSpan.FromSeconds(60));
+
+        return new RequestResult<MarkeerAlsDubbelVanRequest>(VCode.Create(_scenario.FeitelijkeVerenigingWerdGeregistreerd.VCode), request);
+    }
+}
---
+++ test/AssociationRegistry.Test.E2E/When_Markeer_Als_Dubbel_Van/MarkeerAlsDubbelVanContext.cs
@@ -0,0 +1,35 @@
+namespace AssociationRegistry.Test.E2E.When_Markeer_Als_Dubbel_Van;
+
+using Framework.ApiSetup;
+using Framework.TestClasses;
+using Marten.Events;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using Scenarios.Givens.FeitelijkeVereniging;
+using Scenarios.Requests.FeitelijkeVereniging;
+using Vereniging;
+
+public class MarkeerAlsDubbelVanContext: TestContextBase<MarkeerAlsDubbelVanRequest>
+{
+    public VCode VCode => RequestResult.VCode;
+    public MultipleWerdGeregistreerdScenario Scenario { get; }
+
+    public MarkeerAlsDubbelVanContext(FullBlownApiSetup apiSetup)
+    {
+        ApiSetup = apiSetup;
+        Scenario = new();
+    }
+
+    public override async Task InitializeAsync()
+    {
+        await ApiSetup.ExecuteGiven(Scenario);
+        RequestResult = await new MarkeerAlsDubbelVanRequestFactory(Scenario).ExecuteRequest(ApiSetup);
+        await ApiSetup.AdminProjectionHost.WaitForNonStaleProjectionDataAsync(TimeSpan.FromSeconds(10));
+        await ApiSetup.AdminApiHost.Services.GetRequiredService<IElasticClient>().Indices.RefreshAsync(Indices.All);
+    }
+}
+
+public class MarkeerAlsDubbelVanRequest
+{
+    public string IsDubbelVan { get; set; }
+}
