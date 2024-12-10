e4fa44457088509af0d07b5bf48b8d47b1580c13
emalfroy <egon.malfroy@gmail.com>
2024-12-10 11:17:30 +0100

    feat: or-2334 add controller & command for markeer als dubbel van

---
+++ test/AssociationRegistry.Test.Admin.Api/Commands/FeitelijkeVereniging/When_MarkeerAlsDubbelVan/RequestValidating/Is_Empty.cs
@@ -0,0 +1,21 @@
+﻿namespace AssociationRegistry.Test.Admin.Api.Commands.FeitelijkeVereniging.When_MarkeerAlsDubbelVan.RequestValidating;
+
+using AssociationRegistry.Admin.Api.Verenigingen.Dubbels.FeitelijkeVereniging.MarkeerAlsDubbelVan;
+using AssociationRegistry.Admin.Api.Verenigingen.Dubbels.FeitelijkeVereniging.MarkeerAlsDubbelVan.RequestModels;
+using FluentValidation.TestHelper;
+using Framework;
+using Xunit;
+
+public class Is_Empty : ValidatorTest
+{
+    [Fact]
+    public void Has_validation_error_IsDubbelVan_is_verplicht()
+    {
+        var validator = new MarkeerAlsDubbelVanValidator();
+        var request = new MarkeerAlsDubbelVanRequest();
+        var result = validator.TestValidate(request);
+
+        result.ShouldHaveValidationErrorFor(toeRequest => toeRequest.IsDubbelVan)
+              .WithErrorMessage("'IsDubbelVan' is verplicht.");
+    }
+}
