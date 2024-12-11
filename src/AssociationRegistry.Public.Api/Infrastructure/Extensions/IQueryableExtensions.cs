32d532bb7eee75648da5cfcfc39c18eafdce667d
emalfroy <egon.malfroy@gmail.com>
2024-12-11 15:54:28 +0100

    fix: or-2334 also return verenigingen with status dubbel for publiek detail

--- src/AssociationRegistry.Public.Api/Infrastructure/Extensions/IQueryableExtensions.cs
+++ src/AssociationRegistry.Public.Api/Infrastructure/Extensions/IQueryableExtensions.cs
@@ -13,8 +13,8 @@ public static class IQueryableExtensions
     public static IQueryable<T> WithVCode<T>(this IQueryable<T> source, string vCode) where T : IVCode
         => source.Where(x => x.VCode.Equals(vCode, StringComparison.CurrentCultureIgnoreCase));
 
-    public static IQueryable<PubliekVerenigingDetailDocument> OnlyActief(this IQueryable<PubliekVerenigingDetailDocument> source)
-        => source.Where(x => x.Status == VerenigingStatus.Actief);
+    public static IQueryable<PubliekVerenigingDetailDocument> OnlyActiefOrDubbel(this IQueryable<PubliekVerenigingDetailDocument> source)
+        => source.Where(x => x.Status == VerenigingStatus.Actief || x.Status == VerenigingStatus.Dubbel);
 
     public static IQueryable<T> OnlyIngeschrevenInPubliekeDatastroom<T>(this IQueryable<T> source)
         where T : ICanBeUitgeschrevenUitPubliekeDatastroom
