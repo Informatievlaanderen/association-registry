71dd88a340af691effe90edbc21b5cbe25e1fd77
Koen Metsu <koen@metsu.be>
2024-12-11 11:32:57 +0100

    feat: or-2334 add aanvaarde vereniging to corresponderende vcodes of authentieke vereniging

--- src/AssociationRegistry.Admin.ProjectionHost/Projections/Detail/BeheerVerenigingDetailProjection.cs
+++ src/AssociationRegistry.Admin.ProjectionHost/Projections/Detail/BeheerVerenigingDetailProjection.cs
@@ -227,6 +227,9 @@ public class BeheerVerenigingDetailProjection : EventProjection
     public async Task Project(IEvent<VerenigingWerdGermarkeerdAlsDubbelVan> @event, IDocumentOperations ops)
         => await Update(@event, ops, BeheerVerenigingDetailProjector.Apply);
 
+    public async Task Project(IEvent<VerenigingAanvaardeDubbeleVereniging> @event, IDocumentOperations ops)
+        => await Update(@event, ops, BeheerVerenigingDetailProjector.Apply);
+
     private async Task SoftDelete(string? streamKey, IDocumentOperations ops)
         => ops.Delete<BeheerVerenigingDetailDocument>(streamKey);
 
