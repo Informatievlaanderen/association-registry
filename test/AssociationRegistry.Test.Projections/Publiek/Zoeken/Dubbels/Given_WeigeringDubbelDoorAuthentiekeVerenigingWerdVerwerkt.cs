4000779cbd572d0a14fad9e09c25251e6cf9ef42
emalfroy <egon.malfroy@gmail.com>
2024-12-23 11:39:20 +0100

    feat: or-2335 implement publiek zoeken for weigering dubbel

---
+++ test/AssociationRegistry.Test.Projections/Publiek/Zoeken/Dubbels/Given_WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerkt.cs
@@ -0,0 +1,10 @@
+﻿namespace AssociationRegistry.Test.Projections.Publiek.Zoeken.Dubbels;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerkt(PubliekZoekenScenarioFixture<WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerktScenario> fixture)
+    : PubliekZoekenScenarioClassFixture<WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerktScenario>
+{
+    [Fact]
+    public void Status_Is_Dubbel()
+        => fixture.Result.IsDubbel.Should().BeFalse();
+}
