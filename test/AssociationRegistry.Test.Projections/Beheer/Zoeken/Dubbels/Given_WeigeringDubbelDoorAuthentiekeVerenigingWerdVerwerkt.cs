12c063e47a61712630abdc8c890445db9f3d96a3
emalfroy <egon.malfroy@gmail.com>
2024-12-23 11:00:59 +0100

    feat: or-2335 implement beheer zoeken for weigering dubbel

---
+++ test/AssociationRegistry.Test.Projections/Beheer/Zoeken/Dubbels/Given_WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerkt.cs
@@ -0,0 +1,10 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.Zoeken.Dubbels;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerkt(BeheerZoekenScenarioFixture<WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerktScenario> fixture)
+    : BeheerZoekenScenarioClassFixture<WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerktScenario>
+{
+    [Fact]
+    public void Status_Is_Dubbel()
+        => fixture.Result.IsDubbel.Should().BeFalse();
+}
