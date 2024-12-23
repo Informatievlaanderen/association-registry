e19d070f428253de892a45b38d3c8ffa09756f64
emalfroy <egon.malfroy@gmail.com>
2024-12-23 10:00:37 +0100

    feat: or-2335 implement acm projection for weigering dubbel

---
+++ test/AssociationRegistry.Test.Projections/Acm/Dubbels/Given_WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerkt.cs
@@ -0,0 +1,16 @@
+﻿namespace AssociationRegistry.Test.Projections.Acm.Dubbels;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerkt(
+    VerenigingenPerInszScenarioFixture<WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerktScenario> fixture)
+    : VerenigingenPerInszScenarioClassFixture<WeigeringDubbelDoorAuthentiekeVerenigingWerdVerwerktScenario>
+{
+    [Fact]
+    public void Then_Dubbele_Vereniging_Is_Gemarkeerd_Als_Dubbel()
+    {
+        var dubbeleVereniging =
+            fixture.Result.Verenigingen.Single(x => x.VCode == fixture.Scenario.DubbeleVerenigingWerdGeregistreerd.VCode);
+
+        dubbeleVereniging.IsDubbel.Should().BeFalse();
+    }
+}
