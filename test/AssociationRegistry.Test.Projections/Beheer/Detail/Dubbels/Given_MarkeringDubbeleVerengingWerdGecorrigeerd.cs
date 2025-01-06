956fe521a92f93939ad7a6e53ac6cbf995db37fb
emalfroy <egon.malfroy@gmail.com>
2025-01-06 16:02:24 +0100

    test: or-1716 add projection test for MarkeringDubbeleVerengingWerdGecorrigeerd

---
+++ test/AssociationRegistry.Test.Projections/Beheer/Detail/Dubbels/Given_MarkeringDubbeleVerengingWerdGecorrigeerd.cs
@@ -0,0 +1,21 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.Detail.Dubbels;
+
+using Admin.Schema.Constants;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_MarkeringDubbeleVerengingWerdGecorrigeerd(BeheerDetailScenarioFixture<MarkeringDubbeleVerengingWerdGecorrigeerdScenario> fixture)
+    : BeheerDetailScenarioClassFixture<MarkeringDubbeleVerengingWerdGecorrigeerdScenario>
+{
+    [Fact]
+    public void Metadata_Is_Updated()
+        => fixture.Result
+                  .Metadata.Version.Should().Be(3);
+
+    [Fact]
+    public void Document_IsDubbelVan_Is_Updated()
+        => fixture.Result.IsDubbelVan.Should().Be(String.Empty);
+
+    [Fact]
+    public void Document_Status_Is_Dubbel()
+        => fixture.Result.Status.Should().Be(VerenigingStatus.Actief);
+}
---
+++ test/AssociationRegistry.Test.Projections/Scenario/MarkeringDubbeleVerengingWerdGecorrigeerdScenario.cs
@@ -0,0 +1,56 @@
+﻿namespace AssociationRegistry.Test.Projections.Scenario;
+
+using AutoFixture;
+using Events;
+
+public class MarkeringDubbeleVerengingWerdGecorrigeerdScenario : InszScenarioBase
+{
+    public FeitelijkeVerenigingWerdGeregistreerd DubbeleVerenigingWerdGeregistreerd { get; }
+    public FeitelijkeVerenigingWerdGeregistreerd AuthentiekeVerenigingWerdGeregistreerd { get; }
+    public VerenigingWerdGemarkeerdAlsDubbelVan VerenigingWerdGemarkeerdAlsDubbelVan { get; set; }
+    public VerenigingAanvaarddeDubbeleVereniging VerenigingAanvaarddeDubbeleVereniging { get; set; }
+    public MarkeringDubbeleVerengingWerdGecorrigeerd MarkeringDubbeleVerengingWerdGecorrigeerd { get; set; }
+
+    private string _insz { get; }
+    public MarkeringDubbeleVerengingWerdGecorrigeerdScenario()
+    {
+
+        DubbeleVerenigingWerdGeregistreerd = AutoFixture.Create<FeitelijkeVerenigingWerdGeregistreerd>();
+
+        AuthentiekeVerenigingWerdGeregistreerd = AutoFixture.Create<FeitelijkeVerenigingWerdGeregistreerd>()
+            with
+            {
+                Vertegenwoordigers = DubbeleVerenigingWerdGeregistreerd.Vertegenwoordigers,
+            };
+
+        _insz = AuthentiekeVerenigingWerdGeregistreerd.Vertegenwoordigers[0].Insz;
+
+        VerenigingWerdGemarkeerdAlsDubbelVan = AutoFixture.Create<VerenigingWerdGemarkeerdAlsDubbelVan>() with
+        {
+            VCode = DubbeleVerenigingWerdGeregistreerd.VCode,
+            VCodeAuthentiekeVereniging = AuthentiekeVerenigingWerdGeregistreerd.VCode,
+        };
+
+        VerenigingAanvaarddeDubbeleVereniging = AutoFixture.Create<VerenigingAanvaarddeDubbeleVereniging>() with
+        {
+            VCode = AuthentiekeVerenigingWerdGeregistreerd.VCode,
+            VCodeDubbeleVereniging = DubbeleVerenigingWerdGeregistreerd.VCode,
+        };
+
+        MarkeringDubbeleVerengingWerdGecorrigeerd = AutoFixture.Create<MarkeringDubbeleVerengingWerdGecorrigeerd>() with
+        {
+            VCode = DubbeleVerenigingWerdGeregistreerd.VCode,
+            VCodeAuthentiekeVereniging = AuthentiekeVerenigingWerdGeregistreerd.VCode,
+        };
+    }
+
+    public override string VCode => DubbeleVerenigingWerdGeregistreerd.VCode;
+
+    public override EventsPerVCode[] Events =>
+    [
+        new(VCode, DubbeleVerenigingWerdGeregistreerd, VerenigingWerdGemarkeerdAlsDubbelVan, MarkeringDubbeleVerengingWerdGecorrigeerd),
+        new(AuthentiekeVerenigingWerdGeregistreerd.VCode, AuthentiekeVerenigingWerdGeregistreerd, VerenigingAanvaarddeDubbeleVereniging),
+    ];
+
+    public override string Insz => _insz;
+}
