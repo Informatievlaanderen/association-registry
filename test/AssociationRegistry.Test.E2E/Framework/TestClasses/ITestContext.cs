cdb0c5e629b78bef7f5b5ac25e7fb132de855613
Koen Metsu <koen@metsu.be>
2025-02-03 13:49:42 +0100

    test: or-2350 make sure e2e test setups run only once

--- test/AssociationRegistry.Test.E2E/Framework/TestClasses/ITestContext.cs
+++ test/AssociationRegistry.Test.E2E/Framework/TestClasses/ITestContext.cs
@@ -1,10 +1,13 @@
 namespace AssociationRegistry.Test.E2E.Framework.TestClasses;
 
 using ApiSetup;
+using Scenarios.Requests;
 using Xunit;
 
 public interface ITestContext<TRequest>: IAsyncLifetime
 {
     IApiSetup ApiSetup { get; }
     TRequest Request { get; }
+    RequestResult<TRequest> RequestResult { get; set; }
+    Task Init();
 }
