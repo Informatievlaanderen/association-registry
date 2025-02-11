813ca2cda929d70ced9940ba3f450814a4664875
emalfroy <egon.malfroy@gmail.com>
2025-02-11 16:05:10 +0100

    refactor: or-2665 move verenigingsType to another class

---
+++ src/AssociationRegistry.Admin.Api/DecentraalBeheer/Verenigingen/Detail/VerenigingsTypeMapper.cs
@@ -0,0 +1,52 @@
+﻿namespace AssociationRegistry.Admin.Api.Verenigingen.Detail;
+
+using ResponseModels;
+using Vereniging;
+
+public interface IVerenigingsTypeMapper
+{
+    VerenigingsType Map(Schema.Detail.VerenigingsType verenigingsType);
+}
+
+public class VerenigingsTypeMapper : IVerenigingsTypeMapper
+{
+    public VerenigingsType Map(Schema.Detail.VerenigingsType verenigingsType)
+    {
+        // TODO: uncomment when implementing VZER
+        // if (Vereniging.Verenigingstype.IsVerenigingZonderEigenRechtspersoonlijkheid(verenigingsType.Code))
+        // {
+        //     return new VerenigingsType
+        //     {
+        //         Code = Vereniging.Verenigingstype.FeitelijkeVereniging.Code,
+        //         Naam = Vereniging.Verenigingstype.FeitelijkeVereniging.Naam,
+        //     };
+        // }
+
+        return new VerenigingsType
+        {
+            Code = verenigingsType.Code,
+            Naam = verenigingsType.Naam,
+        };
+    }
+}
+
+public class VerenigingsTypeMapperV2 : IVerenigingsTypeMapper
+{
+    public VerenigingsType Map(Schema.Detail.VerenigingsType verenigingsType)
+    {
+        if (Verenigingstype.IsVerenigingZonderEigenRechtspersoonlijkheid(verenigingsType.Code))
+        {
+            return new VerenigingsType
+            {
+                Code = Verenigingstype.VZER.Code,
+                Naam = Verenigingstype.VZER.Naam,
+            };
+        }
+
+        return new VerenigingsType
+        {
+            Code = verenigingsType.Code,
+            Naam = verenigingsType.Naam,
+        };
+    }
+}
