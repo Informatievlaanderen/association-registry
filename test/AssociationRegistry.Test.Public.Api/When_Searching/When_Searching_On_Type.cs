51fed14309fcf12ff603341b4b04e96d80cfab41
emalfroy <egon.malfroy@gmail.com>
2025-02-13 15:09:05 +0100

    feat: or-2665 modify search query to search on vzer and fv when searching for verenigingstype

--- test/AssociationRegistry.Test.Public.Api/When_Searching/When_Searching_On_Type.cs
+++ test/AssociationRegistry.Test.Public.Api/When_Searching/When_Searching_On_Type.cs
@@ -1,39 +1,38 @@
-﻿namespace AssociationRegistry.Test.Admin.Api.Queries;
+﻿namespace AssociationRegistry.Test.Public.Api.When_Searching;
 
-using AssociationRegistry.Admin.Api.Queries;
-using AssociationRegistry.Admin.Api.Verenigingen.Search.RequestModels;
-using AssociationRegistry.Admin.Schema.Search;
+using AssociationRegistry.Public.Api.Queries;
+using AssociationRegistry.Public.Api.Verenigingen.Search.RequestModels;
+using AssociationRegistry.Public.Schema.Search;
 using AutoFixture;
 using Common.AutoFixture;
+using Fixtures.GivenEvents;
 using FluentAssertions;
 using Framework;
-using Framework.Fixtures;
 using Microsoft.Extensions.DependencyInjection;
 using Nest;
 using Vereniging;
 using Xunit;
 using Xunit.Categories;
 
-[Collection(nameof(AdminApiCollection))]
-[Category("AdminApi")]
+[Collection(nameof(PublicApiCollection))]
+[Category("PublicApi")]
 [IntegrationTest]
 public class When_Searching_On_Type
 {
-    private BeheerVerenigingenZoekQuery _query;
+    private PubliekVerenigingenZoekQuery _query;
     private VerenigingZoekDocument? _feitelijkeVereniging;
     private VerenigingZoekDocument? _vzer;
     private IElasticClient _elasticClient;
     private TypeMapping _typeMapping;
 
-    public When_Searching_On_Type(EventsInDbScenariosFixture fixture)
+    public When_Searching_On_Type(GivenEventsFixture fixture)
     {
         var elasticClient = fixture.ElasticClient;
         _typeMapping = fixture.ServiceProvider.GetRequiredService<TypeMapping>();
 
-        var autoFixture = new Fixture().CustomizeAdminApi();
+        var autoFixture = new Fixture().CustomizePublicApi();
 
         _feitelijkeVereniging = autoFixture.Create<VerenigingZoekDocument>();
-        _feitelijkeVereniging.VCode = VCode.Create("V0099001");
         _feitelijkeVereniging.Naam = "de kleine vereniging";
         _feitelijkeVereniging.Verenigingstype = new VerenigingZoekDocument.VerenigingsType
         {
@@ -41,7 +40,7 @@ public class When_Searching_On_Type
             Naam = Verenigingstype.FeitelijkeVereniging.Naam,
         };
         _vzer = autoFixture.Create<VerenigingZoekDocument>();
-        _vzer.VCode = VCode.Create("V0088001");
+
         _vzer.Naam = "de kleine vereniging";
         _vzer.Verenigingstype = new VerenigingZoekDocument.VerenigingsType
         {
@@ -74,11 +73,12 @@ public class When_Searching_On_Type
         indexVzer.ShouldBeValidIndexResponse();
 
         await _elasticClient.Indices.RefreshAsync(Indices.AllIndices);
-        _query = new BeheerVerenigingenZoekQuery(_elasticClient, _typeMapping);
+        _query = new PubliekVerenigingenZoekQuery(_elasticClient, _typeMapping);
 
         var searchResponse = await _query.ExecuteAsync(
-            new BeheerVerenigingenZoekFilter(query: query,
+            new PubliekVerenigingenZoekFilter(query: query,
                                              sort: "vCode",
+                                             hoofdactiviteiten:[],
                                              paginationQueryParams: new PaginationQueryParams()),
             CancellationToken.None);
 
