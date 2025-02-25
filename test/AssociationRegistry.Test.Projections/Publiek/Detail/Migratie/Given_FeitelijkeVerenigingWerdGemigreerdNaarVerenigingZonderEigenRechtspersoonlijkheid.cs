891c789c67812f35d0b00c3ee583ff1665ecee68
emalfroy <egon.malfroy@gmail.com>
2025-02-25 11:50:15 +0100

    feat: or-2670 implement vzer migratie for publiek detail

---
+++ test/AssociationRegistry.Test.Projections/Publiek/Detail/Migratie/Given_FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid.cs
@@ -0,0 +1,17 @@
+﻿namespace AssociationRegistry.Test.Projections.Publiek.Detail.Migratie;
+
+using Public.Schema.Detail;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid(PubliekDetailScenarioFixture<FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheidScenario> fixture)
+    : PubliekDetailScenarioClassFixture<FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheidScenario>
+{
+    [Fact]
+    public void Document_Is_Updated()
+        => fixture.Result
+                  .Verenigingstype.Should().BeEquivalentTo(new PubliekVerenigingDetailDocument.VerenigingsType()
+                   {
+                       Code = Vereniging.Verenigingstype.VZER.Code,
+                       Naam = Vereniging.Verenigingstype.VZER.Naam,
+                   });
+}
