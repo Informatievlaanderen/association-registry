04722e0111951997df01a974e46d2136d9d525cc
emalfroy <egon.malfroy@gmail.com>
2025-02-25 11:34:59 +0100

    feat :or-2670 implement vzer migratie for dubbel detectie

---
+++ test/AssociationRegistry.Test.Projections/Beheer/DuplicateDetection/Migratie/Given_FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid.cs
@@ -0,0 +1,12 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.DuplicateDetection.Migratie;
+
+using Vereniging;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid(DuplicateDetectionScenarioFixture<FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheidScenario> fixture)
+    : DuplicateDetectionClassFixture<FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheidScenario>
+{
+    [Fact]
+    public void Status_Is_Dubbel()
+        => fixture.Result.VerenigingsTypeCode.Should().Be(Verenigingstype.VZER.Code);
+}
