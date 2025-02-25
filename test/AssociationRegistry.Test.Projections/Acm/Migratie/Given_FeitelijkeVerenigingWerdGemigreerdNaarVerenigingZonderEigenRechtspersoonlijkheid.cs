93ffb8909a99458ad2e553b059c94fa607421331
emalfroy <egon.malfroy@gmail.com>
2025-02-25 11:25:46 +0100

    feat: or-2670 implement vzer migratie for acm

---
+++ test/AssociationRegistry.Test.Projections/Acm/Migratie/Given_FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid.cs
@@ -0,0 +1,18 @@
+﻿namespace AssociationRegistry.Test.Projections.Acm.Migratie;
+
+using AssociationRegistry.Acm.Schema.VerenigingenPerInsz;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid(
+    VerenigingenPerInszScenarioFixture<FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheidScenario> fixture)
+    : VerenigingenPerInszScenarioClassFixture<FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheidScenario>
+{
+    [Fact]
+    public void Then_VerenigingsType_Is_Vzer()
+    {
+        var vzer =
+            fixture.Result.Verenigingen.Single(x => x.VCode == fixture.Scenario.FeitelijkeVerenigingWerdGeregistreerd.VCode);
+
+        vzer.Verenigingstype.Should().BeEquivalentTo(new Verenigingstype(AssociationRegistry.Vereniging.Verenigingstype.VZER.Code, AssociationRegistry.Vereniging.Verenigingstype.VZER.Naam));
+    }
+}
