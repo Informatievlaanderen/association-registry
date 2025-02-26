f77533f3f5467fbcbe1c10691b78a8d974d0371b
emalfroy <egon.malfroy@gmail.com>
2025-02-26 15:54:25 +0100

    refactor: or-2200 move einddatum projection tests to new projection tests

---
+++ test/AssociationRegistry.Test.Projections/Scenario/Einddatum/EinddatumWerdGewijzigdScenario.cs
@@ -0,0 +1,28 @@
+namespace AssociationRegistry.Test.Projections.Scenario.Einddatum;
+
+using AssociationRegistry.Events;
+using AutoFixture;
+
+public class EinddatumWerdGewijzigdScenario : ScenarioBase
+{
+    public VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd { get; }
+    public EinddatumWerdGewijzigd EinddatumWerdGewijzigd { get; }
+
+    public EinddatumWerdGewijzigdScenario()
+    {
+        VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd =
+            AutoFixture.Create<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd>();
+
+        EinddatumWerdGewijzigd = AutoFixture.Create<EinddatumWerdGewijzigd>();
+    }
+
+    public override string VCode => VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd.VCode;
+
+    public override EventsPerVCode[] Events =>
+    [
+        new(VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd.VCode,
+            VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd,
+            EinddatumWerdGewijzigd
+        ),
+    ];
+}
