9e302f585fedd29306ec3ee2f93aa5a1462c0471
emalfroy <egon.malfroy@gmail.com>
2025-02-26 10:18:14 +0100

    fix: or-2721 implement missing events to powerbi projections

--- test/AssociationRegistry.Test.Projections/Beheer/SanityChecks/For_Any_Event.cs
+++ test/AssociationRegistry.Test.Projections/Beheer/SanityChecks/For_Any_Event.cs
@@ -1,5 +1,6 @@
 namespace AssociationRegistry.Test.Projections.Beheer.SanityChecks;
 
+using Admin.ProjectionHost.Projections.PowerBiExport;
 using AssociationRegistry.Admin.ProjectionHost.Projections.Detail;
 using Events;
 using Marten.Events;
@@ -34,6 +35,34 @@ public class For_Any_Event
             );
 
             Assert.True(hasCreateOrProjectMethod, $"No Create or Project method found for event type {eventType.Name}");
+        }
+    }
+
+    [Fact]
+    public void There_Should_Be_A_Create_Or_Project_Method_For_PowerBi()
+    {
+        var eventTypes = typeof(Events.IEvent).Assembly
+                                                                     .GetTypes()
+                                                                     .Where(t => typeof(Events.IEvent)
+                                                                               .IsAssignableFrom(t) && !t.IsAbstract && t.IsClass)
+                                                                     .Except([typeof(AfdelingWerdGeregistreerd)]) // because it's obsolete
+                                                                     .ToList();
+
+        var projectionType = typeof(PowerBiExportProjection);
+        var methods = projectionType.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var eventType in eventTypes)
+        {
+            var hasCreateOrProjectMethod = methods.Any(m =>
+                                                           (m.Name == "Create" || m.Name == "Project" || m.Name == "Apply") &&
+                                                           m.GetParameters().Any(p =>
+                                                                                     p.ParameterType.IsGenericType &&
+                                                                                     p.ParameterType.GetGenericTypeDefinition() == typeof(IEvent<>) &&
+                                                                                     p.ParameterType.GetGenericArguments()[0] == eventType
+                                                           )
+            );
+
+            Assert.True(hasCreateOrProjectMethod, $"No Create or Project or Apply method found for event type {eventType.Name}");
         }
     }
 }
