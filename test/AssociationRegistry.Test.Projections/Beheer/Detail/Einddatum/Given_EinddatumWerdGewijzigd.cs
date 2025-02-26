32d9be2bddeb5575a239d77ffb9f88fc8d9f8255
emalfroy <egon.malfroy@gmail.com>
2025-02-26 15:54:25 +0100

    refactor: or-2200 move einddatum projection tests to new projection tests

---
+++ test/AssociationRegistry.Test.Projections/Beheer/Detail/Einddatum/Given_EinddatumWerdGewijzigd.cs
@@ -0,0 +1,21 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.Detail.Einddatum;
+
+using Formats;
+using Scenario.Einddatum;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_EinddatumWerdGewijzigd(
+    BeheerDetailScenarioFixture<EinddatumWerdGewijzigdScenario> fixture)
+    : BeheerDetailScenarioClassFixture<EinddatumWerdGewijzigdScenario>
+{
+    [Fact]
+    public void Metadata_Is_Updated()
+        => fixture.Result
+                  .Metadata.Version.Should().Be(2);
+
+    [Fact]
+    public void Document_Is_Updated()
+    {
+        fixture.Result.Einddatum.Should().BeEquivalentTo(fixture.Scenario.EinddatumWerdGewijzigd.Einddatum.FormatAsBelgianDate());
+    }
+}
