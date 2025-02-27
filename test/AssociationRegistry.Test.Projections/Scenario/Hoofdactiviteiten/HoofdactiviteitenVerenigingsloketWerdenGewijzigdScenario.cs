3998d7f9359b3fe905174e7e78d6cd5ecafb476d
emalfroy <egon.malfroy@gmail.com>
2025-02-27 08:37:52 +0100

    refactor: or-2200 move hoofdactiviteiten projection tests to new projection tests

---
+++ test/AssociationRegistry.Test.Projections/Scenario/Hoofdactiviteiten/HoofdactiviteitenVerenigingsloketWerdenGewijzigdScenario.cs
@@ -0,0 +1,28 @@
+namespace AssociationRegistry.Test.Projections.Scenario.Hoofdactiviteiten;
+
+using AutoFixture;
+using Events;
+
+public class HoofdactiviteitenVerenigingsloketWerdenGewijzigdScenario : ScenarioBase
+{
+    public VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd { get; }
+    public HoofdactiviteitenVerenigingsloketWerdenGewijzigd HoofdactiviteitenVerenigingsloketWerdenGewijzigd { get; }
+
+    public HoofdactiviteitenVerenigingsloketWerdenGewijzigdScenario()
+    {
+        VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd =
+            AutoFixture.Create<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd>();
+
+        HoofdactiviteitenVerenigingsloketWerdenGewijzigd = AutoFixture.Create<HoofdactiviteitenVerenigingsloketWerdenGewijzigd>();
+    }
+
+    public override string VCode => VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd.VCode;
+
+    public override EventsPerVCode[] Events =>
+    [
+        new(VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd.VCode,
+            VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd,
+            HoofdactiviteitenVerenigingsloketWerdenGewijzigd
+        ),
+    ];
+}
