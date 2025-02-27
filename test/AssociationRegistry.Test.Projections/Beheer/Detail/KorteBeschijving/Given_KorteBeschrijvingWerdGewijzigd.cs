c0ee958862ceecaf1011152d8c4007b225182340
emalfroy <egon.malfroy@gmail.com>
2025-02-27 08:41:54 +0100

    refactor: or-2200 move korte beschrijving projection tests to new projection tests

---
+++ test/AssociationRegistry.Test.Projections/Beheer/Detail/KorteBeschijving/Given_KorteBeschrijvingWerdGewijzigd.cs
@@ -0,0 +1,21 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.Detail.KorteBeschijving;
+
+using AssociationRegistry.Test.Projections.Scenario.KorteBeschijving;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_KorteBeschrijvingWerdGewijzigd(
+    BeheerDetailScenarioFixture<KorteBeschrijvingWerdGewijzigdScenario> fixture)
+    : BeheerDetailScenarioClassFixture<KorteBeschrijvingWerdGewijzigdScenario>
+{
+    [Fact]
+    public void Metadata_Is_Updated()
+        => fixture.Result
+                  .Metadata.Version.Should().Be(2);
+
+    [Fact]
+    public void Document_Is_Updated()
+    {
+        fixture.Result.KorteBeschrijving.Should()
+               .BeEquivalentTo(fixture.Scenario.KorteBeschrijvingWerdGewijzigd.KorteBeschrijving);
+    }
+}
