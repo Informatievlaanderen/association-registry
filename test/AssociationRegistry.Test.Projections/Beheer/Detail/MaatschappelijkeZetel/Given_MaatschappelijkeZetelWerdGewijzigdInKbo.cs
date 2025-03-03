a958595fea5fec2c13a3ff6e1c0afc6eab6818af
emalfroy <egon.malfroy@gmail.com>
2025-03-03 15:40:03 +0100

    refactor: or-2200 maatschappelijke zetel projections tests to new projection tests

---
+++ test/AssociationRegistry.Test.Projections/Beheer/Detail/MaatschappelijkeZetel/Given_MaatschappelijkeZetelWerdGewijzigdInKbo.cs
@@ -0,0 +1,54 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.Detail.MaatschappelijkeZetel;
+
+using AssociationRegistry.Admin.Schema;
+using AssociationRegistry.Admin.Schema.Detail;
+using AssociationRegistry.Events;
+using AssociationRegistry.Formats;
+using AssociationRegistry.JsonLdContext;
+using AssociationRegistry.Test.Projections.Scenario.MaatschappelijkeZetelVolgensKbo;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_MaatschappelijkeZetelWerdGewijzigdInKbo(
+    BeheerDetailScenarioFixture<MaatschappelijkeZetelWerdGewijzigdInKboScenario> fixture)
+    : BeheerDetailScenarioClassFixture<MaatschappelijkeZetelWerdGewijzigdInKboScenario>
+{
+    [Fact]
+    public void Metadata_Is_Updated()
+        => fixture.Result
+                  .Metadata.Version.Should().Be(3);
+
+    [Fact]
+    public void Only_Adres_Is_Updated()
+    {
+        var scenarioMaatschappelijkeZetelWerdGewijzigdInKbo = fixture.Scenario.MaatschappelijkeZetelWerdGewijzigdInKbo;
+        var actual = fixture.Result.Locaties.Single(x => x.LocatieId == scenarioMaatschappelijkeZetelWerdGewijzigdInKbo.Locatie.LocatieId);
+        var vCode = fixture.Scenario.VCode;
+
+        actual.Adres.Should().BeEquivalentTo(scenarioMaatschappelijkeZetelWerdGewijzigdInKbo.Locatie.Adres);
+        actual.Adresvoorstelling.Should().BeEquivalentTo(scenarioMaatschappelijkeZetelWerdGewijzigdInKbo.Locatie.Adres.ToAdresString());
+
+        VerifyJsonLdMetadata(actual, vCode, scenarioMaatschappelijkeZetelWerdGewijzigdInKbo);
+    }
+
+    private static void VerifyJsonLdMetadata(Locatie actual, string vCode, MaatschappelijkeZetelWerdGewijzigdInKbo locatie)
+    {
+        actual.JsonLdMetadata.Should().BeEquivalentTo(new JsonLdMetadata
+        {
+            Id = JsonLdType.Locatie.CreateWithIdValues(vCode, locatie.Locatie.LocatieId.ToString()),
+            Type = JsonLdType.Locatie.Type,
+        });
+
+        actual.Adres.JsonLdMetadata.Should().BeEquivalentTo(new JsonLdMetadata
+        {
+            Id = JsonLdType.Adres.CreateWithIdValues(vCode, locatie.Locatie.LocatieId.ToString()),
+            Type = JsonLdType.Adres.Type,
+        });
+
+        actual.VerwijstNaar.JsonLdMetadata.Should().BeEquivalentTo(new JsonLdMetadata
+        {
+            Id = JsonLdType.AdresVerwijzing.CreateWithIdValues(
+                locatie.Locatie.AdresId?.Bronwaarde.Split('/').Last()),
+            Type = JsonLdType.AdresVerwijzing.Type,
+        });
+    }
+}
