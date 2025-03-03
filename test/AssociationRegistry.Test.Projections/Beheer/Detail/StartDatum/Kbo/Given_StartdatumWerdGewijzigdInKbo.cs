38d566161c1eef7990e7c3bf9c3bf23992a15782
emalfroy <egon.malfroy@gmail.com>
2025-03-03 15:55:19 +0100

    refactor: or-2200 startdatum werd gewijzigd projections tests to new projection tests

---
+++ test/AssociationRegistry.Test.Projections/Beheer/Detail/StartDatum/Kbo/Given_StartdatumWerdGewijzigdInKbo.cs
@@ -0,0 +1,22 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.Detail.StartDatum.Kbo;
+
+using Formats;
+using Scenario.Startdatum.Kbo;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_StartdatumWerdGewijzigdInKbo(
+    BeheerDetailScenarioFixture<StartdatumWerdGewijzigdInKboScenario> fixture)
+    : BeheerDetailScenarioClassFixture<StartdatumWerdGewijzigdInKboScenario>
+{
+    [Fact]
+    public void Metadata_Is_Updated()
+        => fixture.Result
+                  .Metadata.Version.Should().Be(2);
+
+    [Fact]
+    public void Document_Is_Updated()
+    {
+        fixture.Result.Startdatum.Should()
+               .BeEquivalentTo(fixture.Scenario.StartdatumWerdGewijzigdInKbo.Startdatum.FormatAsBelgianDate());
+    }
+}
---
+++ test/AssociationRegistry.Test.Projections/Scenario/Startdatum/Vzer/StartdatumWerdGewijzigdScenario.cs
@@ -0,0 +1,28 @@
+namespace AssociationRegistry.Test.Projections.Scenario.Startdatum.Vzer;
+
+using AssociationRegistry.Events;
+using AutoFixture;
+
+public class StartdatumWerdGewijzigdScenario : ScenarioBase
+{
+    public VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd { get; }
+    public StartdatumWerdGewijzigd StartdatumWerdGewijzigd { get; }
+
+    public StartdatumWerdGewijzigdScenario()
+    {
+        VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd =
+            AutoFixture.Create<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd>();
+
+        StartdatumWerdGewijzigd = AutoFixture.Create<StartdatumWerdGewijzigd>();
+    }
+
+    public override string VCode => VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd.VCode;
+
+    public override EventsPerVCode[] Events =>
+    [
+        new(VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd.VCode,
+            VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd,
+            StartdatumWerdGewijzigd
+        ),
+    ];
+}
