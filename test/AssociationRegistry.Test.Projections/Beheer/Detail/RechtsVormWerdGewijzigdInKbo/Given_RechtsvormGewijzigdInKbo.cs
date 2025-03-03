4315f2870a3414ab7f1f7dc6507a6834e4f81932
emalfroy <egon.malfroy@gmail.com>
2025-03-03 15:48:34 +0100

    refactor: or-2200 rechtsvorm in kbo werd gewijzigd projections tests to new projection tests

---
+++ test/AssociationRegistry.Test.Projections/Beheer/Detail/RechtsVormWerdGewijzigdInKbo/Given_RechtsvormGewijzigdInKbo.cs
@@ -0,0 +1,29 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.Detail.RechtsVormWerdGewijzigdInKbo;
+
+using AssociationRegistry.Test.Projections.Scenario.NaamWerdGewijzigd.Kbo;
+using Public.Schema.Detail;
+using Vereniging;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_RechtsvormWerdGewijzigdInKBO(
+    BeheerDetailScenarioFixture<RechtsvormWerdGewijzigdInKBOScenario> fixture)
+    : BeheerDetailScenarioClassFixture<RechtsvormWerdGewijzigdInKBOScenario>
+{
+    [Fact]
+    public void Metadata_Is_Updated()
+        => fixture.Result
+                  .Metadata.Version.Should().Be(2);
+
+    [Fact]
+    public void Document_Is_Updated()
+    {
+        fixture.Result.Rechtsvorm.Should()
+               .BeEquivalentTo(fixture.Scenario.RechtsvormWerdGewijzigdInKBO.Rechtsvorm);
+
+        fixture.Result.Verenigingstype.Should().BeEquivalentTo(new PubliekVerenigingDetailDocument.VerenigingsType
+        {
+            Code = Verenigingstype.Parse(fixture.Scenario.RechtsvormWerdGewijzigdInKBO.Rechtsvorm).Code,
+            Naam = Verenigingstype.Parse(fixture.Scenario.RechtsvormWerdGewijzigdInKBO.Rechtsvorm).Naam,
+        });
+    }
+}
