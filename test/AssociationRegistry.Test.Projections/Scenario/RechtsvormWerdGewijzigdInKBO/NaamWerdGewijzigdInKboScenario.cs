618763b0ce9cec33383d9f2edb819962ce5c2f8b
emalfroy <egon.malfroy@gmail.com>
2025-03-03 15:48:34 +0100

    refactor: or-2200 rechtsvorm in kbo werd gewijzigd projections tests to new projection tests

---
+++ test/AssociationRegistry.Test.Projections/Scenario/RechtsvormWerdGewijzigdInKBO/NaamWerdGewijzigdInKboScenario.cs
@@ -0,0 +1,28 @@
+namespace AssociationRegistry.Test.Projections.Scenario.NaamWerdGewijzigd.Kbo;
+
+using AssociationRegistry.Events;
+using AutoFixture;
+
+public class RechtsvormWerdGewijzigdInKBOScenario : ScenarioBase
+{
+    public VerenigingMetRechtspersoonlijkheidWerdGeregistreerd VerenigingMetRechtspersoonlijkheidWerdGeregistreerd { get; }
+    public RechtsvormWerdGewijzigdInKBO RechtsvormWerdGewijzigdInKBO { get; }
+
+    public RechtsvormWerdGewijzigdInKBOScenario()
+    {
+        VerenigingMetRechtspersoonlijkheidWerdGeregistreerd =
+            AutoFixture.Create<VerenigingMetRechtspersoonlijkheidWerdGeregistreerd>();
+
+        RechtsvormWerdGewijzigdInKBO = AutoFixture.Create<RechtsvormWerdGewijzigdInKBO>();
+    }
+
+    public override string VCode => VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.VCode;
+
+    public override EventsPerVCode[] Events =>
+    [
+        new(VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.VCode,
+            VerenigingMetRechtspersoonlijkheidWerdGeregistreerd,
+            RechtsvormWerdGewijzigdInKBO
+        ),
+    ];
+}
