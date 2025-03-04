d8c7980aea118e41fcbf01f276b623ea5b748ba8
emalfroy <egon.malfroy@gmail.com>
2025-03-04 10:55:56 +0100

    test: or-2671 add e2e test for verfijn subtype fv

---
+++ src/AssociationRegistry/Vereniging/Subtype/Verenigingstype.cs
@@ -0,0 +1,33 @@
+﻿namespace AssociationRegistry.Vereniging.Subtype;
+
+public class Subtype : ISubtype
+{
+    public static readonly Subtype FeitelijkeVereniging = new(code: "FV", naam: "Feitelijke vereniging");
+    public static readonly Subtype SubVereniging = new(code: "SUB", naam: "Subvereniging");
+    public static readonly Subtype NogNietBepaald = new(code: "NNB", naam: "Nog niet bepaald");
+
+    public static readonly Subtype[] All =
+    {
+        FeitelijkeVereniging,
+        SubVereniging,
+        NogNietBepaald,
+    };
+
+    public Subtype(string code, string naam)
+    {
+        Code = code;
+        Naam = naam;
+    }
+
+    public string Code { get; init; }
+    public string Naam { get; init; }
+
+    public static Subtype Parse(string code)
+        => All.Single(t => t.Code == code);
+}
+
+public interface ISubtype
+{
+    string Code { get; init; }
+    string Naam { get; init; }
+}
---
+++ test/AssociationRegistry.Test.E2E/When_SubtypeWerdVerfijndNaarFeitelijkeVereniging/Beheer/Detail/Returns_Detail_With_Toegevoegd_Lidmaatschap.cs
@@ -0,0 +1,48 @@
+﻿namespace AssociationRegistry.Test.E2E.When_SubtypeWerdVerfijndNaarFeitelijkeVereniging.Beheer.Detail;
+
+using Admin.Api.Verenigingen.Detail.ResponseModels;
+using Admin.Api.Verenigingen.Subtype;
+using FluentAssertions;
+using Framework.AlbaHost;
+using Framework.ApiSetup;
+using Framework.TestClasses;
+using KellermanSoftware.CompareNetObjects;
+using Xunit;
+using Subtype = Vereniging.Subtype.Subtype;
+
+[Collection(FullBlownApiCollection.Name)]
+public class Returns_Detail : End2EndTest<VerfijnSubtypeNaarFeitelijkeVerenigingContext, WijzigSubtypeRequest, DetailVerenigingResponse>
+{
+    private readonly VerfijnSubtypeNaarFeitelijkeVerenigingContext _context;
+
+    public Returns_Detail(VerfijnSubtypeNaarFeitelijkeVerenigingContext context): base(context)
+    {
+        _context = context;
+    }
+
+    [Fact]
+    public void With_Context()
+    {
+        Response.Context.ShouldCompare("http://127.0.0.1:11003/v1/contexten/beheer/detail-vereniging-context.json");
+    }
+
+    [Fact]
+    public void JsonContentMatches()
+    {
+        var expected = new SubtypeData
+        {
+            //id = JsonLdType.Subtype.CreateWithIdValues(_context.VCode, "1"),
+            //type = JsonLdType.Subtype.Type,
+            Subtype = new AssociationRegistry.Admin.Api.Verenigingen.Detail.ResponseModels.Subtype()
+            {
+                Code = Subtype.FeitelijkeVereniging.Code,
+                Naam = Subtype.FeitelijkeVereniging.Naam
+            },
+        };
+
+        Response.Vereniging.Subtype.Should().BeEquivalentTo(expected);
+    }
+
+    public override Func<IApiSetup, DetailVerenigingResponse> GetResponse
+        => setup => setup.AdminApiHost.GetBeheerDetail(TestContext.VCode);
+}
