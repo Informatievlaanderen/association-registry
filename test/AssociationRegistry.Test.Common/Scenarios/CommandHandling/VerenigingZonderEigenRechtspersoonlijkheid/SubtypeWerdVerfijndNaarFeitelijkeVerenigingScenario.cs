10cfe8a5e8df93f6abaa4058262de727e06ddcb3
emalfroy <egon.malfroy@gmail.com>
2025-03-04 15:12:50 +0100

    test: or-2671 when already subtype fv then no event is saved

---
+++ test/AssociationRegistry.Test.Common/Scenarios/CommandHandling/VerenigingZonderEigenRechtspersoonlijkheid/SubtypeWerdVerfijndNaarFeitelijkeVerenigingScenario.cs
@@ -0,0 +1,30 @@
+namespace AssociationRegistry.Test.Common.Scenarios.CommandHandling.VerenigingZonderEigenRechtspersoonlijkheid;
+
+using AssociationRegistry.Events;
+using AssociationRegistry.Test.Common.AutoFixture;
+using AssociationRegistry.Vereniging;
+using global::AutoFixture;
+
+public class SubtypeWerdVerfijndNaarFeitelijkeVerenigingScenario : CommandhandlerScenarioBase
+{
+    public override VCode VCode => VCode.Create("V0009008");
+    public readonly VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd;
+    public readonly SubtypeWerdVerfijndNaarFeitelijkeVereniging SubtypeWerdVerfijndNaarFeitelijkeVereniging;
+    public SubtypeWerdVerfijndNaarFeitelijkeVerenigingScenario()
+    {
+        var fixture = new Fixture().CustomizeAdminApi();
+        VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd = fixture.Create<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd>() with { VCode = VCode };
+
+        SubtypeWerdVerfijndNaarFeitelijkeVereniging = fixture.Create<SubtypeWerdVerfijndNaarFeitelijkeVereniging>() with
+        {
+            VCode = VCode,
+            Subtype = new Registratiedata.Subtype(Subtype.FeitelijkeVereniging.Code, Subtype.FeitelijkeVereniging.Naam)
+        };
+    }
+
+    public override IEnumerable<IEvent> Events()
+        => new IEvent[]
+        {
+            VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd,SubtypeWerdVerfijndNaarFeitelijkeVereniging
+        };
+}
