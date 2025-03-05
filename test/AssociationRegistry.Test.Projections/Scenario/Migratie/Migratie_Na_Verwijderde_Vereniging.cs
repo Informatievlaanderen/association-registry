f85342b7df215e5cb6a1e73fc0dc832a37a931a0
emalfroy <egon.malfroy@gmail.com>
2025-03-05 09:49:15 +0100

    fix: or-2670 handle migration for deleted verenigingen in insz projection

---
+++ test/AssociationRegistry.Test.Projections/Scenario/Migratie/Migratie_Na_Verwijderde_Vereniging.cs
@@ -0,0 +1,31 @@
+namespace AssociationRegistry.Test.Projections.Scenario.Migratie;
+
+using Events;
+using AssociationRegistry.Test.Common.AutoFixture;
+using AutoFixture;
+
+public class Migratie_Na_Verwijderde_Vereniging : InszScenarioBase
+{
+    public readonly FeitelijkeVerenigingWerdGeregistreerd FeitelijkeVerenigingWerdGeregistreerd;
+    public readonly VerenigingWerdVerwijderd VerenigingWerdVerwijderd;
+    public readonly FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid;
+
+    public Migratie_Na_Verwijderde_Vereniging()
+    {
+        var fixture = new Fixture().CustomizeAdminApi();
+
+        FeitelijkeVerenigingWerdGeregistreerd = fixture.Create<FeitelijkeVerenigingWerdGeregistreerd>();
+        VerenigingWerdVerwijderd = fixture.Create<VerenigingWerdVerwijderd>();
+
+        FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid = new FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid(VCode: FeitelijkeVerenigingWerdGeregistreerd.VCode);
+    }
+
+    public override string VCode => FeitelijkeVerenigingWerdGeregistreerd.VCode;
+    public override string Insz => FeitelijkeVerenigingWerdGeregistreerd.Vertegenwoordigers.First().Insz;
+
+    public override EventsPerVCode[] Events =>
+    [
+        new(VCode, [FeitelijkeVerenigingWerdGeregistreerd,VerenigingWerdVerwijderd, FeitelijkeVerenigingWerdGemigreerdNaarVerenigingZonderEigenRechtspersoonlijkheid])
+    ];
+}
+
