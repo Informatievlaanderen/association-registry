dc07d98f8bc0a5b6b2cef57492023c02f3ece451
emalfroy <egon.malfroy@gmail.com>
2025-03-12 13:18:48 +0100

    test: or-2671 set up e2e tests

---
+++ test/AssociationRegistry.Test.E2E/Scenarios/Givens/VerenigingZonderEigenRechtspersoonlijkheid/VzerAndKboVerenigingWerdenGeregistreerdScenario.cs
@@ -0,0 +1,48 @@
+namespace AssociationRegistry.Test.E2E.Scenarios.Givens.VerenigingZonderEigenRechtspersoonlijkheid;
+
+using AssociationRegistry.Framework;
+using AutoFixture;
+using Common.AutoFixture;
+using Events;
+using EventStore;
+using Framework.TestClasses;
+using Vereniging;
+
+public class VzerAndKboVerenigingWerdenGeregistreerdScenario : IScenario
+{
+    public VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd { get; set; }
+    public VerenigingMetRechtspersoonlijkheidWerdGeregistreerd VerenigingMetRechtspersoonlijkheidWerdGeregistreerd { get; set; }
+
+    private CommandMetadata Metadata;
+
+    public VzerAndKboVerenigingWerdenGeregistreerdScenario()
+    {
+    }
+
+    public async Task<KeyValuePair<string, IEvent[]>[]> GivenEvents(IVCodeService service)
+    {
+        var fixture = new Fixture().CustomizeAdminApi();
+
+        VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd = fixture.Create<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd>() with
+        {
+            VCode = await service.GetNext(),
+        };
+        VerenigingMetRechtspersoonlijkheidWerdGeregistreerd = fixture.Create<VerenigingMetRechtspersoonlijkheidWerdGeregistreerd>() with
+        {
+            VCode = await service.GetNext(),
+        };
+
+        Metadata = fixture.Create<CommandMetadata>() with { ExpectedVersion = null };
+
+        return
+        [
+            new(VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd.VCode, [VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd]),
+            new(VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.VCode, [VerenigingMetRechtspersoonlijkheidWerdGeregistreerd]),
+        ];
+    }
+
+    public StreamActionResult Result { get; set; } = null!;
+
+    public CommandMetadata GetCommandMetadata()
+        => Metadata;
+}
---
+++ test/AssociationRegistry.Test.E2E/When_SubtypeWerdVerfijndNaarSubvereniging/Beheer/Detail/With_Header/Returns_Detail.cs
@@ -0,0 +1,57 @@
+﻿namespace AssociationRegistry.Test.E2E.When_SubtypeWerdVerfijndNaarSubvereniging.Beheer.Detail.With_Header;
+
+using AssociationRegistry.Admin.Api;
+using AssociationRegistry.Admin.Api.Verenigingen.Detail.ResponseModels;
+using AssociationRegistry.Admin.Api.Verenigingen.Subtype.RequestModels;
+using AssociationRegistry.Test.E2E.Framework.AlbaHost;
+using AssociationRegistry.Test.E2E.Framework.ApiSetup;
+using AssociationRegistry.Test.E2E.Framework.TestClasses;
+using FluentAssertions;
+using KellermanSoftware.CompareNetObjects;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Verenigingssubtype = Admin.Api.Verenigingen.Detail.ResponseModels.Verenigingssubtype;
+
+[Collection(FullBlownApiCollection.Name)]
+public class Returns_Detail : End2EndTest<VerfijnSubtypeNaarFeitelijkeVerenigingContext, WijzigSubtypeRequest, DetailVerenigingResponse>
+{
+    private readonly VerfijnSubtypeNaarFeitelijkeVerenigingContext _context;
+
+    public Returns_Detail(VerfijnSubtypeNaarFeitelijkeVerenigingContext context): base(context)
+    {
+        _context = context;
+    }
+
+    [Fact]
+    public void With_Context()
+    {
+        Response.Context.ShouldCompare("http://127.0.0.1:11003/v1/contexten/beheer/detail-vereniging-context.json");
+    }
+
+    [Fact]
+    public void JsonContentMatches()
+    {
+        var expected = new Verenigingssubtype()
+        {
+            Code = Vereniging.Verenigingssubtype.FeitelijkeVereniging.Code,
+            Naam = Vereniging.Verenigingssubtype.FeitelijkeVereniging.Naam
+        };
+
+        Response.Vereniging.Verenigingssubtype.Should().BeEquivalentTo(expected);
+    }
+
+    public override Func<IApiSetup, DetailVerenigingResponse> GetResponse
+    {
+        get { return setup =>
+        {
+            var logger = setup.AdminApiHost.Services.GetRequiredService<ILogger<Program>>();
+
+            logger.LogInformation("EXECUTING GET REQUEST");
+
+            return setup.AdminApiHost.GetBeheerDetailWithHeader(setup.SuperAdminHttpClient, TestContext.RequestResult.VCode,
+                                                                TestContext.RequestResult.Sequence)
+                        .GetAwaiter().GetResult();
+        }; }
+    }
+}
