1ea59c2785eee4b9a818f25c81df84adcaafaa7c
emalfroy <egon.malfroy@gmail.com>
2025-03-25 09:46:00 +0100

    fix: or-2675 remove feitelijke vereniging from documentation acm

--- src/AssociationRegistry.Acm.Api/VerenigingenPerInsz/VerenigingenPerInszController.cs
+++ src/AssociationRegistry.Acm.Api/VerenigingenPerInsz/VerenigingenPerInszController.cs
@@ -20,7 +20,7 @@ using ProblemDetails = Be.Vlaanderen.Basisregisters.BasicApiProblem.ProblemDetai
 [ApiVersion("1.0")]
 [AdvertiseApiVersions("1.0")]
 [ApiRoute("verenigingen")]
-[ApiExplorerSettings(GroupName = "Decentraal beheer van feitelijke verenigingen")]
+[ApiExplorerSettings(GroupName = "Decentraal beheer van verenigingen")]
 public class VerenigingenPerInszController : ApiController
 {
     /// <summary>
