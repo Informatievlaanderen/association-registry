576784f9e0229e19e6687a53af1f8cdabf007b95
emalfroy <egon.malfroy@gmail.com>
2025-03-27 10:22:09 +0100

    fix: or-2672 set subverenigingVan to default when changing from subtype subvereniging

---
+++ test/AssociationRegistry.Test.Common/Scenarios/CommandHandling/VerenigingZonderEigenRechtspersoonlijkheid/SubverenigingWerdTerugGezetNaarNietBepaaldScenario.cs
@@ -0,0 +1,34 @@
+namespace AssociationRegistry.Test.Common.Scenarios.CommandHandling.VerenigingZonderEigenRechtspersoonlijkheid;
+
+using AutoFixture;
+using Events;
+using global::AutoFixture;
+using Vereniging;
+
+public class SubverenigingWerdTerugGezetNaarNietBepaaldScenario : CommandhandlerScenarioBase
+{
+    public override VCode VCode => VCode.Create("V0009002");
+    public readonly VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd;
+    public readonly VerenigingssubtypeWerdVerfijndNaarSubvereniging VerenigingssubtypeWerdVerfijndNaarSubvereniging;
+    public readonly VerenigingssubtypeWerdTerugGezetNaarNietBepaald VerenigingssubtypeWerdTerugGezetNaarNietBepaald;
+
+    public SubverenigingWerdTerugGezetNaarNietBepaaldScenario(VCode vCodeAndereVereniging)
+    {
+        var fixture = new Fixture().CustomizeAdminApi();
+        VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd = fixture.Create<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd>() with { VCode = VCode };
+
+        VerenigingssubtypeWerdVerfijndNaarSubvereniging = new VerenigingssubtypeWerdVerfijndNaarSubvereniging(
+            VCode,
+            new Registratiedata.SubverenigingVan(vCodeAndereVereniging, fixture.Create<string>(), fixture.Create<string>(), fixture.Create<string>()));
+
+        VerenigingssubtypeWerdTerugGezetNaarNietBepaald = new VerenigingssubtypeWerdTerugGezetNaarNietBepaald(VCode);
+    }
+
+    public override IEnumerable<IEvent> Events()
+        => new IEvent[]
+        {
+            VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd,
+            VerenigingssubtypeWerdVerfijndNaarSubvereniging,
+            VerenigingssubtypeWerdTerugGezetNaarNietBepaald
+        };
+}
