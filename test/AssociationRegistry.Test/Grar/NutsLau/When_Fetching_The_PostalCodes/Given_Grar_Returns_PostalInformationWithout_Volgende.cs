bf24dcabcf63bd747bc30ef4e2254c632bd5c951
emalfroy <egon.malfroy@gmail.com>
2025-04-07 16:40:36 +0200

    feat: or-2711 fetch all postcodes from grar

---
+++ test/AssociationRegistry.Test/Grar/NutsLau/When_Fetching_The_PostalCodes/Given_Grar_Returns_PostalInformationWithout_Volgende.cs
@@ -0,0 +1,30 @@
+﻿namespace AssociationRegistry.Test.Grar.NutsLau.When_Fetching_The_PostalCodes;
+
+using AssociationRegistry.Grar.Clients;
+using AssociationRegistry.Grar.Models.PostalInfo;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+public class Given_Grar_Returns_PostalInformationWithout_Volgende
+{
+    [Fact]
+    public async Task Then_Returns_Postcodes()
+    {
+        var grarClient = new Mock<IGrarClient>();
+
+        string[] postcodes = ["8000", "9000"];
+
+        grarClient.Setup(x => x.GetPostalInformationList("0", "100"))
+                  .ReturnsAsync(new PostcodesLijstResponse()
+                   {
+                       Postcodes = postcodes,
+                       VolgendeOffset = null,
+                       VolgendeLimit = null,
+                   });
+
+        var sut = new PostcodesFromGrarFetcher(grarClient.Object);
+        var actual = await sut.FetchPostalCodes();
+        actual.Should().BeEquivalentTo(postcodes);
+    }
+}
