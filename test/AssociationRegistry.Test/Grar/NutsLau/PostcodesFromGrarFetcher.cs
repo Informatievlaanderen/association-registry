f6155d7ea9821e0806bef7b4d214859bef366661
emalfroy <egon.malfroy@gmail.com>
2025-04-07 16:40:36 +0200

    feat: or-2711 fetch all postcodes from grar

---
+++ test/AssociationRegistry.Test/Grar/NutsLau/PostcodesFromGrarFetcher.cs
@@ -0,0 +1,43 @@
+﻿namespace AssociationRegistry.Test.Grar.NutsLau;
+
+using AssociationRegistry.Grar.Clients;
+using AssociationRegistry.Grar.Models.PostalInfo;
+
+public class PostcodesFromGrarFetcher:  IPostcodesFromGrarFetcher
+{
+    private readonly IGrarClient _client;
+
+    public PostcodesFromGrarFetcher(IGrarClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<string[]> FetchPostalCodes()
+    {
+        var postalInformationList = await _client.GetPostalInformationList("0", "100");
+
+        if (postalInformationList is null)
+            return Array.Empty<string>();
+
+        var postcodes = postalInformationList.Postcodes.ToList();
+
+        while (HasVolgende(postalInformationList))
+        {
+            postalInformationList =
+                await _client.GetPostalInformationList(postalInformationList!.VolgendeOffset!, postalInformationList!.VolgendeLimit!);
+
+            if (postalInformationList is not null)
+                postcodes.AddRange(postalInformationList.Postcodes);
+        }
+
+        return postcodes.ToArray();
+    }
+
+    private static bool HasVolgende(PostcodesLijstResponse? postalInformationList)
+        => postalInformationList?.VolgendeOffset is not null && postalInformationList?.VolgendeLimit is not null;
+}
+
+public interface IPostcodesFromGrarFetcher
+{
+    Task<string[]> FetchPostalCodes();
+}
---
+++ test/AssociationRegistry.Test/Grar/NutsLau/When_Fetching_The_PostalCodes/Given_Grar_Returns_PostalInformationWith_Volgende.cs
@@ -0,0 +1,43 @@
+﻿namespace AssociationRegistry.Test.Grar.NutsLau.When_Fetching_The_PostalCodes;
+
+using AssociationRegistry.Grar.Clients;
+using AssociationRegistry.Grar.Models.PostalInfo;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+public class Given_Grar_Returns_PostalInformationWith_Volgende
+{
+    [Fact]
+    public async Task Then_Returns_Postcodes_With_A_Next_Batch_Of_Postal_Codes()
+    {
+        var grarClient = new Mock<IGrarClient>();
+
+        string[] firstBatchPostcodes = ["1500", "1501"];
+        var firstBatchOffset = "0";
+        var firstBatchLimit = "100";
+
+        string[] nextBatchPostcodes = ["8000", "9000"];
+        var nextBatchOffset = "100";
+        var nextBatchLimit = "200";
+
+        SetupGrarClient(grarClient, firstBatchOffset, firstBatchLimit, nextBatchOffset, nextBatchLimit, firstBatchPostcodes);
+        SetupGrarClient(grarClient, nextBatchOffset, nextBatchLimit, null, null, nextBatchPostcodes);
+
+        var sut = new PostcodesFromGrarFetcher(grarClient.Object);
+        var actual = await sut.FetchPostalCodes();
+
+        actual.Should().BeEquivalentTo(firstBatchPostcodes.Concat(nextBatchPostcodes));
+    }
+
+    private static void SetupGrarClient(Mock<IGrarClient> grarClient, string offset, string limit, string nextOffset, string nextLimit, string[] batchPostcodes)
+    {
+        grarClient.Setup(x => x.GetPostalInformationList(offset, limit))
+                  .ReturnsAsync(new PostcodesLijstResponse()
+                   {
+                       Postcodes = batchPostcodes,
+                       VolgendeOffset = nextOffset,
+                       VolgendeLimit = nextLimit,
+                   });
+    }
+}
