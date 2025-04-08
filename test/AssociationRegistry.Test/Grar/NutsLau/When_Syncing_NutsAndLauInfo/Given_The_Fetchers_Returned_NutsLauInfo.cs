94691dd10e64dc87952387852f7e1c483dd7af3e
emalfroy <egon.malfroy@gmail.com>
2025-04-08 10:45:41 +0200

    or-2711 implement nuts lau syncing service

---
+++ test/AssociationRegistry.Test/Grar/NutsLau/When_Syncing_NutsAndLauInfo/Given_The_Fetchers_Returned_NutsLauInfo.cs
@@ -0,0 +1,43 @@
+﻿namespace AssociationRegistry.Test.Grar.NutsLau.When_Syncing_NutsAndLauInfo;
+
+using AssociationRegistry.Test.Common.AutoFixture;
+using AutoFixture;
+using Marten;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Xunit;
+
+public class Given_The_Fetchers_Returned_NutsLauInfo
+{
+    [Fact]
+    public async Task Then_Stores_NutsAndLauInfo()
+    {
+
+        var postcodesFromGrarFetcher = new Mock<IPostcodesFromGrarFetcher>();
+        var nutsLauFromGrarFetcher = new Mock<INutsLauFromGrarFetcher>();
+
+        var postalNutslauInfos = SetupMocks(postcodesFromGrarFetcher, nutsLauFromGrarFetcher);
+
+        var documentSession = new Mock<IDocumentSession>();
+        var sut = new NutsAndLauSyncService(nutsLauFromGrarFetcher.Object, postcodesFromGrarFetcher.Object, documentSession.Object, NullLogger<NutsAndLauSyncService>.Instance);
+
+        await sut.SyncNutsLauInfo();
+
+        documentSession.Verify(x => x.Store(postalNutslauInfos), Times.Once);
+        documentSession.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    private PostalNutsLauInfo[] SetupMocks(
+        Mock<IPostcodesFromGrarFetcher> postcodesFromGrarFetcher,
+        Mock<INutsLauFromGrarFetcher> nutsLauFromGrarFetcher)
+    {
+        var fixture = new Fixture().CustomizeDomain();
+        var postalCodes = fixture.CreateMany<string>().ToArray();
+        var postalNutslauInfos = fixture.CreateMany<PostalNutsLauInfo>().ToArray();
+        postcodesFromGrarFetcher.Setup(x => x.FetchPostalCodes())
+                                .ReturnsAsync(postalCodes);
+        nutsLauFromGrarFetcher.Setup(x => x.GetFlemishNutsAndLauByPostcode(postalCodes))
+                              .ReturnsAsync(postalNutslauInfos);
+        return postalNutslauInfos;
+    }
+}
