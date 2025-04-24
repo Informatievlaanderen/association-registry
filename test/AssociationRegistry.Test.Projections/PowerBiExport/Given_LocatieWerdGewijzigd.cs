47a61aaf9671d05a4b3319ff0839c3cdc4e4deba
Koen Metsu <koen@metsu.be>
2025-04-24 14:51:21 +0200

    test: or-2722 add test for locatie wijziging in powerbi projections

---
+++ test/AssociationRegistry.Test.Projections/PowerBiExport/Given_LocatieWerdGewijzigd.cs
@@ -0,0 +1,16 @@
+namespace AssociationRegistry.Test.Projections.PowerBiExport;
+
+using Events;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_LocatieWerdGewijzigd(PowerBiScenarioFixture<LocatieWerdGewijzigdEventsScenario> fixture) : PowerBiScenarioClassFixture<LocatieWerdGewijzigdEventsScenario>
+{
+    [Fact]
+    public void TheLocatieWerdGewijzigd()
+    {
+        var locatie = fixture.Result.Locaties.SingleOrDefault(x => x.LocatieId == fixture.Scenario.LocatieWerdGewijzigd.Locatie.LocatieId);
+
+        locatie.Should().NotBeNull();
+        locatie!.Naam.Should().Be(fixture.Scenario.LocatieWerdGewijzigd.Locatie.Naam);
+    }
+}
