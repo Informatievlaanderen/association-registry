a9c87eb8de7e237e746ccb657a5f49313a44ac53
emalfroy <egon.malfroy@gmail.com>
2025-05-07 16:41:33 +0200

    fix: or-2784 return unexpected aggregate version when loading an aggregate with higher version

---
+++ test/AssociationRegistry.Test/EventStore/When_Loading_An_Aggregate/Given_An_Higer_Version.cs
@@ -0,0 +1,67 @@
+namespace AssociationRegistry.Test.When_Loading_An_Aggregate;
+
+using AssociationRegistry.Framework;
+using AutoFixture;
+using AutoFixture.Kernel;
+using Common.AutoFixture;
+using Common.Framework;
+using Events;
+using EventStore;
+using Microsoft.Extensions.Logging.Abstractions;
+using NodaTime;
+using Vereniging;
+using Xunit;
+using Xunit.Categories;
+
+[IntegrationTest]
+public class Given_An_Higer_Version
+{
+    private readonly Fixture _fixture;
+    private readonly EventConflictResolver _conflictResolver;
+
+    public Given_An_Higer_Version()
+    {
+        _fixture = new Fixture().CustomizeAdminApi();
+
+        _conflictResolver = new EventConflictResolver(
+            new IEventPreConflictResolutionStrategy[]
+            {
+                new AddressMatchConflictResolutionStrategy(),
+            },
+            new IEventPostConflictResolutionStrategy[]
+            {
+                new AddressMatchConflictResolutionStrategy(),
+            });
+
+
+    }
+
+    [Theory]
+    [InlineData(typeof(FeitelijkeVerenigingWerdGeregistreerd))]
+    [InlineData(typeof(VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd))]
+    public async Task Then_it_Throws_Exception(Type verenigingType)
+    {
+        var context = new SpecimenContext(_fixture);
+        var verenigingWerdGeregistreerd = (IVerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd)context.Resolve(verenigingType);
+        var vCode = VCode.Create(verenigingWerdGeregistreerd.VCode);
+
+        var eventStore = await SetupEventStore(vCode, verenigingWerdGeregistreerd);
+
+        await Assert.ThrowsAsync<UnexpectedAggregateVersionException>(() => eventStore.Load<VerenigingState>(vCode, expectedVersion: 999));
+    }
+
+    private async Task<EventStore> SetupEventStore(VCode vCode, IVerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd verenigingWerdGeregistreerd)
+    {
+        var documentStore = await TestDocumentStoreFactory.CreateAsync(nameof(Given_An_Higer_Version));
+
+        await using var session = documentStore.LightweightSession();
+        var eventStore = new EventStore(documentStore, _conflictResolver, NullLogger<EventStore>.Instance);
+        var locatieWerdToegevoegd = _fixture.Create<LocatieWerdToegevoegd>();
+
+        await eventStore.Save(vCode, EventStore.ExpectedVersion.NewStream, session, new CommandMetadata(Initiator: "brol", Instant.MinValue, Guid.NewGuid()),
+                              CancellationToken.None,
+                              (dynamic)verenigingWerdGeregistreerd, locatieWerdToegevoegd);
+
+        return eventStore;
+    }
+}
