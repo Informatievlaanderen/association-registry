6a0c45f8e41be4b9acd7f1cdb46d3811459c6164
emalfroy <egon.malfroy@gmail.com>
2025-05-13 14:58:02 +0200

    test: registreer met rechtspersoon

--- test/AssociationRegistry.Test.E2E/When_Registreer_VerenigingMetRechtsperoonslijkheid/Beheer/Zoeken/Without_Header/Returns_ZoekResponse.cs
+++ test/AssociationRegistry.Test.E2E/When_Registreer_VerenigingMetRechtsperoonslijkheid/Beheer/Zoeken/Without_Header/Returns_ZoekResponse.cs
@@ -1,33 +1,24 @@
 ﻿namespace AssociationRegistry.Test.E2E.When_Registreer_VerenigingMetRechtsperoonslijkheid.Beheer.Zoeken.Without_Header;
 
-using Admin.Api.Verenigingen.Registreer.MetRechtspersoonlijkheid.RequestModels;
-using AssociationRegistry.Admin.Api.Verenigingen.Registreer.VerenigingZonderEigenRechtspersoonlijkheid.RequestModels;
-using AssociationRegistry.Admin.Api.Verenigingen.Search.ResponseModels;
-using AssociationRegistry.Formats;
-using AssociationRegistry.JsonLdContext;
-using AssociationRegistry.Test.E2E.Framework.AlbaHost;
-using AssociationRegistry.Test.E2E.Framework.ApiSetup;
-using AssociationRegistry.Test.E2E.Framework.Comparison;
-using AssociationRegistry.Test.E2E.Framework.Mappers;
-using AssociationRegistry.Test.E2E.Framework.TestClasses;
+using Admin.Api.Verenigingen.Search.ResponseModels;
 using FluentAssertions;
+using Framework.AlbaHost;
+using Framework.ApiSetup;
+using Framework.TestClasses;
 using KellermanSoftware.CompareNetObjects;
-using NodaTime;
-using When_Registreer_VerenigingZonderEigenRechtspersoonlijkheid;
 using Xunit;
-using Vereniging = Admin.Api.Verenigingen.Search.ResponseModels.Vereniging;
-using VerenigingStatus = Admin.Schema.Constants.VerenigingStatus;
-using Verenigingstype = Admin.Api.Verenigingen.Search.ResponseModels.Verenigingstype;
 
-[Collection(FullBlownApiCollection.Name)]
-public class Returns_SearchVerenigingenResponse : End2EndTest<RegistreerVerenigingMetRechtsperoonlijkheidTestContext, RegistreerVerenigingUitKboRequest, SearchVerenigingenResponse>
+[Collection(nameof(RegistreerVerenigingMetRechtsperoonlijkheidCollection))]
+public class Returns_Vereniging : End2EndTest<SearchVerenigingenResponse>
 {
-    private readonly RegistreerVerenigingMetRechtsperoonlijkheidTestContext _testContext;
-
-    public Returns_SearchVerenigingenResponse(RegistreerVerenigingMetRechtsperoonlijkheidTestContext testContext)
+    private readonly RegistreerVerenigingMetRechtsperoonlijkheidContext _testContext;
+    public Returns_Vereniging(RegistreerVerenigingMetRechtsperoonlijkheidContext testContext) : base(testContext.ApiSetup)
     {
-        TestContext = _testContext = testContext;
+        _testContext = testContext;
     }
+
+    public override SearchVerenigingenResponse GetResponse(FullBlownApiSetup setup)
+        => setup.AdminApiHost.GetBeheerZoeken($"vCode:{_testContext.CommandResult.VCode}");
 
     [Fact]
     public void With_Context()
@@ -38,7 +29,4 @@ public class Returns_SearchVerenigingenResponse : End2EndTest<RegistreerVerenigi
     [Fact]
     public async ValueTask WithFeitelijkeVereniging()
         => Response.Verenigingen.Single().Verenigingssubtype.Should().BeNull();
-
-    public override Func<IApiSetup, SearchVerenigingenResponse> GetResponse
-        => setup => setup.AdminApiHost.GetBeheerZoeken($"vCode:{_testContext.CommandResult.VCode}");
 }
