833bf8a13e160aa6ea70fae150cec5f71938675e
emalfroy <egon.malfroy@gmail.com>
2025-05-14 11:11:48 +0200

    api started

---
+++ test/AssociationRegistry.Test.E2E/When_The_Api_Is_Started/ApiStartedContext.cs
@@ -0,0 +1,31 @@
+﻿namespace AssociationRegistry.Test.E2E.When_The_Api_Is_Started;
+
+using Framework.ApiSetup;
+using Framework.TestClasses;
+using Scenarios.Givens;
+using Scenarios.Requests;
+using When_Messages_Are_Queued;
+using Xunit;
+
+// CollectionFixture for database setup ==> Context
+[CollectionDefinition(nameof(ApiStartedCollection))]
+public class ApiStartedCollection : ICollectionFixture<MessageQueueContext>
+{
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
+public class ApiStartedContext : TestContextBase<EmptyScenario, NullRequest>
+{
+    protected override EmptyScenario InitializeScenario()
+        => new();
+
+    public ApiStartedContext(FullBlownApiSetup apiSetup): base(apiSetup)
+    {
+        CommandResult = CommandResult<NullRequest>.NullCommandResult();
+    }
+
+    protected override async ValueTask ExecuteScenario(EmptyScenario scenario)
+    {
+    }
+}
