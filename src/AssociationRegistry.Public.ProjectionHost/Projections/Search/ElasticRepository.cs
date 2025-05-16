483c9099321cd8d537290e27475330da81671970
emalfroy <109064991+emalfroy@users.noreply.github.com>
2025-05-16 08:39:20 +0200

    Revert "retry when elastic fails"
    This reverts commit 25dffced3a200fb50ed9668301f3182b6e144796.

--- src/AssociationRegistry.Public.ProjectionHost/Projections/Search/ElasticRepository.cs
+++ src/AssociationRegistry.Public.ProjectionHost/Projections/Search/ElasticRepository.cs
@@ -122,7 +122,7 @@ public class ElasticRepository : IElasticRepository
     {
         var response = await _elasticClient.UpdateAsync<VerenigingZoekDocument>(
             id,
-            selector: u => u.RetryOnConflict(3).Script(
+            selector: u => u.Script(
                 s => s
                     .Source("if (ctx._source.sequence == null || params.seq > ctx._source.sequence) {" +
                             "ctx._source.locaties.add(params.locatie);" +
