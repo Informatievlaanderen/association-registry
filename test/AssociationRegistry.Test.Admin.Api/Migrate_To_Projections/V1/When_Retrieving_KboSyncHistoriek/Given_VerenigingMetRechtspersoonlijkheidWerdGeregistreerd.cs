08e79e15bdc0da3c27b1272445114aeeb92ba755
emalfroy <109064991+emalfroy@users.noreply.github.com>
2025-05-16 08:39:20 +0200

    Revert "fix kbo sync test"
    This reverts commit 64cd2930e121edf7fa38976774263b849207fef3.

--- test/AssociationRegistry.Test.Admin.Api/Migrate_To_Projections/V1/When_Retrieving_KboSyncHistoriek/Given_VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.cs
+++ test/AssociationRegistry.Test.Admin.Api/Migrate_To_Projections/V1/When_Retrieving_KboSyncHistoriek/Given_VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.cs
@@ -1,13 +1,11 @@
 ﻿namespace AssociationRegistry.Test.Admin.Api.Migrate_To_Projections.V1.When_Retrieving_KboSyncHistoriek;
 
-using AssociationRegistry.Admin.Api.Verenigingen.KboSync.ResponseModels;
 using AssociationRegistry.Formats;
 using AssociationRegistry.Test.Admin.Api.Framework.Fixtures;
 using AssociationRegistry.Test.Admin.Api.Framework.templates.kboSyncHistoriek;
 using AssociationRegistry.Test.Common.Extensions;
 using AssociationRegistry.Test.Common.Scenarios.EventsInDb;
 using FluentAssertions;
-using Newtonsoft.Json;
 using Xunit;
 
 [Collection(nameof(AdminApiCollection))]
@@ -28,31 +26,26 @@ public class Given_VerenigingMetRechtspersoonlijkheidWerdGeregistreerd
     public async ValueTask Then_we_get_registreer_inschrijving_gebeurtenis()
     {
         var content = await _response.Content.ReadAsStringAsync();
-        var response = JsonConvert.DeserializeObject<KboSyncHistoriekResponse>(content);
 
-        List<KboSyncHistoriekGebeurtenisResponse> expected =
-        [
-            new KboSyncHistoriekGebeurtenisResponse()
-            {
-                VCode = _inschrijvingZonderSync.VCode,
-                Beschrijving = "Registreer inschrijving geslaagd",
-                Kbonummer = _inschrijvingZonderSync.KboNummer,
-                Tijdstip = _inschrijvingZonderSync.GetCommandMetadata().Tijdstip.FormatAsZuluTime(),
-            },new KboSyncHistoriekGebeurtenisResponse()
-            {
-                VCode = _inschrijvingMetSync.VCode,
-                Beschrijving = "Registreer inschrijving geslaagd",
-                Kbonummer = _inschrijvingMetSync.VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.KboNummer,
-                Tijdstip = _inschrijvingMetSync.GetCommandMetadata().Tijdstip.FormatAsZuluTime(),
-            },new KboSyncHistoriekGebeurtenisResponse()
-            {
-                VCode = _inschrijvingMetSync.VCode,
-                Beschrijving = "Vereniging succesvol up to date gebracht met data uit de KBO",
-                Kbonummer = _inschrijvingMetSync.VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.KboNummer,
-                Tijdstip = _inschrijvingMetSync.GetCommandMetadata().Tijdstip.FormatAsZuluTime(),
-            },
-        ];
+        var expected = new KboSyncHistoriekTemplate(
+                new KboSyncHistoriekGebeurtenis(
+                    _inschrijvingZonderSync.KboNummer,
+                    _inschrijvingZonderSync.VCode,
+                    Beschrijving: "Registreer inschrijving geslaagd",
+                    _inschrijvingZonderSync.GetCommandMetadata().Tijdstip.FormatAsZuluTime()),
+                new KboSyncHistoriekGebeurtenis(
+                    _inschrijvingMetSync.VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.KboNummer,
+                    _inschrijvingMetSync.VCode,
+                    Beschrijving: "Registreer inschrijving geslaagd",
+                    _inschrijvingMetSync.GetCommandMetadata().Tijdstip.FormatAsZuluTime()),
+                new KboSyncHistoriekGebeurtenis(
+                    _inschrijvingMetSync.VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.KboNummer,
+                    _inschrijvingMetSync.VCode,
+                    Beschrijving: "Vereniging succesvol up to date gebracht met data uit de KBO",
+                    _inschrijvingMetSync.GetCommandMetadata().Tijdstip.FormatAsZuluTime())
+            )
+           .Build();
 
-        expected.ForEach(x => response.Should().Contain(x));
+        content.Should().BeEquivalentJson(expected);
     }
 }
