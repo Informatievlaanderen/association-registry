a1665c99ed4e02b63702fd6480556797aa4bd619
emalfroy <109064991+emalfroy@users.noreply.github.com>
2025-05-16 08:39:20 +0200

    Revert "when messages are processed"
    This reverts commit 0cc96f30f4d3fa2bc778f655e2ef729a8d18b520.

--- test/AssociationRegistry.Test.E2E/When_Messages_Are_Queued/Via_SQS/When_Sending_An_Incorrect_Message_On_The_Grar_Sync_Queue.cs
+++ test/AssociationRegistry.Test.E2E/When_Messages_Are_Queued/Via_SQS/When_Sending_An_Incorrect_Message_On_The_Grar_Sync_Queue.cs
@@ -10,7 +10,7 @@ using Grar.GrarConsumer.Messaging;
 using Xunit;
 using ITestOutputHelper = Xunit.ITestOutputHelper;
 
-[Collection(nameof(MessageQueueCollection))]
+[Collection(FullBlownApiCollection.Name)]
 public class When_Sending_An_Incorrect_Message_On_The_Grar_Sync_Queue
 {
     private readonly FullBlownApiSetup _setup;
