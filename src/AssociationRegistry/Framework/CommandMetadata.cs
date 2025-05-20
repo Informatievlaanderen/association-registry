e5f0e02fa52fca44cfc79dd7632c6cf1e319ee53
emalfroy <egon.malfroy@gmail.com>
2025-05-20 09:37:33 +0200

    fix: or-2829 make command metadata less static

--- src/AssociationRegistry/Framework/CommandMetadata.cs
+++ src/AssociationRegistry/Framework/CommandMetadata.cs
@@ -4,6 +4,6 @@ using NodaTime;
 
 public record CommandMetadata(string Initiator, Instant Tijdstip, Guid CorrelationId, long? ExpectedVersion = null)
 {
-    public static CommandMetadata ForDigitaalVlaanderenProcess =
+    public static CommandMetadata ForDigitaalVlaanderenProcess =>
         new CommandMetadata(AssociationRegistry.EventStore.EventStore.DigitaalVlaanderenOvoNumber, SystemClock.Instance.GetCurrentInstant(), Guid.NewGuid());
 };
