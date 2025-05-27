a8f330b842139e13036416f46dc80487cd7048fb
Koen Metsu <koen@metsu.be>
2025-05-27 16:03:36 +0200

    refactor: or-2818 move health check

---
+++ src/AssociationRegistry.Admin.Api/Infrastructure/HealthChecks/MigrationHealthCheck.cs
@@ -0,0 +1,34 @@
+namespace AssociationRegistry.Admin.Api.Infrastructure.HealthChecks;
+
+using AssociationRegistry.Admin.Api.HostedServices.GeotagsInitialisation;
+using Marten;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+public class MigrationHealthCheck : IHealthCheck
+{
+    private readonly IDocumentStore _store;
+
+    public MigrationHealthCheck(IDocumentStore store)
+    {
+        _store = store;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            using var session = _store.LightweightSession();
+            var ok = await session.Query<GeotagMigration>()
+                                  .AnyAsync(x => x.Id == new GeotagMigration().Id, cancellationToken);
+            return ok
+                ? HealthCheckResult.Healthy("Migration run")
+                : HealthCheckResult.Unhealthy("Migration not yet run");
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy("Migration check threw exception", ex);
+        }
+    }
+}
