3a8b34e51d0d5b620b1e7bb772891838da6cd531
emalfroy <egon.malfroy@gmail.com>
2025-06-04 10:06:55 +0200

    feat: or-2818 implement GeotagsWerdenBepaald in powerbi

---
+++ test/AssociationRegistry.Test.Projections/PowerBiExport/Given_GeotagsWerdenBepaald.cs
@@ -0,0 +1,14 @@
+﻿namespace AssociationRegistry.Test.Projections.PowerBiExport;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_GeotagsWerdenBepaald(PowerBiScenarioFixture<GeotagsWerdenBepaaldScenario> fixture)
+    : PowerBiScenarioClassFixture<GeotagsWerdenBepaaldScenario>
+{
+    [Fact]
+    public void MetadataIsUpdated()
+    {
+        fixture.Result.Historiek.Should()
+               .ContainSingle(x => x.EventType == "GeotagsWerdenBepaald");
+
+    }
+}
