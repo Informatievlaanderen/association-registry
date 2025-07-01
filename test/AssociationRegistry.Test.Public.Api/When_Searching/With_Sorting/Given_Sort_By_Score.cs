19b4cb421c7934136d51a648d0c7450d6af0e9ed
Koen Metsu <koen@metsu.be>
2025-07-01 11:28:04 +0200

    feat: or-2823 allow sorting by _score

--- test/AssociationRegistry.Test.Public.Api/When_Searching/With_Sorting/Given_Sort_By_Score.cs
+++ test/AssociationRegistry.Test.Public.Api/When_Searching/With_Sorting/Given_Sort_By_Score.cs
@@ -8,6 +8,7 @@ using FluentAssertions;
 using Newtonsoft.Json;
 using Resources;
 using System.Net;
+using Vereniging;
 using Xunit;
 
 [Collection(nameof(PublicApiCollection))]
@@ -16,20 +17,60 @@ public class Given_Sort_By_Score
     private readonly GivenEventsFixture _fixture;
     private readonly ITestOutputHelper _outputHelper;
     private readonly PublicApiClient _publicApiClient;
+    private string _query;
+    private VCode _vCodeOfFullyMatched;
 
     public Given_Sort_By_Score(GivenEventsFixture fixture, ITestOutputHelper outputHelper)
     {
         _fixture = fixture;
         _outputHelper = outputHelper;
         _publicApiClient = fixture.PublicApiClient;
+
+        _query = SetUpQueryWithFullORPartialMatch();
+        _vCodeOfFullyMatched = _fixture.V006VerenigingMetRechtspersoonlijkheidWerdGeregistreerdScenario.VCode;
+    }
+
+    private string SetUpQueryWithFullORPartialMatch()
+    {
+        var exactMatch = _fixture.V006VerenigingMetRechtspersoonlijkheidWerdGeregistreerdScenario.VerenigingMetRechtspersoonlijkheidWerdGeregistreerd.Naam;
+        var partialMatch = _fixture.V003BasisgegevensWerdenGewijzigdScenario.NaamWerdGewijzigd.Naam.Split(" ")[0];
+
+        var query = $"{exactMatch} OR {partialMatch}";
+
+        return query;
+    }
+
+    [Theory]
+    [InlineData("-score")]
+    public async Task? Descending_Returns_FullMatch_First(string sortField)
+    {
+        var response = await _publicApiClient.Search(q: _query, sortField);
+
+        var verenigingen = await AssertVerenigingenWereReturned(response);
+
+        verenigingen.Verenigingen
+                    .First()
+                    .VCode
+                    .Should()
+                    .Be(_vCodeOfFullyMatched);
     }
 
     [Theory]
     [InlineData("score")]
-    public async Task? Then_it_sorts_ascending(string field)
+    public async Task? Descending_Returns_FullMatch_Last(string sortField)
     {
-        var response = await _publicApiClient.Search(q: _fixture.V003BasisgegevensWerdenGewijzigdScenario.NaamWerdGewijzigd.Naam, field);
+        var searchResponse = await _publicApiClient.Search(q: _query, sortField);
 
+        var verenigingen = await AssertVerenigingenWereReturned(searchResponse);
+
+        verenigingen.Verenigingen
+                    .Last()
+                    .VCode
+                    .Should().Be(_vCodeOfFullyMatched);
+    }
+
+    private async Task<SearchVerenigingenResponse?> AssertVerenigingenWereReturned(HttpResponseMessage response)
+    {
         response.Should().BeSuccessful();
 
         var json = await response.Content.ReadAsStringAsync();
@@ -37,9 +78,8 @@ public class Given_Sort_By_Score
 
         var verenigingen = JsonConvert.DeserializeObject<SearchVerenigingenResponse>(json);
 
-        // just a checkup
         verenigingen.Verenigingen.Should().HaveCountGreaterThan(1);
 
-        verenigingen.Verenigingen.First().VCode.Should().Be(_fixture.V003BasisgegevensWerdenGewijzigdScenario.VCode);
+        return verenigingen;
     }
 }
