05ec213626afb4e07c0dc0214960ae9b05d94476
emalfroy <egon.malfroy@gmail.com>
2025-07-18 15:52:54 +0200

    fix: or-2882 allow changes after adresmatch events

--- test/AssociationRegistry.Test.Admin.Api/Framework/Fixtures/MinimalApi/MinimalAdminApiCollection.cs
+++
@@ -1,8 +0,0 @@
-namespace AssociationRegistry.Test.Admin.Api.Framework.Fixtures.MinimalApi;
-
-using Xunit;
-
-[CollectionDefinition(nameof(MinimalAdminApiCollection))]
-public class MinimalAdminApiCollection : ICollectionFixture<MinimalAdminApiFixture>
-{
-}
