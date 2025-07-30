30a6995f8141658d8758b1c153f2ad30f5116961
Koen Metsu <koen@metsu.be>
2025-07-30 14:40:05 +0200

    test: or-2887 verify delete behavior on projection on addresswerdontkoppeld

---
+++ test/AssociationRegistry.Test.Projections/Beheer/LocatieLookup/Given_AdresWerdOntkoppeld.cs
@@ -0,0 +1,20 @@
+﻿namespace AssociationRegistry.Test.Projections.Beheer.LocatieLookup;
+
+using Scenario.Adressen;
+
+[Collection(nameof(ProjectionContext))]
+public class AdresWerdOntkoppeldVanAdressenregister(
+    LocatieLookupScenarioFixture<AdresWerdOntkoppeldVanAdressenregisterScenario> fixture)
+    : LocatieLookupScenarioClassFixture<AdresWerdOntkoppeldVanAdressenregisterScenario>
+{
+    [Fact]
+    public void Adres_Properties_Are_Correctly_Set()
+    {
+        fixture
+                    .Result
+                    .SingleOrDefault(x => x.Id ==
+                                          $"{fixture.Scenario.VCode}-{fixture.Scenario.AdresWerdOntkoppeldVanAdressenregister.LocatieId}")
+                    .Should()
+                    .BeNull();
+    }
+}
