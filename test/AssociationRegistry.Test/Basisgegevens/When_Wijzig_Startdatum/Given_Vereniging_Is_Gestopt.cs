7678a96da9b2a204c763a7f5628f4dc2450a0f3d
emalfroy <egon.malfroy@gmail.com>
2025-08-05 15:06:03 +0200

    test: or-2894 fix flaky tests and skip dlq tests

--- test/AssociationRegistry.Test/Basisgegevens/When_Wijzig_Startdatum/Given_Vereniging_Is_Gestopt.cs
+++ test/AssociationRegistry.Test/Basisgegevens/When_Wijzig_Startdatum/Given_Vereniging_Is_Gestopt.cs
@@ -16,26 +16,27 @@ public class Given_Vereniging_Is_Gestopt
     [Theory]
     [InlineData(typeof(FeitelijkeVerenigingWerdGeregistreerd))]
     [InlineData(typeof(VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd))]
-    public void Then_It_Throws_If_Startdatum_Is_After_Einddatum(Type verenigingType)
+    public void Then_It_Throws_If_Startdatum_Is_After_Einddatum2(Type verenigingType)
     {
         var fixture = new Fixture().CustomizeDomain();
         var context = new SpecimenContext(fixture);
 
         var vereniging = new Vereniging();
 
-        var einddatum = fixture.Create<Datum>();
         var verenigingWerdGeregistreerd = (IVerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerd)context.Resolve(verenigingType);
-        var verenigingWerdGestopt = EventFactory.VerenigingWerdGestopt(einddatum);
+
+        var einddatum = verenigingWerdGeregistreerd.Startdatum.Value.AddDays(10);
+        var verenigingWerdGestopt = EventFactory.VerenigingWerdGestopt(Datum.Hydrate(einddatum));
 
         vereniging.Hydrate(
             new VerenigingState()
                .Apply((dynamic)verenigingWerdGeregistreerd)
                .Apply(verenigingWerdGestopt));
 
-        var clock = new ClockStub(einddatum.Value.AddDays(100));
+        var clockStub = new ClockStub(verenigingWerdGestopt.Einddatum.AddDays(20));
+        var nieuweStartDatum = verenigingWerdGestopt.Einddatum.AddDays(10);
 
-        var startdatum = Datum.Hydrate(einddatum.Value.AddDays(1));
-        var wijzigStartdatum = () => vereniging.WijzigStartdatum(startdatum, clock);
+        var wijzigStartdatum = () => vereniging.WijzigStartdatum(Datum.Hydrate(nieuweStartDatum), clockStub);
 
         wijzigStartdatum.Should().Throw<StartdatumLigtNaEinddatum>();
     }
