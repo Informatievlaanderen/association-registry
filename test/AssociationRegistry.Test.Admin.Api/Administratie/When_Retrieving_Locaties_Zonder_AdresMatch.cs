2ed071e91475e215a3e9fcc1b1b03dc082ec970b
emalfroy <egon.malfroy@gmail.com>
2025-08-26 10:12:32 +0200

    Reapply "feat: or-2901 map wolverine dlq endpoints"
    This reverts commit 39eebf132fa57b903dcfcfbf497bdce985bad3dc.

--- test/AssociationRegistry.Test.Admin.Api/Administratie/When_Retrieving_Locaties_Zonder_AdresMatch.cs
+++ test/AssociationRegistry.Test.Admin.Api/Administratie/When_Retrieving_Locaties_Zonder_AdresMatch.cs
@@ -5,6 +5,8 @@ using FluentAssertions;
 using Framework.Fixtures;
 using Newtonsoft.Json;
 using System.Net;
+using System.Net.Http.Json;
+using Wolverine.Http;
 using Xunit;
 
 [Collection(nameof(AdminApiCollection))]
@@ -35,5 +37,36 @@ public class When_Retrieving_Locaties_Zonder_AdresMatch
         var content = result.Content.ReadAsStringAsync().Result;
         var actual = JsonConvert.DeserializeObject<LocatieZonderAdresMatchDocument[]>(content);
         actual.Should().NotBeEmpty();
+    }
+}
+
+[Collection(nameof(AdminApiCollection))]
+public class When_Querying_Deadletters
+{
+    private readonly EventsInDbScenariosFixture _fixture;
+
+    public When_Querying_Deadletters(EventsInDbScenariosFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Without_SuperAdmin_Returns403Forbidden()
+    {
+        var testClient = _fixture.AdminApiClient.HttpClient;
+        var result = await testClient.PostAsJsonAsync("dead-letters", new DeadLetterEnvelopeGetRequest());
+        result.StatusCode.Should().Be(HttpStatusCode.Forbidden);
+    }
+
+    [Fact]
+    public async Task With_SuperAdmin_ReturnsLocatiesZonderAdresMatch()
+    {
+        var testClient = _fixture.SuperAdminApiClient.HttpClient;
+        var result = await testClient.PostAsJsonAsync("dead-letters", new DeadLetterEnvelopeGetRequest());
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        result.Content.Should().NotBeNull();
+        var content = result.Content.ReadAsStringAsync().Result;
+        var actual = JsonConvert.DeserializeObject<DeadLetterEnvelopesFoundResponse>(content);
+        actual.Should().NotBeNull();
     }
 }
