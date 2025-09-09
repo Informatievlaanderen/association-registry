ee59445003e23626360eef076fdc20cf5ec54daf
emalfroy <egon.malfroy@gmail.com>
2025-09-09 17:09:57 +0200

    feat: or-2876 create a docker per init container for local and e2e

--- migrations/admin.api/scripts/up/00001_anon_to_kbowerdgereserveerd.sql
+++
@@ -1,5 +0,0 @@
-UPDATE public.mt_events
-SET type = 'kbo_nummer_werd_gereserveerd'
-  , mt_dotnet_type = 'AssociationRegistry.Events.KboNummerWerdGereserveerd, AssociationRegistry'
-WHERE type = '<>f__AnonymousType0<string>'
-  AND mt_dotnet_type = '<>f__AnonymousType0`1[[System.String, System.Private.CoreLib, Version=8.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]], AssociationRegistry';
