d6574565285488a4083e17037eb8e84e8bd66573
emalfroy <egon.malfroy@gmail.com>
2025-09-16 10:16:25 +0200

    fix: or-2876 register doc types in admin api

---
+++ src/AssociationRegistry.Admin.Api/00004_something.drop.sql
@@ -0,0 +1,66 @@
+drop function if exists public.mt_upsert_locatiezonderadresmatchdocument(JSONB, varchar, varchar, integer) cascade;
+CREATE OR REPLACE FUNCTION public.mt_upsert_locatiezonderadresmatchdocument(doc jsonb, docdotnettype character varying, docid character varying, revision integer)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  final_version INTEGER;
+  current_version INTEGER;
+BEGIN
+
+SELECT version into current_version FROM public.mt_streams WHERE id = docId ;
+if revision = 0 then
+  if current_version is not null then
+    revision = current_version;
+  else
+    revision = 1;
+  end if;
+else
+  if current_version is not null then
+    if current_version > revision then
+      return 0;
+    end if;
+  end if;
+end if;
+
+INSERT INTO public.mt_doc_locatiezonderadresmatchdocument ("data", "mt_dotnet_type", "id", "mt_version", mt_last_modified) VALUES (doc, docDotNetType, docId, revision, transaction_timestamp())
+  ON CONFLICT (id)
+  DO UPDATE SET "data" = doc, "mt_dotnet_type" = docDotNetType, "mt_version" = revision, mt_last_modified = transaction_timestamp() where revision > public.mt_doc_locatiezonderadresmatchdocument.mt_version;
+
+  SELECT mt_version into final_version FROM public.mt_doc_locatiezonderadresmatchdocument WHERE id = docId ;
+  RETURN final_version;
+END;
+$function$;
+drop function if exists public.mt_upsert_powerbiexportdocument(JSONB, varchar, varchar, integer) cascade;
+CREATE OR REPLACE FUNCTION public.mt_upsert_powerbiexportdocument(doc jsonb, docdotnettype character varying, docid character varying, revision integer)
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  final_version INTEGER;
+  current_version INTEGER;
+BEGIN
+
+SELECT version into current_version FROM public.mt_streams WHERE id = docId ;
+if revision = 0 then
+  if current_version is not null then
+    revision = current_version;
+  else
+    revision = 1;
+  end if;
+else
+  if current_version is not null then
+    if current_version > revision then
+      return 0;
+    end if;
+  end if;
+end if;
+
+INSERT INTO public.mt_doc_powerbiexportdocument ("data", "mt_dotnet_type", "id", "mt_version", mt_last_modified) VALUES (doc, docDotNetType, docId, revision, transaction_timestamp())
+  ON CONFLICT (id)
+  DO UPDATE SET "data" = doc, "mt_dotnet_type" = docDotNetType, "mt_version" = revision, mt_last_modified = transaction_timestamp() where revision > public.mt_doc_powerbiexportdocument.mt_version;
+
+  SELECT mt_version into final_version FROM public.mt_doc_powerbiexportdocument WHERE id = docId ;
+  RETURN final_version;
+END;
+$function$;
