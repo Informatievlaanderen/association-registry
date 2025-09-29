7c31e0fdf051bb57ab2b71b3aa8de61342bbf800
emalfroy <egon.malfroy@gmail.com>
2025-09-29 16:41:56 +0200

    refactor: or-1733 move methods to magdaOndernemingExtensions

--- src/integrations/AssociationRegistry.Integrations.Magda/MagdaGeefVerenigingService.cs
+++ src/integrations/AssociationRegistry.Integrations.Magda/MagdaGeefVerenigingService.cs
@@ -13,17 +13,8 @@ using Models.GeefOnderneming;
 using Onderneming.GeefOnderneming;
 using ResultNet;
 
-
 public class MagdaGeefVerenigingService : IMagdaGeefVerenigingService
 {
-    protected readonly Dictionary<string, Verenigingstype> rechtsvormMap = new()
-    {
-        { RechtsvormCodes.VZW, Verenigingstype.VZW },
-        { RechtsvormCodes.IVZW, Verenigingstype.IVZW },
-        { RechtsvormCodes.PrivateStichting, Verenigingstype.PrivateStichting },
-        { RechtsvormCodes.StichtingVanOpenbaarNut, Verenigingstype.StichtingVanOpenbaarNut },
-    };
-
     protected readonly IMagdaCallReferenceRepository _magdaCallReferenceRepository;
     protected readonly IMagdaClient _magdaClient;
     protected readonly ILogger<MagdaGeefVerenigingService> _logger;
@@ -48,7 +39,8 @@ public class MagdaGeefVerenigingService : IMagdaGeefVerenigingService
             var reference = await CreateReference(_magdaCallReferenceRepository, metadata.Initiator, metadata.CorrelationId, kboNummer,
                                                   cancellationToken);
 
-            _logger.LogInformation($"MAGDA Call Reference - GeefOnderneming Service - KBO nummer '{kboNummer}' met referentie '{reference.Reference}'");
+            _logger.LogInformation(
+                $"MAGDA Call Reference - GeefOnderneming Service - KBO nummer '{kboNummer}' met referentie '{reference.Reference}'");
 
             var magdaResponse = await _magdaClient.GeefOnderneming(kboNummer, reference);
 
@@ -58,34 +50,12 @@ public class MagdaGeefVerenigingService : IMagdaGeefVerenigingService
             var magdaOnderneming = magdaResponse?.Body?.GeefOndernemingResponse?.Repliek.Antwoorden.Antwoord.Inhoud.Onderneming ?? null;
 
             if (magdaOnderneming is null ||
-                !HeeftToegestaneActieveRechtsvorm(magdaOnderneming) ||
-                !IsOnderneming(magdaOnderneming) ||
-                !IsRechtspersoon(magdaOnderneming))
+                !magdaOnderneming.HeeftToegestaneActieveRechtsvorm() ||
+                !magdaOnderneming.IsOnderneming() ||
+                !magdaOnderneming.IsRechtspersoon())
                 return VerenigingVolgensKboResult.GeenGeldigeVereniging;
 
-            var naamOndernemingType = GetBestMatchingNaam(magdaOnderneming.Namen.MaatschappelijkeNamen);
-
-            if (naamOndernemingType is null)
-                return VerenigingVolgensKboResult.GeenGeldigeVereniging;
-
-            var maatschappelijkeZetel =
-                magdaOnderneming.Adressen.SingleOrDefault(a => a.Type.Code.Value == AdresCodes.MaatschappelijkeZetel &&
-                                                               IsActiveToday(a.DatumBegin, a.DatumEinde));
-
-            return VerenigingVolgensKboResult.GeldigeVereniging(
-                new VerenigingVolgensKbo
-                {
-                    KboNummer = KboNummer.Create(kboNummer),
-                    Type = rechtsvormMap[GetActiveRechtsvorm(magdaOnderneming)!.Code.Value],
-                    Naam = naamOndernemingType.Naam ?? string.Empty,
-                    KorteNaam = GetBestMatchingNaam(magdaOnderneming.Namen.AfgekorteNamen)?.Naam ?? string.Empty,
-                    Startdatum = DateOnlyHelper.ParseOrNull(magdaOnderneming.Start.Datum, Formats.DateOnly),
-                    EindDatum = DateOnlyHelper.ParseOrNull(magdaOnderneming.Stopzetting?.Datum, Formats.DateOnly),
-                    IsActief = IsActiefOfInOprichting(magdaOnderneming),
-                    Adres = GetAdresFrom(maatschappelijkeZetel),
-                    Contactgegevens = GetContactgegevensFrom(maatschappelijkeZetel),
-                    Vertegenwoordigers = GetVertegenwoordigers(magdaOnderneming.Functies),
-                });
+            return VerenigingVolgensKboResult.GeldigeVereniging(magdaOnderneming.MapVerenigingVolgensKbo(kboNummer));
         }
         catch (Exception e)
         {
@@ -103,7 +73,8 @@ public class MagdaGeefVerenigingService : IMagdaGeefVerenigingService
             var reference = await CreateReference(_magdaCallReferenceRepository, metadata.Initiator, metadata.CorrelationId, kboNummer,
                                                   cancellationToken);
 
-            _logger.LogInformation($"MAGDA Call Reference - GeefOnderneming Service - KBO nummer '{kboNummer}' met referentie '{reference.Reference}'");
+            _logger.LogInformation(
+                $"MAGDA Call Reference - GeefOnderneming Service - KBO nummer '{kboNummer}' met referentie '{reference.Reference}'");
 
             var magdaResponse = await _magdaClient.GeefOnderneming(kboNummer, reference);
 
@@ -113,156 +84,23 @@ public class MagdaGeefVerenigingService : IMagdaGeefVerenigingService
             var magdaOnderneming = magdaResponse?.Body?.GeefOndernemingResponse?.Repliek.Antwoorden.Antwoord.Inhoud.Onderneming ?? null;
 
             if (magdaOnderneming is null ||
-                !IsOnderneming(magdaOnderneming) ||
-                !IsRechtspersoon(magdaOnderneming))
+                !magdaOnderneming.IsOnderneming() ||
+                !magdaOnderneming.IsRechtspersoon())
                 return VerenigingVolgensKboResult.GeenGeldigeVereniging;
 
-            if (!IsActief(magdaOnderneming))
+            if (!magdaOnderneming.IsActief())
                 return VerenigingVolgensKboResult.InactieveVereniging(new InactieveVereniging()
                 {
                     EindDatum = DateOnlyHelper.ParseOrNull(magdaOnderneming.Stopzetting?.Datum, Formats.DateOnly)
                 });
 
-            var naamOndernemingType = GetBestMatchingNaam(magdaOnderneming.Namen.MaatschappelijkeNamen);
-
-            if (naamOndernemingType is null)
-                return VerenigingVolgensKboResult.GeenGeldigeVereniging;
-
-            var maatschappelijkeZetel =
-                magdaOnderneming.Adressen.SingleOrDefault(a => a.Type.Code.Value == AdresCodes.MaatschappelijkeZetel &&
-                                                               IsActiveToday(a.DatumBegin, a.DatumEinde));
-
-            return VerenigingVolgensKboResult.GeldigeVereniging(
-                new VerenigingVolgensKbo
-                {
-                    KboNummer = KboNummer.Create(kboNummer),
-                    Type = rechtsvormMap[GetActiveRechtsvorm(magdaOnderneming)!.Code.Value],
-                    Naam = naamOndernemingType.Naam ?? string.Empty,
-                    KorteNaam = GetBestMatchingNaam(magdaOnderneming.Namen.AfgekorteNamen)?.Naam ?? string.Empty,
-                    Startdatum = DateOnlyHelper.ParseOrNull(magdaOnderneming.Start.Datum, Formats.DateOnly),
-                    EindDatum = DateOnlyHelper.ParseOrNull(magdaOnderneming.Stopzetting?.Datum, Formats.DateOnly),
-                    IsActief = IsActiefOfInOprichting(magdaOnderneming),
-                    Adres = GetAdresFrom(maatschappelijkeZetel),
-                    Contactgegevens = GetContactgegevensFrom(maatschappelijkeZetel),
-                    Vertegenwoordigers = []// GetVertegenwoordigers(magdaOnderneming.Functies),
-                });
+            return VerenigingVolgensKboResult.GeldigeVereniging(magdaOnderneming.MapVerenigingVolgensKbo(kboNummer));
         }
         catch (Exception e)
         {
             throw new MagdaException(message: "Er heeft zich een fout voorgedaan bij het aanroepen van de Magda GeefOndernemingDienst.", e);
         }
     }
-
-    protected bool IsActiefOfInOprichting(Onderneming2_0Type magdaOnderneming)
-        => IsActief(magdaOnderneming) || IsInOprichting(magdaOnderneming);
-
-    protected VertegenwoordigerVolgensKbo[] GetVertegenwoordigers(FunctieType[] functies)
-        => functies.Select(x => new VertegenwoordigerVolgensKbo()
-        {
-            Insz = x.Persoon.INSZ,
-            Voornaam = x.Persoon.VoorNaam,
-            Achternaam = x.Persoon.AchterNaam
-        }).ToArray();
-
-    protected bool HeeftToegestaneActieveRechtsvorm(Onderneming2_0Type magdaOnderneming)
-    {
-        var rechtsvormCode = GetActiveRechtsvorm(magdaOnderneming)?.Code.Value;
-
-        return rechtsvormCode != null && rechtsvormMap.ContainsKey(rechtsvormCode);
-    }
-
-    protected ContactgegevensVolgensKbo GetContactgegevensFrom(AdresOndernemingType? maatschappelijkeZetel)
-    {
-        if (maatschappelijkeZetel is null)
-            return new ContactgegevensVolgensKbo();
-
-        var contactDetails = GetBestMatchingAdres(maatschappelijkeZetel.Descripties).Contact;
-
-        return new ContactgegevensVolgensKbo
-        {
-            Email = contactDetails?.Email,
-            Telefoonnummer = contactDetails?.Telefoonnummer,
-            GSM = contactDetails?.GSM,
-            Website = contactDetails?.Website,
-        };
-    }
-
-    protected AdresVolgensKbo GetAdresFrom(AdresOndernemingType? maatschappelijkeZetel)
-    {
-        if (maatschappelijkeZetel is null)
-            return new AdresVolgensKbo();
-
-        var adresDetails = GetBestMatchingAdres(maatschappelijkeZetel.Descripties).Adres;
-
-        return new AdresVolgensKbo
-        {
-            Straatnaam = adresDetails?.Straat?.Naam ?? maatschappelijkeZetel.Straat?.Naam,
-            Huisnummer = adresDetails?.Huisnummer ?? maatschappelijkeZetel.Huisnummer,
-            Busnummer = adresDetails?.Busnummer ?? maatschappelijkeZetel.Busnummer,
-            Postcode = adresDetails?.Gemeente?.PostCode ?? maatschappelijkeZetel.Gemeente?.PostCode,
-            Gemeente = adresDetails?.Gemeente?.Naam ?? maatschappelijkeZetel.Gemeente?.Naam,
-            Land = adresDetails?.Land?.Naam ?? maatschappelijkeZetel.Land?.Naam,
-        };
-    }
-
-    protected static NaamOndernemingType? GetBestMatchingNaam(NaamOndernemingType[]? namen)
-    {
-        if (namen is null) return null;
-
-        var activeNamen = namen
-                         .Where(n => IsActiveToday(n.DatumBegin, n.DatumEinde))
-                         .ToArray();
-
-        if (activeNamen.Length == 0)
-            return null;
-
-        if (activeNamen.Length == 1)
-            return activeNamen.Single();
-
-        return GetNaamInTaal(activeNamen, TaalCodes.Nederlands) ??
-               GetNaamInTaal(activeNamen, TaalCodes.Frans) ??
-               GetNaamInTaal(activeNamen, TaalCodes.Duits) ??
-               GetNaamInTaal(activeNamen, TaalCodes.Engels) ??
-               activeNamen.First();
-    }
-
-    protected static DescriptieType GetBestMatchingAdres(DescriptieType[] descripties)
-    {
-        if (descripties.Length == 1)
-            return descripties.Single();
-
-        return GetDescriptieInTaal(descripties, TaalCodes.Nederlands) ??
-               GetDescriptieInTaal(descripties, TaalCodes.Frans) ??
-               GetDescriptieInTaal(descripties, TaalCodes.Duits) ??
-               GetDescriptieInTaal(descripties, TaalCodes.Engels) ??
-               descripties.First();
-    }
-
-    protected static NaamOndernemingType? GetNaamInTaal(NaamOndernemingType[] namen, string taalcode)
-        => namen.SingleOrDefault(n => n.Taalcode.Equals(taalcode, StringComparison.InvariantCultureIgnoreCase));
-
-    protected static DescriptieType? GetDescriptieInTaal(DescriptieType[] descripties, string taalcode)
-        => descripties.SingleOrDefault(n => n.Taalcode.Equals(taalcode, StringComparison.InvariantCultureIgnoreCase));
-
-    protected static bool IsActiveToday(string datumBegin, string datumEinde)
-        => DateOnlyHelper.ParseOrNull(datumBegin, Formats.DateOnly).IsNullOrBeforeToday() &&
-           DateOnlyHelper.ParseOrNull(datumEinde, Formats.DateOnly).IsNullOrAfterToday();
-
-    protected static RechtsvormExtentieType? GetActiveRechtsvorm(Onderneming2_0Type magdaOnderneming)
-        => magdaOnderneming.Rechtsvormen?.FirstOrDefault(
-            r => IsActiveToday(r.DatumBegin, r.DatumEinde));
-
-    protected static bool IsRechtspersoon(Onderneming2_0Type magdaOnderneming)
-        => magdaOnderneming.SoortOnderneming.Code.Value == SoortOndernemingCodes.Rechtspersoon;
-
-    protected static bool IsActief(Onderneming2_0Type magdaOnderneming)
-        => magdaOnderneming.StatusKBO.Code.Value == StatusKBOCodes.Actief;
-
-    protected static bool IsInOprichting(Onderneming2_0Type magdaOnderneming)
-        => magdaOnderneming.StatusKBO.Code.Value == StatusKBOCodes.InOprichting;
-
-    protected static bool IsOnderneming(Onderneming2_0Type magdaOnderneming)
-        => magdaOnderneming.OndernemingOfVestiging.Code.Value == OndernemingOfVestigingCodes.Onderneming;
 
     protected virtual Result<VerenigingVolgensKbo> HandleUitzonderingen(
         string kboNummer,
