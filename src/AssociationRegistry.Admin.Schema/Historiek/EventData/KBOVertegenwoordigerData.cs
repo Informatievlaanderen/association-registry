61e46ec5565845ca61a02021127a2a304f9933a4
emalfroy <egon.malfroy@gmail.com>
2025-10-13 14:18:19 +0200

    fix: or-1847 remove empty values from kbo changes in historiek

---
+++ src/AssociationRegistry.Admin.Schema/Historiek/EventData/KBOVertegenwoordigerData.cs
@@ -0,0 +1,31 @@
+﻿namespace AssociationRegistry.Admin.Schema.Historiek.EventData;
+
+using Events;
+using System.Runtime.Serialization;
+
+[DataContract]
+public record KBOVertegenwoordigerData
+(
+    [property: DataMember]
+    int VertegenwoordigerId,
+    [property: DataMember]
+    string Voornaam,
+    [property: DataMember]
+    string Achternaam
+)
+{
+    public static KBOVertegenwoordigerData Create(VertegenwoordigerWerdToegevoegdVanuitKBO vertegenwoordiger)
+        => new(vertegenwoordiger.VertegenwoordigerId,
+               vertegenwoordiger.Voornaam,
+               vertegenwoordiger.Achternaam);
+
+    public static KBOVertegenwoordigerData Create(VertegenwoordigerWerdGewijzigdInKBO vertegenwoordiger)
+        => new(vertegenwoordiger.VertegenwoordigerId,
+               vertegenwoordiger.Voornaam,
+               vertegenwoordiger.Achternaam);
+
+    public static KBOVertegenwoordigerData Create(VertegenwoordigerWerdVerwijderdUitKBO vertegenwoordiger)
+        => new(vertegenwoordiger.VertegenwoordigerId,
+               vertegenwoordiger.Voornaam,
+               vertegenwoordiger.Achternaam);
+}
