85229a75a876d7597966763fb27d26c11a6c7fbd
emalfroy <egon.malfroy@gmail.com>
2025-11-04 11:00:12 +0100

    feat: or-2949 add vertegenwoordigers persoonsgegevens

---
+++ src/AssociationRegistry/Persoonsgegevens/VertegenwoordigerPersoonsgegevens.cs
@@ -0,0 +1,21 @@
+﻿namespace AssociationRegistry.Persoonsgegevens;
+
+using Marten.Schema;
+
+public class VertegenwoordigerPersoonsgegevens
+{
+    [Identity]
+    public Guid RefId { get; set; }
+    public string VCode { get; set; }
+    public int VertegenwoordigerId { get; set; }
+    public string Insz { get; init; }
+    public bool IsPrimair { get; init; }
+    public string? Roepnaam { get; }
+    public string? Rol { get; }
+    public string Voornaam { get; }
+    public string Achternaam { get; }
+    public string Email { get; }
+    public string Telefoon { get; }
+    public string Mobiel { get; }
+    public string SocialMedia { get; }
+}
