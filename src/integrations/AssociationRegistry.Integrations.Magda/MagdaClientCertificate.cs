d4f8ed83f020960e655a000de2c535fc514ce98f
emalfroy <egon.malfroy@gmail.com>
2025-12-02 10:55:47 +0100

    feat: or-2935 set storage flags for magda client certificate os based

--- src/integrations/AssociationRegistry.Integrations.Magda/MagdaClientCertificate.cs
+++ src/integrations/AssociationRegistry.Integrations.Magda/MagdaClientCertificate.cs
@@ -4,14 +4,23 @@ using System.Security.Cryptography.X509Certificates;
 
 public class MagdaClientCertificate : X509Certificate2
 {
+
     private MagdaClientCertificate(string base64Cert, string password) :
         base(
             Convert.FromBase64String(base64Cert),
             password,
-            X509KeyStorageFlags.MachineKeySet |
-            X509KeyStorageFlags.PersistKeySet |
-            X509KeyStorageFlags.Exportable)
+            GetKeyStorageFlags())
     {
+    }
+
+    private static X509KeyStorageFlags GetKeyStorageFlags()
+    {
+        if(OperatingSystem.IsWindows())
+            return X509KeyStorageFlags.UserKeySet | X509KeyStorageFlags.PersistKeySet;
+
+        return X509KeyStorageFlags.MachineKeySet |
+               X509KeyStorageFlags.PersistKeySet |
+               X509KeyStorageFlags.Exportable;
     }
 
     public static MagdaClientCertificate Create(string base64Cert, string password)
