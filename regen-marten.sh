95532d2a332383720caedbb852df882ccaf926a6
Koen Metsu <koen@metsu.be>
2026-01-28 13:45:36 +0100

    refactor: or-2996 use pushd/popd

--- regen-marten.sh
+++ regen-marten.sh
@@ -5,6 +5,6 @@ dotnet run --project src/AssociationRegistry.Admin.ProjectionHost -- codegen wri
 dotnet run --project src/AssociationRegistry.Admin.Api -- codegen write
 dotnet run --project src/AssociationRegistry.Public.ProjectionHost -- codegen write
 dotnet run --project src/AssociationRegistry.Acm.Api -- codegen write
-cd src/AssociationRegistry.Admin.AddressSync
+pushd src/AssociationRegistry.Admin.AddressSync
 dotnet run -- codegen write
-cd ../..
+popd
