0a2770bb1085ec47e5d94a3d4b237d325e5fe979
jandammans <jan.dammans@vlaanderen.be>
2026-02-27 11:42:29 +0100

    feat: or-3015 log if event BankrekeningnummerWerdGevalideerdZonderPersoonsgegevens exists

--- src/AssociationRegistry/Events/Obsolete/BankrekeningnummerValidatieWerdOngedaanGemaaktDoorWijzigingTitularis.cs
+++ src/AssociationRegistry/Events/Obsolete/BankrekeningnummerValidatieWerdOngedaanGemaaktDoorWijzigingTitularis.cs
@@ -2,7 +2,7 @@
 
 using System.Runtime.Serialization;
 using Vereniging.Bronnen;
-[Obsolete("Feature werd gedeployed op testomgeving maar werd gedropped. Event behouden voor documentatie.")]
+[Obsolete("Feature was gedeployed on test environment but was dropped. Kept event for documentation.")]
 public record BankrekeningnummerValidatieWerdOngedaanGemaaktDoorWijzigingTitularis(
     int BankrekeningnummerId,
     string OngedaanGemaaktDoor
@@ -14,6 +14,7 @@ public record BankrekeningnummerValidatieWerdOngedaanGemaaktDoorWijzigingTitular
     protected virtual bool PrintMembers(System.Text.StringBuilder builder)
     {
         builder.Append($"BankrekeningnummerId = {BankrekeningnummerId}, ");
+        builder.Append($"OngedaanGemaaktDoor = {OngedaanGemaaktDoor}, ");
         return true;
     }
 }
