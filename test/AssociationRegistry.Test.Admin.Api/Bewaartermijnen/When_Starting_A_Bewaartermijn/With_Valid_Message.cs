b7dfb05d4165412758a4daee402b9fdcf949037e
emalfroy <egon.malfroy@gmail.com>
2026-03-30 11:21:35 +0200

    feat: commit for localstack test

--- test/AssociationRegistry.Test.Admin.Api/Bewaartermijnen/When_Starting_A_Bewaartermijn/With_Valid_Message.cs
+++ test/AssociationRegistry.Test.Admin.Api/Bewaartermijnen/When_Starting_A_Bewaartermijn/With_Valid_Message.cs
@@ -5,7 +5,6 @@ using AssociationRegistry.DecentraalBeheer.Vereniging;
 using AssociationRegistry.Framework;
 using AssociationRegistry.Test.Common.AutoFixture;
 using AutoFixture;
-using CommandHandling.Bewaartermijnen;
 using Events;
 using Integrations.Grar.Bewaartermijnen;
 using MartenDb.Store;
@@ -29,12 +28,17 @@ public class With_Valid_Message
         _commandMetadata = fixture.Create<CommandMetadata>();
         var commandHandler = new StartBewaartermijnMessageHandler();
         _eventStore = new Mock<IEventStore>();
-        _bewaartermijnOptions = new BewaartermijnOptions(){
-            Duration = TimeSpan.FromDays(1),
-        };
+        _bewaartermijnOptions = new BewaartermijnOptions() { Duration = TimeSpan.FromDays(1) };
 
-        commandHandler.Handle(new CommandEnvelope<StartBewaartermijnMessage>(command, _commandMetadata), _eventStore.Object, _bewaartermijnOptions, CancellationToken.None)
-                      .GetAwaiter().GetResult();
+        commandHandler
+            .Handle(
+                new CommandEnvelope<StartBewaartermijnMessage>(command, _commandMetadata),
+                _eventStore.Object,
+                _bewaartermijnOptions,
+                CancellationToken.None
+            )
+            .GetAwaiter()
+            .GetResult();
     }
 
     [Fact]
@@ -42,19 +46,26 @@ public class With_Valid_Message
     {
         var expectedAggregateId = $"{_vCode}-{_vertegenwoordigerId}";
         var expectedVervaldag = _commandMetadata.Tijdstip.PlusTicks(_bewaartermijnOptions.Duration.Ticks);
-        var expectedEvent = new BewaartermijnWerdGestart(expectedAggregateId, _vCode.ToString(), _vertegenwoordigerId,
-                                                         expectedVervaldag);
-        _eventStore.Verify(x => x.SaveNew(
-                               expectedAggregateId,
-                               _commandMetadata,
-                               It.IsAny<CancellationToken>(),
-                               It.Is<IEvent[]>(events =>
-                                                                     events.Length == 1 &&
-                                                                     ((BewaartermijnWerdGestart)events[0]).BewaartermijnId == expectedAggregateId &&
-                                                                     ((BewaartermijnWerdGestart)events[0]).VCode == _vCode.ToString() &&
-                                                                     ((BewaartermijnWerdGestart)events[0]).VertegenwoordigerId == _vertegenwoordigerId &&
-                                                                     ((BewaartermijnWerdGestart)events[0]).Vervaldag == expectedVervaldag
-                               )));
+        var expectedEvent = new BewaartermijnWerdGestart(
+            expectedAggregateId,
+            _vCode.ToString(),
+            _vertegenwoordigerId,
+            expectedVervaldag
+        );
+        _eventStore.Verify(x =>
+            x.SaveNew(
+                expectedAggregateId,
+                _commandMetadata,
+                It.IsAny<CancellationToken>(),
+                It.Is<IEvent[]>(events =>
+                    events.Length == 1
+                    && ((BewaartermijnWerdGestart)events[0]).BewaartermijnId == expectedAggregateId
+                    && ((BewaartermijnWerdGestart)events[0]).VCode == _vCode.ToString()
+                    && ((BewaartermijnWerdGestart)events[0]).VertegenwoordigerId == _vertegenwoordigerId
+                    && ((BewaartermijnWerdGestart)events[0]).Vervaldag == expectedVervaldag
+                )
+            )
+        );
     }
 
     //
