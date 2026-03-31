43af6d0c2ef8acfe2fab066914957d568c7e3c22
emalfroy <egon.malfroy@gmail.com>
2026-03-31 16:13:33 +0200

    test: or-3065 use bevestigingstoken on requestfactory

--- test/AssociationRegistry.Test.E2E/Scenarios/Requests/VZER/BeheerdetailRequestFactory.cs
+++ test/AssociationRegistry.Test.E2E/Scenarios/Requests/VZER/BeheerdetailRequestFactory.cs
@@ -1,51 +1,65 @@
 namespace AssociationRegistry.Test.E2E.Scenarios.Requests.VZER;
 
-using Alba;
+using System.Net;
 using Admin.Api.Infrastructure;
+using Admin.Api.WebApi.Verenigingen;
 using Admin.Api.WebApi.Verenigingen.Registreer.VerenigingZonderEigenRechtspersoonlijkheid.RequestModels;
-using Hosts.Configuration.ConfigurationBindings;
+using Alba;
 using AssociationRegistry.Test.Common.AutoFixture;
-using Framework.ApiSetup;
-using Vereniging;
 using AutoFixture;
 using DecentraalBeheer.Vereniging;
+using Framework.ApiSetup;
+using Hosts.Configuration.ConfigurationBindings;
 using Microsoft.Extensions.DependencyInjection;
-using System.Net;
+using Vereniging;
 
-public class BeheerdetailRequestFactory : ITestRequestFactory<RegistreerVerenigingZonderEigenRechtspersoonlijkheidRequest>
+public class BeheerdetailRequestFactory
+    : ITestRequestFactory<RegistreerVerenigingZonderEigenRechtspersoonlijkheidRequest>
 {
     private readonly string _isPositiveInteger = "^[1-9][0-9]*$";
 
-    public BeheerdetailRequestFactory()
-    {
-    }
+    public BeheerdetailRequestFactory() { }
 
-    public async Task<CommandResult<RegistreerVerenigingZonderEigenRechtspersoonlijkheidRequest>> ExecuteRequest(IApiSetup apiSetup)
+    public async Task<CommandResult<RegistreerVerenigingZonderEigenRechtspersoonlijkheidRequest>> ExecuteRequest(
+        IApiSetup apiSetup
+    )
     {
         var autoFixture = new Fixture().CustomizeAdminApi();
 
         var request = autoFixture.Create<RegistreerVerenigingZonderEigenRechtspersoonlijkheidRequest>();
+        var bevestigingsTokenHelper = new BevestigingsTokenHelper(
+            apiSetup.AdminApiHost.Services.GetRequiredService<AppSettings>()
+        );
 
-        var response = (await apiSetup.AdminApiHost.Scenario(s =>
-        {
-            s.Post
-             .Json(request, JsonStyle.Mvc)
-             .ToUrl("/v1/verenigingen/vzer");
+        var hashForAllowingDuplicate = bevestigingsTokenHelper.Calculate(request);
+        var response = (
+            await apiSetup.AdminApiHost.Scenario(s =>
+            {
+                s.WithRequestHeader(WellknownHeaderNames.BevestigingsToken, hashForAllowingDuplicate);
 
-            s.StatusCodeShouldBe(HttpStatusCode.Accepted);
+                s.Post.Json(request, JsonStyle.Mvc).ToUrl("/v1/verenigingen/vzer");
 
-            s.Header("Location").ShouldHaveValues();
+                s.StatusCodeShouldBe(HttpStatusCode.Accepted);
 
-            s.Header("Location")
-             .SingleValueShouldMatch($"{apiSetup.AdminApiHost.Services.GetRequiredService<AppSettings>().BaseUrl}/v1/verenigingen/V");
+                s.Header("Location").ShouldHaveValues();
 
-            s.Header(WellknownHeaderNames.Sequence).ShouldHaveValues();
-            s.Header(WellknownHeaderNames.Sequence).SingleValueShouldMatch(_isPositiveInteger);
-        })).Context.Response;
+                s.Header("Location")
+                    .SingleValueShouldMatch(
+                        $"{apiSetup.AdminApiHost.Services.GetRequiredService<AppSettings>().BaseUrl}/v1/verenigingen/V"
+                    );
+
+                s.Header(WellknownHeaderNames.Sequence).ShouldHaveValues();
+                s.Header(WellknownHeaderNames.Sequence).SingleValueShouldMatch(_isPositiveInteger);
+            })
+        ).Context.Response;
 
         var vCode = response.Headers.Location.First().Split('/').Last();
         long sequence = Convert.ToInt64(response.Headers[WellknownHeaderNames.Sequence].First());
 
-        return new CommandResult<RegistreerVerenigingZonderEigenRechtspersoonlijkheidRequest>(VCode.Create(vCode), request, sequence);
+        return new CommandResult<RegistreerVerenigingZonderEigenRechtspersoonlijkheidRequest>(
+            VCode.Create(vCode),
+            request,
+            sequence
+        );
     }
 }
