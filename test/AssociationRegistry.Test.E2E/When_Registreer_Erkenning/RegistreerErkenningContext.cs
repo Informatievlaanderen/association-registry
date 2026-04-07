42f9bf35c53f665558d5311f3babfc7c92fdc135
Jan Dammans <jan.dammans@vlaanderen.be>
2026-04-07 15:58:34 +0200

    test: or-3071 implement e2e test for registreer erkenning

---
+++ test/AssociationRegistry.Test.E2E/When_Registreer_Erkenning/RegistreerErkenningContext.cs
@@ -0,0 +1,29 @@
+namespace AssociationRegistry.Test.E2E.When_Registreer_Erkenning;
+
+using Admin.Api.WebApi.Verenigingen.Erkenningen.RegistreerErkenning.RequestModels;
+using Framework.ApiSetup;
+using Framework.TestClasses;
+using Scenarios.Givens.VerenigingZonderEigenRechtspersoonlijkheid;
+using Scenarios.Requests.FeitelijkeVereniging;
+using Xunit;
+
+public class RegistreerErkenningContext : TestContextBase<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdScenario, RegistreerErkenningRequest>
+{
+
+    protected override VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdScenario InitializeScenario()
+        => new();
+
+    public RegistreerErkenningContext(FullBlownApiSetup apiSetup) : base(apiSetup)
+    {
+    }
+
+    protected override async ValueTask ExecuteScenario(VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdScenario scenario)
+    {
+        CommandResult = await new RegistreerErkenningRequestFactory(scenario).ExecuteRequest(ApiSetup);
+    }
+}
+
+[CollectionDefinition(nameof(RegistreerErkenningCollection))]
+public class RegistreerErkenningCollection : ICollectionFixture<RegistreerErkenningContext>
+{
+}
