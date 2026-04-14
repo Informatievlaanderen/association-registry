f4e9be3ee3238bf5ffcdf54033cf3e92436a52fa
Jan Dammans <jan.dammans@vlaanderen.be>
2026-04-14 14:10:23 +0200

    feat: or-3097 add enpoints to trigger bewaartermijn scheduled host

---
+++ src/AssociationRegistry.Admin.Api/Infrastructure/HttpClients/ScheduledHostHttpClient.cs
@@ -0,0 +1,19 @@
+﻿namespace AssociationRegistry.Admin.Api.Infrastructure.HttpClients;
+
+public class ScheduledHostHttpClient : IDisposable
+{
+    private readonly HttpClient _httpClient;
+
+    public ScheduledHostHttpClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<HttpResponseMessage> TriggerBewaartermijnScheduledHost(CancellationToken cancellationToken) =>
+        await _httpClient.PostAsync(requestUri: "/v1/trigger/bewaartermijn", content: null, cancellationToken);
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
---
+++ src/AssociationRegistry.Admin.Api/WebApi/Administratie/ScheduledHost/ScheduledHostController.cs
@@ -0,0 +1,31 @@
+﻿namespace AssociationRegistry.Admin.Api.WebApi.Administratie.ScheduledHost;
+
+using Asp.Versioning;
+using Be.Vlaanderen.Basisregisters.Api;
+using Infrastructure.HttpClients;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// SuperAdmin endpoint to manually call.
+/// This will try to match any location without an AdresId.
+/// </summary>
+[ApiVersion("1.0")]
+[AdvertiseApiVersions("1.0")]
+[ApiRoute("admin")]
+[ApiExplorerSettings(IgnoreApi = true)]
+[Authorize(Policy = Program.SuperAdminPolicyName)]
+public class ScheduledHostController : ApiController
+{
+    [HttpPost("scheduledhost/trigger/bewaartermijn")]
+    public async Task<IActionResult> Get(
+    [FromServices] ScheduledHostHttpClient client,
+        [FromServices] ILogger<ScheduledHostController> logger,
+        CancellationToken cancellationToken
+    )
+    {
+        await client.TriggerBewaartermijnScheduledHost(cancellationToken);
+
+        return Ok();
+    }
+}
