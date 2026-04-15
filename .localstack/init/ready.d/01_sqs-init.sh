88ad6d6fb1876b0ed11af6c0af8b7b9f811f6c02
Sadik-Dev <66747875+Sadik-Dev@users.noreply.github.com>
2026-04-15 09:56:18 +0200

    Revert "fix: or-3099 create ministack s3 bucket during init"
    This reverts commit ec7e1592be6624fa844952d7e8c9ad088165def6.

--- .localstack/init/ready.d/01_sqs-init.sh
+++ .localstack/init/ready.d/01_sqs-init.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Set variables
-REGION="${AWS_DEFAULT_REGION:-us-east-1}"
-ACCOUNT_ID="${MINISTACK_ACCOUNT_ID:-123456789012}"
-ENDPOINT_URL="${AWS_ENDPOINT_URL:-http://127.0.0.1:4566}"
+REGION="us-east-1"
+ACCOUNT_ID="000000000000" # Default account ID used by LocalStack
+MAX_RECEIVE_COUNT=3
+QUEUE_PART=""
 
 with_dlq()
 {
@@ -11,10 +12,10 @@ with_dlq()
   DLQ_NAME="$QUEUE_NAME-dlq"
   DLQ_ARN="arn:aws:sqs:$REGION:$ACCOUNT_ID:$DLQ_NAME"
 
-  aws --endpoint-url="$ENDPOINT_URL" sqs create-queue --region "$REGION" --queue-name "$QUEUE_NAME"
-  aws --endpoint-url="$ENDPOINT_URL" sqs create-queue --region "$REGION" --queue-name "$DLQ_NAME"
-  QUEUE_URL=$(aws --endpoint-url="$ENDPOINT_URL" sqs get-queue-url --region "$REGION" --queue-name "$QUEUE_NAME" --query QueueUrl --output text)
-  aws --endpoint-url="$ENDPOINT_URL" sqs set-queue-attributes \
+  aws --endpoint-url=http://127.0.0.1:4566 sqs create-queue --region $REGION --queue-name $QUEUE_NAME
+  aws --endpoint-url=http://127.0.0.1:4566 sqs create-queue --region $REGION --queue-name $DLQ_NAME
+  QUEUE_URL=$(aws --endpoint-url=http://127.0.0.1:4566 sqs get-queue-url --region $REGION --queue-name $QUEUE_NAME --query QueueUrl --output text)
+  aws --endpoint-url=http://127.0.0.1:4566 sqs set-queue-attributes \
     --queue-url "$QUEUE_URL" \
     --attributes "{
       \"RedrivePolicy\": \"{ \\\"maxReceiveCount\\\": \\\"3\\\", \\\"deadLetterTargetArn\\\": \\\"$DLQ_ARN\\\" }\"
