75c1cf3eb7232a7c01d749bbda4133e18546f020
Sadik-Dev <66747875+Sadik-Dev@users.noreply.github.com>
2026-04-15 09:56:18 +0200

    Revert "fix: or-3099 create ministack s3 bucket during init"
    This reverts commit ec7e1592be6624fa844952d7e8c9ad088165def6.

--- .localstack/init/ready.d/02_s3-init.sh
+++ .localstack/init/ready.d/02_s3-init.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-set -e
-
 REGION="${AWS_DEFAULT_REGION:-us-east-1}"
-ENDPOINT_URL="${AWS_ENDPOINT_URL:-http://127.0.0.1:4566}"
 
-if ! aws --endpoint-url="$ENDPOINT_URL" s3api head-bucket --bucket verenigingsregister-uwp-data >/dev/null 2>&1; then
-  aws --endpoint-url="$ENDPOINT_URL" s3api create-bucket --region "$REGION" --bucket verenigingsregister-uwp-data
-fi
+aws --endpoint-url=http://127.0.0.1:4566 s3api create-bucket --bucket verenigingsregister-uwp-data
