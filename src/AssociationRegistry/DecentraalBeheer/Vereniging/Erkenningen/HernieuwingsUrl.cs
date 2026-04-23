8c78ac5d986d61993ebe28bd8e00df0d5952403c
Jan Dammans <jan.dammans@vlaanderen.be>
2026-04-23 10:00:48 +0200

    feat: or-3071 throw WebsiteMoetStartenMetHttps for hernieuwingsurl

--- src/AssociationRegistry/DecentraalBeheer/Vereniging/Erkenningen/HernieuwingsUrl.cs
+++ src/AssociationRegistry/DecentraalBeheer/Vereniging/Erkenningen/HernieuwingsUrl.cs
@@ -1,6 +1,7 @@
 namespace AssociationRegistry.DecentraalBeheer.Vereniging.Erkenningen;
 
 using Exceptions;
+using Websites.Exceptions;
 
 public record HernieuwingsUrl
 {
@@ -16,16 +17,23 @@ public record HernieuwingsUrl
         if (string.IsNullOrEmpty(url))
             return new HernieuwingsUrl(string.Empty);
 
+        if (!IsValidScheme(url))
+            throw new WebsiteMoetStartenMetHttps();
+
         if (!IsValid(url))
             throw new OngeldigUrl();
 
         return new HernieuwingsUrl(url);
     }
 
+    private static bool IsValidScheme(string url) =>
+        url.StartsWith("http://", StringComparison.OrdinalIgnoreCase) ||
+        url.StartsWith("https://", StringComparison.OrdinalIgnoreCase);
+
     public static HernieuwingsUrl Hydrate(string url) => new(url);
 
     private static bool IsValid(string url) =>
-        Uri.TryCreate(url, UriKind.Absolute, out var uri)
-        && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)
-        && !string.IsNullOrWhiteSpace(uri.Host);
+        Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out var uri)
+     && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps)
+     && !string.IsNullOrWhiteSpace(uri.Host);
 }
