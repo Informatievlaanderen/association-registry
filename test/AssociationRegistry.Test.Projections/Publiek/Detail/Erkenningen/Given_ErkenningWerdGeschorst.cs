2a9733aa654ef10ef7a6e9ce220a5834ccf9a09f
Jan Dammans <jan.dammans@vlaanderen.be>
2026-04-28 15:13:27 +0200

    feat: or-3076 implement ErkenningWerdGeschorst in publiek detail en mutatiedienst

---
+++ test/AssociationRegistry.Test.Projections/Publiek/Detail/Erkenningen/Given_ErkenningWerdGeschorst.cs
@@ -0,0 +1,40 @@
+﻿namespace AssociationRegistry.Test.Projections.Publiek.Detail.Erkenningen;
+
+using DecentraalBeheer.Vereniging.Erkenningen;
+using Public.Schema.Detail;
+using Scenario.Erkenningen;
+
+[Collection(nameof(ProjectionContext))]
+public class Given_ErkenningWerdGeschorst(PubliekDetailScenarioFixture<ErkenningWerdGeschorstScenario> fixture)
+    : PubliekDetailScenarioClassFixture<ErkenningWerdGeschorstScenario>
+{
+    [Fact]
+    public void Document_Is_Updated() =>
+        fixture
+            .Result.Erkenningen.First()
+            .Should()
+            .BeEquivalentTo(
+                new PubliekVerenigingDetailDocument.Types.Erkenning
+                {
+                    JsonLdMetadata = null,
+                    ErkenningId = fixture.Scenario.ErkenningWerdGeregistreerd.ErkenningId,
+                    GeregistreerdDoor = new()
+                    {
+                        OvoCode = fixture.Scenario.ErkenningWerdGeregistreerd.GeregistreerdDoor.OvoCode,
+                        Naam = fixture.Scenario.ErkenningWerdGeregistreerd.GeregistreerdDoor.Naam,
+                    },
+                    IpdcProduct = new()
+                    {
+                        Nummer = fixture.Scenario.ErkenningWerdGeregistreerd.IpdcProduct.Nummer,
+                        Naam = fixture.Scenario.ErkenningWerdGeregistreerd.IpdcProduct.Naam,
+                    },
+                    Startdatum = fixture.Scenario.ErkenningWerdGeregistreerd.Startdatum,
+                    Einddatum = fixture.Scenario.ErkenningWerdGeregistreerd.Einddatum,
+                    Hernieuwingsdatum = fixture.Scenario.ErkenningWerdGeregistreerd.Hernieuwingsdatum,
+                    HernieuwingsUrl = fixture.Scenario.ErkenningWerdGeregistreerd.HernieuwingsUrl,
+                    Motivering = fixture.Scenario.ErkenningWerdGeschorst.RedenSchorsing,
+                    Status = ErkenningStatus.Geschorst,
+                },
+                config: options => options.Excluding(x => x.JsonLdMetadata)
+            );
+}
