000dd4814ab3411b6342f1da8f89c391bcfe6c5a
Sadik-Dev <oussama.sadik@vlaanderen.be>
2026-05-19 11:00:07 +0200

    feat: or-3074 add validation for empty corrigeererkenning request

--- src/AssociationRegistry.Admin.Api/WebApi/DecentraalBeheer/Verenigingen/Erkenningen/CorrigeerErkenning/CorrigeerErkenningValidator.cs
+++ src/AssociationRegistry.Admin.Api/WebApi/DecentraalBeheer/Verenigingen/Erkenningen/CorrigeerErkenning/CorrigeerErkenningValidator.cs
@@ -2,6 +2,7 @@
 
 using AssociationRegistry.Admin.Api.Infrastructure.WebApi.Validation;
 using FluentValidation;
+using RegistreerErkenning;
 using RequestModels;
 
 // ReSharper disable ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
@@ -9,5 +10,15 @@ public class CorrigeerErkenningValidator : AbstractValidator<CorrigeerErkenningR
 {
     public CorrigeerErkenningValidator()
     {
+        RuleFor(request => request)
+            .Must(HaveAtLeastOneValue)
+            .OverridePropertyName("request")
+            .WithMessage("Een request mag niet leeg zijn.");
     }
+
+    private static bool HaveAtLeastOneValue(CorrigeerErkenningRequest request) =>
+        request.Startdatum.HasValue
+        || request.Einddatum.HasValue
+        || request.Hernieuwingsdatum.HasValue
+        || request.HernieuwingsUrl is not null;
 }
