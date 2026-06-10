d6006b6c79b4a72905a67769ce397e35d0983e73
emalfroy <egon.malfroy@gmail.com>
2026-06-10 11:15:07 +0200

    feat: or-3125 implement get opvolger organisaties from wegwijs client

--- src/integrations/AssociationRegistry.Integrations.Wegwijs/Clients/WegwijsClient.cs
+++ src/integrations/AssociationRegistry.Integrations.Wegwijs/Clients/WegwijsClient.cs
@@ -7,6 +7,8 @@ using Responses;
 
 public class WegwijsClient : IWegwijsClient
 {
+    private static readonly Guid WordtOpgevolgdDoorRelationId = new("2c68b8eb-55d2-ff8e-3301-f0fb12467df7");
+
     private readonly HttpClient _httpClient;
 
     public WegwijsClient(HttpClient httpClient)
@@ -45,4 +47,41 @@ public class WegwijsClient : IWegwijsClient
             throw new WegwijsException(e);
         }
     }
+
+    public async Task<IReadOnlyList<string>> GetOpvolgerOrganisaties(
+        string ovoCode,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var opvolgers = new List<string>();
+        string? volgendOvoCode = ovoCode;
+
+        try
+        {
+            do
+            {
+                volgendOvoCode = await GetDirecteOpvolger(volgendOvoCode, cancellationToken);
+
+                if (volgendOvoCode is not null)
+                    opvolgers.Add(volgendOvoCode);
+            } while (volgendOvoCode is not null);
+        }
+        catch (OrganisatieNietGevondenException)
+        {
+            return opvolgers;
+        }
+
+        return opvolgers;
+    }
+
+    private async Task<string?> GetDirecteOpvolger(string ovoCode, CancellationToken cancellationToken)
+    {
+        var organisatie = await GetOrganisationByOvoCode(ovoCode, cancellationToken);
+        return HaalOpvolgerOvoCodeUitRelaties(organisatie);
+    }
+
+    private static string? HaalOpvolgerOvoCodeUitRelaties(OrganisationResponse organisatie) =>
+        organisatie
+            .Relations.FirstOrDefault(r => r.RelationId == WordtOpgevolgdDoorRelationId)
+            ?.RelatedOrganisationOvoNumber;
 }
