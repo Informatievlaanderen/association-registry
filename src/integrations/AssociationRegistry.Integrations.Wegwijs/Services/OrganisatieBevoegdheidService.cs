340465445efe94c30dbf23cde9845ea8eb048c01
emalfroy <egon.malfroy@gmail.com>
2026-06-11 14:26:26 +0200

    feat: or-3125 implement service to check gemachtigde opvolger organisaties

---
+++ src/integrations/AssociationRegistry.Integrations.Wegwijs/Services/OrganisatieBevoegdheidService.cs
@@ -0,0 +1,27 @@
+﻿namespace AssociationRegistry.Integrations.Wegwijs.Services;
+
+using AssociationRegistry.Wegwijs;
+using Clients;
+using DecentraalBeheer.Vereniging.Erkenningen.Exceptions;
+using Framework;
+
+public class OrganisatieBevoegdheidService : IOrganisatieBevoegdheidService
+{
+    private readonly IWegwijsClient _client;
+
+    public OrganisatieBevoegdheidService(IWegwijsClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<string[]> IsGemachtigdeOrganisatie(string initiator, string geregistreerdDoor)
+    {
+        if (initiator == geregistreerdDoor)
+            return [];
+
+        var opvolgers = await _client.GetOpvolgerOrganisaties(geregistreerdDoor);
+        Throw<GiIsNietBevoegd>.If(!opvolgers.Contains(initiator));
+
+        return opvolgers.ToArray();
+    }
+}
