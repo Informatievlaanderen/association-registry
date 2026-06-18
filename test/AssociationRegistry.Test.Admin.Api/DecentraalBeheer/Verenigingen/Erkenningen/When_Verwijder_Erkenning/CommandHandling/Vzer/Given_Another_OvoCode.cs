7022cbaae424a683072d1991fd74b7ecc859e722
Jan Dammans <jan.dammans@vlaanderen.be>
2026-06-18 17:10:14 +0200

    refactor: or-3125 use context for commandhandling tests for verwijder erkenning
    Signed-off-by: Jan Dammans <jan.dammans@vlaanderen.be>

--- test/AssociationRegistry.Test.Admin.Api/DecentraalBeheer/Verenigingen/Erkenningen/When_Verwijder_Erkenning/CommandHandling/Vzer/Given_Another_OvoCode.cs
+++ test/AssociationRegistry.Test.Admin.Api/DecentraalBeheer/Verenigingen/Erkenningen/When_Verwijder_Erkenning/CommandHandling/Vzer/Given_Another_OvoCode.cs
@@ -1,52 +1,24 @@
 namespace AssociationRegistry.Test.Admin.Api.DecentraalBeheer.Verenigingen.Erkenningen.When_Verwijder_Erkenning.CommandHandling.Vzer;
 
-using AssociationRegistry.CommandHandling.DecentraalBeheer.Acties.Erkenningen.VerwijderErkenning;
 using AssociationRegistry.DecentraalBeheer.Vereniging.Erkenningen.Exceptions;
-using AssociationRegistry.Framework;
-using AutoFixture;
-using Common.AutoFixture;
 using Common.Scenarios.CommandHandling.VerenigingZonderEigenRechtspersoonlijkheid;
-using Common.StubsMocksFakes.VerenigingsRepositories;
-using Common.StubsMocksFakes.Wegwijs;
 using FluentAssertions;
 using Resources;
 using Xunit;
 
 public class Given_Another_OvoCode
 {
-    private readonly VerwijderErkenningCommandHandler _commandHandler;
-    private readonly Fixture _fixture;
-    private readonly VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdWithErkenningScenario _scenario;
-    private readonly AggregateSessionMock _verenigingRepositoryMock;
-
-    public Given_Another_OvoCode()
-    {
-        _fixture = new Fixture().CustomizeAdminApi();
-        _scenario = new VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdWithErkenningScenario();
-        _verenigingRepositoryMock = new AggregateSessionMock(_scenario.GetVerenigingState());
-        _commandHandler = new VerwijderErkenningCommandHandler(_verenigingRepositoryMock);
-    }
+    private readonly VerwijderErkenningContext<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdWithErkenningScenario> _ctx =
+        new(new VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdWithErkenningScenario(),
+            s => s.ErkenningWerdGeregistreerd.ErkenningId);
 
     [Fact]
     public async ValueTask Then_Throw_GiIsNietBevoegd()
     {
-        var invalidErkenningId = _scenario.ErkenningWerdGeregistreerd.ErkenningId;
+        var command = _ctx.CreateCommand();
+        var metadata = _ctx.CreateMetadata();
 
-        var command = _fixture.Create<VerwijderErkenningCommand>() with
-        {
-            VCode = _scenario.VCode,
-            ErkenningId = invalidErkenningId,
-        };
-
-        var commandEnvelope = new CommandEnvelope<VerwijderErkenningCommand>(
-            command,
-            _fixture.Create<CommandMetadata>()
-        );
-
-        var exception = await Assert.ThrowsAsync<GiIsNietBevoegd>(async () =>
-        {
-            await _commandHandler.Handle(commandEnvelope, new IOrganisatieBevoegdheidServiceMockStub().Object);
-        });
+        var exception = await Assert.ThrowsAsync<GiIsNietBevoegd>(async () => await _ctx.Handle(command, metadata));
 
         exception.Message.Should().Be(string.Format(ExceptionMessages.GiIsNietBevoegd));
     }
--- test/AssociationRegistry.Test.Admin.Api/DecentraalBeheer/Verenigingen/Erkenningen/When_Verwijder_Erkenning/CommandHandling/Vzer/Given_Verlopen_Erkenning.cs
+++ test/AssociationRegistry.Test.Admin.Api/DecentraalBeheer/Verenigingen/Erkenningen/When_Verwijder_Erkenning/CommandHandling/Vzer/Given_Verlopen_Erkenning.cs
@@ -1,53 +1,23 @@
 namespace AssociationRegistry.Test.Admin.Api.DecentraalBeheer.Verenigingen.Erkenningen.When_Verwijder_Erkenning.CommandHandling.Vzer;
 
-using AssociationRegistry.CommandHandling.DecentraalBeheer.Acties.Erkenningen.VerwijderErkenning;
-using AssociationRegistry.DecentraalBeheer.Vereniging.Erkenningen.Exceptions;
-using AssociationRegistry.Framework;
-using AutoFixture;
-using Common.AutoFixture;
 using Common.Scenarios.CommandHandling.VerenigingZonderEigenRechtspersoonlijkheid;
-using Common.StubsMocksFakes.VerenigingsRepositories;
-using Common.StubsMocksFakes.Wegwijs;
 using Events;
-using FluentAssertions;
-using Resources;
 using Xunit;
 
 public class Given_Verlopen_Erkenning
 {
-    private readonly VerwijderErkenningCommandHandler _commandHandler;
-    private readonly Fixture _fixture;
-    private readonly VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdWithVerlopenErkenningScenario _scenario;
-    private readonly AggregateSessionMock _verenigingRepositoryMock;
-
-    public Given_Verlopen_Erkenning()
-    {
-        _fixture = new Fixture().CustomizeAdminApi();
-        _scenario = new VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdWithVerlopenErkenningScenario();
-        _verenigingRepositoryMock = new AggregateSessionMock(_scenario.GetVerenigingState());
-        _commandHandler = new VerwijderErkenningCommandHandler(_verenigingRepositoryMock);
-    }
+    private readonly VerwijderErkenningContext<VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdWithVerlopenErkenningScenario> _ctx =
+        new(new VerenigingZonderEigenRechtspersoonlijkheidWerdGeregistreerdWithVerlopenErkenningScenario(),
+            s => s.ErkenningWerdGeregistreerd.ErkenningId,
+            s => s.ErkenningWerdGeregistreerd.GeregistreerdDoor.OvoCode);
 
     [Fact]
     public async ValueTask Then_Then_It_Saves_An_ErkenningWerdVerwijderd_Event()
     {
-        var verlopenErkenningId = _scenario.ErkenningWerdGeregistreerd.ErkenningId;
+        var command = _ctx.CreateCommand();
 
-        var command = _fixture.Create<VerwijderErkenningCommand>() with
-        {
-            VCode = _scenario.VCode,
-            ErkenningId = verlopenErkenningId,
-        };
+        await _ctx.Handle(command);
 
-        var commandMetadata = _fixture.Create<CommandMetadata>() with
-        {
-            Initiator = _scenario.ErkenningWerdGeregistreerd.GeregistreerdDoor.OvoCode,
-        };
-
-        var commandEnvelope = new CommandEnvelope<VerwijderErkenningCommand>(command, commandMetadata);
-
-        await _commandHandler.Handle(new CommandEnvelope<VerwijderErkenningCommand>(command, commandMetadata), new IOrganisatieBevoegdheidServiceMockStub().Object);
-
-        _verenigingRepositoryMock.ShouldHaveSavedExact(new ErkenningWerdVerwijderd(command.ErkenningId));
+        _ctx.AggregateSessionMock.ShouldHaveSavedExact(new ErkenningWerdVerwijderd(command.ErkenningId));
     }
 }
