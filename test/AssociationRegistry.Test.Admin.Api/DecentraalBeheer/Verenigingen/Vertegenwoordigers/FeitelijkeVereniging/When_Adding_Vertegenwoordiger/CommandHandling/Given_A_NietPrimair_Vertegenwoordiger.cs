7f4ca0bb4694086f01b44602ce1e0b60fa006a6f
Jan Dammans <jan.dammans@vlaanderen.be>
2026-06-23 10:48:41 +0200

    test: or-3254 verify that startbewaartermijnmessage is not send on actieve vereniging when adding vertegenwoordiger
    Signed-off-by: Jan Dammans <jan.dammans@vlaanderen.be>

--- test/AssociationRegistry.Test.Admin.Api/DecentraalBeheer/Verenigingen/Vertegenwoordigers/FeitelijkeVereniging/When_Adding_Vertegenwoordiger/CommandHandling/Given_A_NietPrimair_Vertegenwoordiger.cs
+++ test/AssociationRegistry.Test.Admin.Api/DecentraalBeheer/Verenigingen/Vertegenwoordigers/FeitelijkeVereniging/When_Adding_Vertegenwoordiger/CommandHandling/Given_A_NietPrimair_Vertegenwoordiger.cs
@@ -1,8 +1,11 @@
 namespace AssociationRegistry.Test.Admin.Api.DecentraalBeheer.Verenigingen.Vertegenwoordigers.FeitelijkeVereniging.When_Adding_Vertegenwoordiger.CommandHandling;
 
+using AssociationRegistry.DecentraalBeheer.Vereniging.Bewaartermijnen.Messages;
 using Common.Scenarios.CommandHandling.FeitelijkeVereniging;
 using Events;
 using FluentAssertions;
+using Moq;
+using Wolverine;
 using Xunit;
 
 public class Given_A_NietPrimair_Vertegenwoordiger
@@ -48,5 +51,20 @@ public class Given_A_NietPrimair_Vertegenwoordiger
             .VertegenwoordigerId;
 
         result.EntityId.Should().Be(vertegenwoordigerId);
+    }
+
+    [Fact]
+    public async ValueTask Then_BewaartermijnMessage_Is_Not_Sent()
+    {
+        var command = _ctx.CreateCommand();
+
+        await _ctx.Handle(command);
+
+        _ctx.OutboxMock.Verify(
+            x => x.SendAsync(
+                It.IsAny<StartBewaartermijnMessage>(),
+                It.IsAny<DeliveryOptions>()),
+            Times.Never
+        );
     }
 }
